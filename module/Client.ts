e9d120da22ebeb7d7e26f5631b315c3d5c092763
Will Hoskings <resynth1943@outlook.com>
2020-02-09 16:08:29 +0000

    RequestHandler -> RequestManager

--- module/Client.ts
+++ module/Client.ts
@@ -1,21 +1,22 @@
 import { endpoints } from "../constants/discord.ts"
-import RequestHandler from "../services/RequestHandler.ts"
+import RequestManager from "../managers/RequestManager.ts";
 
 class Client {
 	/** The bot's token. This should never be used by end users. It is meant to be used internally to make requests to the Discord API. */
 	token: string
+
 	/** The Rate limit manager to handle all outgoing requests to discord. Not meant to be used by users. */
-	RequestHandler: RequestHandler
+	protected requestManager: RequestManager;
 
 	constructor(token: string) {
 		this.token = `Bot ${token}`
-		this.RequestHandler = new RequestHandler(this, this.token)
+		this.requestManager = new RequestManager(this, this.token);
 	}
 
 	async connect() {
 		// const data = await fetch(endpoints.GATEWAY_BOT).then(res => res.json())
 		// console.log(data)
-		console.log(await this.RequestHandler.get(endpoints.GATEWAY_BOT))
+		console.log(await this.requestManager.get(endpoints.GATEWAY_BOT))
 	}
 }
 
