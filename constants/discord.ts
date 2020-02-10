e936a8c7b30ead273de9160cedf5a87ce6e19acb
Skillz <gankstarsleague@gmail.com>
2020-02-10 13:34:42 -0500

    begin guild structure

--- constants/discord.ts
+++ constants/discord.ts
@@ -6,4 +6,7 @@ export const baseEndpoints = {
 
 export const endpoints = {
 	GATEWAY_BOT: `${baseEndpoints.BASE_URL}/gateway/bot`,
+	GUILD_BANNER: (id: string, icon: string) => `${baseEndpoints.CDN_URL}/banners/${id}/${icon}`,
+	GUILD_ICON: (id: string, icon: string) => `${baseEndpoints.CDN_URL}/icons/${id}/${icon}`,
+	GUILD_SPLASH: (id: string, icon: string) => `${baseEndpoints.CDN_URL}/splashes/${id}/${icon}`
 }
