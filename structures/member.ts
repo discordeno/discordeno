c4edf071bc6a638c71a4c91d753266a6a0d90d5c
Skillz <gankstarsleague@gmail.com>
2020-03-11 10:49:55 -0400

    fix all todos for member structure permission checks

--- structures/member.ts
+++ structures/member.ts
@@ -1,11 +1,11 @@
-import Client from "../module/client.ts";
-import { endpoints } from "../constants/discord.ts";
-import { format_image_url } from "../utils/cdn.ts";
-import { Member_Create_Payload,
-  Edit_Member_Options } from "../types/member.ts";
-import { Image_Size, Image_Formats } from "../types/cdn.ts";
-import { Permission, Permissions } from "../types/permission.ts";
-import { Role_Data } from "../types/role.ts";
+import Client from "../module/client.ts"
+import { endpoints } from "../constants/discord.ts"
+import { format_image_url } from "../utils/cdn.ts"
+import { Member_Create_Payload, Edit_Member_Options } from "../types/member.ts"
+import { Image_Size, Image_Formats } from "../types/cdn.ts"
+import { Permission, Permissions } from "../types/permission.ts"
+import { Role_Data } from "../types/role.ts"
+import { member_has_permission, bot_has_permission } from "../utils/permissions.ts"
 
 export const create_member = (
   data: Member_Create_Payload,
@@ -25,8 +25,7 @@ export const create_member = (
   /** When the user joined the guild */
   joined_at: () => Date.parse(data.joined_at),
   /** When the user used their nitro boost on the server. */
-  premium_since:
-    () => (data.premium_since ? Date.parse(data.premium_since) : undefined),
+  premium_since: () => (data.premium_since ? Date.parse(data.premium_since) : undefined),
   /** Whether the user is deafened in voice channels */
   deaf: () => data.deaf,
   /** Whether the user is muted in voice channels */
@@ -38,80 +37,56 @@ export const create_member = (
   /** The full username#discriminator */
   tag: () => `${data.user.username}#${data.user.discriminator}`,
   /** The users custom avatar or the default avatar */
-  avatar_url: (
-    size: Image_Size = 128,
-    format?: Image_Formats
-  ) => data.user.avatar
-    ? format_image_url(
-      endpoints.USER_AVATAR(data.user.id, data.user.avatar),
-      size,
-      format
-    )
-    : endpoints.USER_DEFAULT_AVATAR(Number(data.user.discriminator) % 5),
+  avatar_url: (size: Image_Size = 128, format?: Image_Formats) =>
+    data.user.avatar
+      ? format_image_url(endpoints.USER_AVATAR(data.user.id, data.user.avatar), size, format)
+      : endpoints.USER_DEFAULT_AVATAR(Number(data.user.discriminator) % 5),
   /** The user mention with nickname if possible */
   mention: () => `<@!${data.user.id}>`,
   /** Whether the member is a bot */
   bot: () => data.user.bot,
   /** Add a role to the member */
   add_role: (role_id: string, reason?: string) => {
-    // TODO: check if the bot has MANAGE_ROLE and its highest role is above this one
-    return client.discordRequestManager.put(
-      endpoints.GUILD_MEMBER_ROLE(guild_id, data.user.id, role_id),
-      { reason }
-    );
+    // TODO: check if the bots highest role is above this one
+    if (!bot_has_permission(guild_id, client.bot_id, [Permissions.MANAGE_ROLES])) throw 'MISSING_MANAGE_ROLES'
+    return client.discordRequestManager.put(endpoints.GUILD_MEMBER_ROLE(guild_id, data.user.id, role_id), { reason })
   },
   /** Remove a role from the member */
   remove_role: (role_id: string, reason?: string) => {
-    // TODO: check if the bot has MANAGE_ROLE permissions and its highest role is above this role
-    return client.discordRequestManager.delete(
-      endpoints.GUILD_MEMBER_ROLE(guild_id, data.user.id, role_id),
-      { reason }
-    );
+    // TODO: check if the bots highest role is above this role
+    if (!bot_has_permission(guild_id, client.bot_id, [Permissions.MANAGE_ROLES])) throw 'MISSING_MANAGE_ROLES'
+    return client.discordRequestManager.delete(endpoints.GUILD_MEMBER_ROLE(guild_id, data.user.id, role_id), { reason })
   },
   /** Kick a member from the server */
   kick: (reason?: string) => {
-    // TODO: check if bot has KICK_MEMBER permissions
-    return client.discordRequestManager.delete(
-      endpoints.GUILD_MEMBER(guild_id, data.user.id),
-      { reason }
-    );
+    // TODO: Check if the bot is above the user so it is capable of kicking
+    if (!bot_has_permission(guild_id, client.bot_id, [Permissions.KICK_MEMBERS])) throw 'MISSING_KICK_MEMBERS'
+    return client.discordRequestManager.delete(endpoints.GUILD_MEMBER(guild_id, data.user.id), { reason })
   },
   /** Edit the member */
   edit: (options: Edit_Member_Options) => {
-    // TODO: check if has MANAGE_NICKNAME Permission
-    // TODO: check if it is a valid nickname like 32 characters
-    options.nick = undefined;
+    if (options.nick) {
+      if (options.nick.length > 32) throw "NICKNAMES_MAX_LENGTH"
+      if (!bot_has_permission(guild_id, client.bot_id, [Permissions.MANAGE_NICKNAMES])) throw "MISSING_MANAGE_NICKNAME"
+    }
 
-    // TODO: check if has MANAGE_ROLES permission
-    options.roles = undefined;
+    if (options.roles && !bot_has_permission(guild_id, client.bot_id, [Permissions.MANAGE_ROLES]))
+      throw "MISSING_MANAGE_ROLES"
 
-    // TODO: This should check if the member is in a voice channel
-    // TODO: CHeck if has MUTE_MEMBERS permission
-    options.mute = undefined;
-    // TODO: check if has DEAFEN_MEMBERS permission
-    options.deaf = undefined;
+    if (options.mute) {
+      // TODO: This should check if the member is in a voice channel
+      if (!bot_has_permission(guild_id, client.bot_id, [Permissions.MUTE_MEMBERS])) throw "MISSING_MUTE_MEMBERS"
+    }
+
+    if (options.deaf && !bot_has_permission(guild_id, client.bot_id, [Permissions.DEAFEN_MEMBERS]))
+      throw "MISSING_DEAFEN_MEMBERS"
+
     // TODO: if channel id is provided check if the bot has CONNECT and MOVE in channel and current channel
-    options.channel_id = undefined;
 
-    return client.discordRequestManager.patch(
-      endpoints.GUILD_MEMBER(guild_id, data.user.id),
-      options
-    );
+    return client.discordRequestManager.patch(endpoints.GUILD_MEMBER(guild_id, data.user.id), options)
   },
   /** Checks if the member has this permission. If the member is an owner or has admin perms it will always be true. */
-  has_permissions: (member_id: string, permissions: Permission[]) => {
-    if (member_id === owner_id) return true;
-
-    const permissionBits = role_data.filter(
-      role => data.roles.includes(role.id)
-    ).reduce((bits, data) => {
-      bits |= data.permissions;
-
-      return bits;
-    }, 0);
-
-    return permissions.every(
-      permission => permissionBits & Permissions[permission]
-    );
+  has_permissions: (permissions: Permission[]) => {
+    return member_has_permission(data.user.id, owner_id, role_data, data.roles, permissions)
   }
-});
+})
---
+++ utils/permissions.ts
@@ -0,0 +1,52 @@
+import { Permission, Permissions } from "../types/permission.ts"
+import { Role_Data } from "../types/role.ts"
+import { cache } from "./cache.ts"
+
+export const member_has_permission = (
+  member_id: string,
+  owner_id: string,
+  role_data: Role_Data[],
+  member_role_ids: string[],
+  permissions: Permission[]
+) => {
+  if (member_id === owner_id) return true
+
+  const permissionBits = role_data
+    .filter(role => member_role_ids.includes(role.id))
+    .reduce((bits, data) => {
+      bits |= data.permissions
+
+      return bits
+    }, 0)
+
+  if (permissionBits & Permissions.ADMINISTRATOR) return true
+
+  return permissions.every(permission => permissionBits & Permissions[permission])
+}
+
+export const bot_has_permission = (
+	guild_id: string,
+	bot_id: string,
+  permissions: Permissions[]
+) => {
+  const guild = cache.guilds.get(guild_id)
+	if (!guild) return false
+
+	const member = guild.members.get(bot_id)
+	if (!member) return false
+
+
+
+  const permissionBits = [...guild.roles().values()]
+    .map(role => role.raw())
+    .filter(role => member.roles().includes(role.id))
+    .reduce((bits, data) => {
+      bits |= data.permissions
+
+      return bits
+    }, 0)
+
+  if (permissionBits & Permissions.ADMINISTRATOR) return true
+
+  return permissions.every(permission => permissionBits & permission)
+}
