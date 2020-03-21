666684144e577cba209ca622b65371c018cd91fa
Skillz <gankstarsleague@gmail.com>
2020-03-21 12:52:11 -0400

    remove unused func

--- module/url.ts
+++
@@ -1,4 +0,0 @@
-// Terrible shim for Node's URL module. I miss Node.
-export function resolveURLs (...urlFragments: string[]) {
-    return urlFragments.map(x => x.replace(/^\/|\/$/, '')).join('/');
-}
