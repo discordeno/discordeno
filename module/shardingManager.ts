8f67c882dbf43b45272784b789672a8c1b36019a
Skillz <gankstarsleague@gmail.com>
2020-05-11 11:52:26 -0400

    debug logging

--- module/shardingManager.ts
+++ module/shardingManager.ts
@@ -62,6 +62,7 @@ function createShardWorker() {
   const path = new URL("./shard.ts", import.meta.url).toString();
   console.log("shard creating path", path);
   const shard = new Worker(path, { type: "module", deno: true });
+  console.log("shard made on manager");
   shard.onmessage = (message) => {
     if (message.data.type === "REQUEST_CLIENT_OPTIONS") {
       console.log("sending client options");
