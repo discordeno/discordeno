05c8d4530ab5ff8e41e4945c74976d83437acf68
Skillz <gankstarsleague@gmail.com>
2020-06-18 10:26:32 -0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Skillz4Killz/Discordeno.git master:gh-pages
+# git push -f git@github.com:Skillz4Killz/Discordeno.git master:gh-pages
 
 cd -
