81f3b6bacdb76e935189a9ee4a4f52aa228cad9f
Skillz <gankstarsleague@gmail.com>
2020-07-11 12:29:42 -0400

    use v7 as better errors

--- constants/discord.ts
+++ constants/discord.ts
@@ -1,6 +1,6 @@
 export const baseEndpoints = {
   /** Although, the version can be defaulted, keep the v6 as it can be changed to test newer versions when necessary. */
-  BASE_URL: "https://discord.com/api/v6",
+  BASE_URL: "https://discord.com/api/v7",
   CDN_URL: "https://cdn.discordapp.com",
 };
 
