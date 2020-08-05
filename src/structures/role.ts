d70ef64bc74a2043f9499bb3a8290af792e99d4b
Skillz <gankstarsleague@gmail.com>
2020-08-05 14:37:07 -0400

    remove storing strings instead of 1 number

--- src/structures/role.ts
+++ src/structures/role.ts
@@ -1,9 +1,7 @@
 import { RoleData } from "../types/role.ts";
-import { calculatePermissions } from "../utils/permissions.ts";
 
 export const createRole = (data: RoleData) => ({
   ...data,
-  permissions: calculatePermissions(BigInt(data.permissions_new)),
   /** The @ mention of the role in a string. */
   mention: `<@&${data.id}>`,
 });
