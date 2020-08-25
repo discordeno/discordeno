1297fb8476928a4fb4cfdf298fc9c91c5a7b5240
Skillz <gankstarsleague@gmail.com>
2020-08-25 17:07:40 -0400

    deletemess using channel.guildID

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -19,7 +19,7 @@ export async function deleteMessage(
   if (message.author.id !== botID) {
     // This needs to check the channels permission not the guild permission
     if (
-      !message.guildID ||
+      !message.channel.guildID ||
       !botHasChannelPermissions(
         message.channel.id,
         [Permissions.MANAGE_MESSAGES],
