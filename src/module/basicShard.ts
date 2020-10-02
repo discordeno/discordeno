c7ba6383256b01de96dcfd43b686c561b1d1b761
chroventer <ayyanlewis@gmail.com>
2020-10-02 23:08:00 +0400

    Typo; this is why we need unit tests

--- src/module/basicShard.ts
+++ src/module/basicShard.ts
@@ -109,7 +109,7 @@ export async function createBasicShard(
     }
 
     if (message instanceof Uint8Array) {
-      message = new TextDecoder().decode(inflate(message, 9));
+      message = new TextDecoder().decode(inflate(message, 0));
     }
 
     if (typeof message === "string") {
