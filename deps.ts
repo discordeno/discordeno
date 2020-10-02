d8f9e77cbf4df73e66e5d6e218cfb9d4af6abf60
chroventer <ayyanlewis@gmail.com>
2020-10-02 01:34:25 -0700

    Organize imports

--- deps.ts
+++ deps.ts
@@ -1,11 +1,10 @@
-export type { WebSocket } from "https://deno.land/std@0.67.0/ws/mod.ts";
-
-export { encode } from "https://deno.land/std@0.67.0/encoding/base64.ts";
 export { delay } from "https://deno.land/std@0.67.0/async/delay.ts";
-export { inflate } from "https://deno.land/x/zlib.es@v1.0.0/mod.ts";
+export { encode } from "https://deno.land/std@0.67.0/encoding/base64.ts";
 export {
   connectWebSocket,
   isWebSocketCloseEvent,
   isWebSocketPingEvent,
   isWebSocketPongEvent,
 } from "https://deno.land/std@0.67.0/ws/mod.ts";
+export type { WebSocket } from "https://deno.land/std@0.67.0/ws/mod.ts";
+export { inflate } from "https://deno.land/x/zlib.es@v1.0.0/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 import createClient from "./src/module/client.ts";
 
-export * from "./src/controllers/mod.ts";
 export * from "./src/controllers/bans.ts";
 export * from "./src/controllers/cache.ts";
 export * from "./src/controllers/channels.ts";
@@ -8,26 +7,23 @@ export * from "./src/controllers/guilds.ts";
 export * from "./src/controllers/members.ts";
 export * from "./src/controllers/messages.ts";
 export * from "./src/controllers/misc.ts";
+export * from "./src/controllers/mod.ts";
 export * from "./src/controllers/reactions.ts";
 export * from "./src/controllers/roles.ts";
-
-export * from "./src/module/client.ts";
-export * from "./src/module/requestManager.ts";
-export * from "./src/module/shardingManager.ts";
-
-export * from "./src/structures/mod.ts";
-export * from "./src/structures/channel.ts";
-export * from "./src/structures/guild.ts";
-export * from "./src/structures/member.ts";
-export * from "./src/structures/message.ts";
-export * from "./src/structures/role.ts";
-
 export * from "./src/handlers/channel.ts";
 export * from "./src/handlers/guild.ts";
 export * from "./src/handlers/member.ts";
 export * from "./src/handlers/message.ts";
 export * from "./src/handlers/webhook.ts";
-
+export * from "./src/module/client.ts";
+export * from "./src/module/requestManager.ts";
+export * from "./src/module/shardingManager.ts";
+export * from "./src/structures/channel.ts";
+export * from "./src/structures/guild.ts";
+export * from "./src/structures/member.ts";
+export * from "./src/structures/message.ts";
+export * from "./src/structures/mod.ts";
+export * from "./src/structures/role.ts";
 export * from "./src/types/activity.ts";
 export * from "./src/types/cdn.ts";
 export * from "./src/types/channel.ts";
@@ -41,7 +37,6 @@ export * from "./src/types/options.ts";
 export * from "./src/types/permission.ts";
 export * from "./src/types/presence.ts";
 export * from "./src/types/role.ts";
-
 export * from "./src/utils/cache.ts";
 export * from "./src/utils/cdn.ts";
 export * from "./src/utils/collection.ts";
