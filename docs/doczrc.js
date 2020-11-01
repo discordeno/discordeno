ebd0490baa361da6595e7711503adcd154062d53
ayntee <ayyantee@gmail.com>
2020-11-01 09:01:30 -0800

    Add react-helmet-async to package.json

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,5 +1,6 @@
 export default {
   title: "Discordeno",
+  src: "./src",
   menu: [
     "Home",
     "Introduction",
--- docs/src/gatsby-theme-docz/wrapper.js
+++ docs/src/gatsby-theme-docz/wrapper.js
@@ -2,21 +2,21 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 export default ({ children }) => {
-    return (
-      <>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <meta
-            property="og:title"
-            content={`${children.props.doc.value.name} | Discordeno`}
-          />
-          <meta
-            property="og:description"
-            content="This guide will help you learn every part of Discordeno, Advanced to create your own bot as you wish."
-          />
-          <meta property="og:url" content="https://discordeno.netlify.com" />
-        </Helmet>
-        {children}
-      </>
-    );
-}
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta
+          property="og:title"
+          content={`${children.props.doc.value.name} | Discordeno`}
+        />
+        <meta
+          property="og:description"
+          content="This guide will help you learn every part of Discordeno, Advanced to create your own bot as you wish."
+        />
+        <meta property="og:url" content="https://discordeno.netlify.com" />
+      </Helmet>
+      {children}
+    </>
+  );
+};
