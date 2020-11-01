f810013c1b1f9dbb9298c1cabfad3dba41ac0904
ayntee <ayyantee@gmail.com>
2020-11-01 03:38:36 -0800

    Docz 🎉🎉🎉

---
+++ docs/doczrc.js
@@ -0,0 +1,23 @@
+export default {
+  title: "Discordeno",
+  menu: [
+    "Home",
+    "Introduction",
+    "FAQ",
+    "Getting Started",
+    {
+      name: "Step By Step",
+      menu: [
+        "Introduction",
+        "Creating Bot",
+        "Creating Commands",
+        "Creating Events",
+        "Creating Inhibitors",
+        "Creating Languages",
+        "Creating Monitors",
+        "Creating Tasks"
+      ]
+    },
+    { name: "Advanced", menu: ["Introduction"] }
+  ]
+};
