1fa5d08370b5b529db0b39a469674891d79bb945
Skillz <gankstarsleague@gmail.com>
2020-11-12 08:32:32 -0500

    editBotProfile

--- src/constants/discord.ts
+++ src/constants/discord.ts
@@ -96,6 +96,7 @@ export const endpoints = {
 
   // User endpoints
   USER: (id: string) => `${baseEndpoints.BASE_URL}/users/${id}`,
+  USER_BOT: `${baseEndpoints.BASE_URL}/users/@me`,
   USER_AVATAR: (id: string, icon: string) =>
     `${baseEndpoints.CDN_URL}/avatars/${id}/${icon}`,
   USER_DEFAULT_AVATAR: (icon: number) =>
