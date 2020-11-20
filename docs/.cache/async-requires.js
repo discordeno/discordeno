556ec8eaaca276692388ab3ade0eadcf677c0810
ayntee <ayyantee@gmail.com>
2020-11-20 13:11:19 +0400

    docs: ignore public/ & .cache/ dir

--- docs/.cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-templates-docs-js": () => import("./../../src/templates/docs.js" /* webpackChunkName: "component---src-templates-docs-js" */)
-}
-
