5e28dcd2cbe76a10613f621b1df3bb4ddc973079
ayntee <ayyantee@gmail.com>
2020-11-22 18:43:48 +0400

    chore(deps): remove std/ws

--- deps.ts
+++ deps.ts
@@ -1,10 +1,3 @@
-export {
-  connectWebSocket,
-  isWebSocketCloseEvent,
-  isWebSocketPingEvent,
-  isWebSocketPongEvent,
-} from "https://deno.land/std@0.67.0/ws/mod.ts";
-export type { WebSocket } from "https://deno.land/std@0.67.0/ws/mod.ts";
 export { delay } from "https://deno.land/std@0.75.0/async/delay.ts";
 export { encode } from "https://deno.land/std@0.75.0/encoding/base64.ts";
 export {
