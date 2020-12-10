64372749587fc66c95dc9b63edde872bd09e1906
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-12-10 10:27:17 -0500

    fix: members intent fetching (#229)
    * fixes fetchMembers #223
    
    * Update guild.ts
    
    Co-authored-by: Ayyan <ayyantee@gmail.com>

--- src/handlers/guild.ts
+++ src/handlers/guild.ts
@@ -417,9 +417,25 @@ export async function pruneMembers(guildID: string, options: PruneOptions) {
   );
 }
 
+/** 
+ * ⚠️ BEGINNER DEVS!! YOU SHOULD ALMOST NEVER NEED THIS AND YOU CAN GET FROM cache.members.get()
+ * 
+ * ADVANCED:
+ * Highly recommended to use this function to fetch members instead of getMember from REST.
+ * REST: 50/s global(across all shards) rate limit with ALL requests this included
+ * GW(this function): 120/m(PER shard) rate limit. Meaning if you have 8 shards your limit is now 960/m.
+*/
 export function fetchMembers(guild: Guild, options?: FetchMembersOptions) {
-  if (!(identifyPayload.intents & Intents.GUILD_MEMBERS)) {
+  // You can request 1 member without the intent
+  if (
+    (!options?.limit || options.limit > 1) &&
+    !(identifyPayload.intents & Intents.GUILD_MEMBERS)
+  ) {
     throw new Error(Errors.MISSING_INTENT_GUILD_MEMBERS);
+  }
+
+  if (options?.userIDs?.length) {
+    options.limit = options.userIDs.length;
   }
 
   return new Promise((resolve) => {
