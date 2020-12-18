92eca9b38a790c64e00440e13f83f4c9a63c6ec2
Ayyan <ayyantee@gmail.com>
2020-12-18 13:22:10 +0400

    Update deps.ts

--- src/interactions/deps.ts
+++ src/interactions/deps.ts
@@ -1,4 +1,3 @@
 export { serve } from "https://deno.land/std@0.81.0/http/server.ts";
 export { verify } from "https://esm.sh/@evan/wasm@0.0.18/target/ed25519/deno.js";
 export { sign_detached_verify } from "https://raw.githubusercontent.com/dr-useless/tweetnacl-deno/master/src/nacl.ts";
-export { encode } from "https://deno.land/std@0.81.0/encoding/hex.ts";
