c293e3f0faf4fd1d1941fc38dee9d08da8768e1f
ITOH <72305210+itohatweb@users.noreply.github.com>
2020-12-27 17:46:06 +0100

    add: emoji types

---
+++ src/types/emoji.ts
@@ -0,0 +1,38 @@
+/** https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure */
+export interface EmojiPayload {
+    /** emoji id */
+    id: string | null;
+    /** emoji name */
+    name: string | null;
+    /** roles this emoji is whitelisted to */
+    roles?: string[];
+    /** user that created this emoji */
+    user?: UserPayload;
+    /** whether this emoji must be wrapped in colons */
+    require_colons?: boolean;
+    /** whether this emoji is managed */
+    managed?: boolean;
+    /** whether this emoji is animated */
+    animated?: boolean;
+    /** whether this emoji can be used, may be false due to loss of Server Boosts */
+    available?: boolean;
+  }
+  
+  /** https://discord.com/developers/docs/resources/emoji#create-guild-emoji */
+  export interface CreateGuildEmojiParams {
+    /** name of the emoji */
+    name: string;
+    /** the 128x128 emoji image (Data URI scheme) */
+    image: string;
+    /** roles for which this emoji will be whitelisted */
+    roles: string[];
+  }
+  
+  /** https://discord.com/developers/docs/resources/emoji#modify-guild-emoji */
+  export interface ModifyGuildEmojiParams {
+    /** name of the emoji */
+    name: string;
+    /** roles to which this emoji will be whitelisted */
+    roles: string[] | null;
+  }
+  
