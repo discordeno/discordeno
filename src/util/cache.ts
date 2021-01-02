ea172af8c61f79ca397ba8c983eda1358e61d0f8
OSCAR-WOS <theoscar@protonmail.com>
2021-01-02 17:21:44 +0000

    Ready

--- src/util/cache.ts
+++ src/util/cache.ts
@@ -9,6 +9,7 @@ import { Collection } from "./collection.ts";
 
 export interface CacheData {
   isReady: boolean;
+  shardedGuilds: number;
   guilds: Collection<string, Guild>;
   channels: Collection<string, Channel>;
   messages: Collection<string, Message>;
@@ -21,6 +22,7 @@ export interface CacheData {
 
 export const cache: CacheData = {
   isReady: false,
+  shardedGuilds: 0,
   guilds: new Collection(),
   channels: new Collection(),
   messages: new Collection(),
