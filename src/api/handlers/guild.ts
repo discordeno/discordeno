e75b0176877b699f75c42e9de357b7dcd3faef02
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-07 18:12:23 +0100

    comment formatting error

--- src/api/handlers/guild.ts
+++ src/api/handlers/guild.ts
@@ -69,8 +69,7 @@ export async function createServer(options: CreateGuildOptions) {
   return structures.createGuild(guild, 0);
 }
 
-/** Delete a guild permanently. User must be owner. Returns 204 No Content on success. Fires a Guild Delete Gateway event.
- */
+/** Delete a guild permanently. User must be owner. Returns 204 No Content on success. Fires a Guild Delete Gateway event. */
 export function deleteServer(guildID: string) {
   return RequestManager.delete(endpoints.GUILD(guildID));
 }
