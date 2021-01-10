544e22fa2e3150a9e673766b83a23728ef234851
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-10 12:54:48 +0100

    revert(interactions): remove redundant member resolution (#358)

--- src/api/controllers/interactions.ts
+++ src/api/controllers/interactions.ts
@@ -1,27 +1,19 @@
 import { eventHandlers } from "../../bot.ts";
 import {
   GatewayPayload,
-  GuildMemberPayload,
   Interaction,
   MessageApplicationPayload,
 } from "../../types/mod.ts";
-import { camelKeysToSnakeCase } from "../../util/utils.ts";
-import { structures } from "../structures/mod.ts";
 
-export async function handleInternalInteractionCreate(data: GatewayPayload) {
+export function handleInternalInteractionCreate(data: GatewayPayload) {
   if (data.t !== "INTERACTION_CREATE") return;
 
   const payload = data.d as Interaction;
-  const member: GuildMemberPayload | undefined = camelKeysToSnakeCase(
-    (await structures.createMember(payload.member, payload.guild_id))
-      .guildMember(payload.guild_id),
-  );
-  if (!member) return;
 
   eventHandlers.interactionCreate?.(
     {
       ...payload,
-      member: member,
+      member: payload.member,
     },
   );
 }
