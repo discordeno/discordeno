a852624415d5184db25a56fda5fdda5650058929
Skillz <gankstarsleague@gmail.com>
2021-01-14 12:47:52 -0500

    remove extra log

--- src/rest/request_manager.ts
+++ src/rest/request_manager.ts
@@ -310,7 +310,6 @@ function runMethod(
 async function logErrors(response: Response, errorStack?: unknown) {
   try {
     const error = await response.json();
-    console.error(error);
     console.error(Deno.inspect(error, { depth: 10 }));
 
     eventHandlers.debug?.({ type: "error", data: { errorStack, error } });
