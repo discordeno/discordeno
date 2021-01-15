ea913613d83211a7ac878e4763cfb7d4e4c87573
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-15 15:50:35 +0100

    USER_DM

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -170,8 +170,7 @@ export const endpoints = {
     `${baseEndpoints.CDN_URL}/avatars/${userID}/${icon}`,
   USER_DEFAULT_AVATAR: (icon: number) =>
     `${baseEndpoints.CDN_URL}/embed/avatars/${icon}.png`,
-  USER_CREATE_DM: `${baseEndpoints.BASE_URL}/users/@me/channels`,
-  USER_GET_DMS: `${baseEndpoints.BASE_URL}/users/@me/channels`,
+  USER_DM: `${baseEndpoints.BASE_URL}/users/@me/channels`,
   USER_CONNECTIONS: `${baseEndpoints.BASE_URL}/users/@me/connections`,
   USER_NICK: (guildID: string) => `${GUILDS_BASE(guildID)}/members/@me/nick`,
 
