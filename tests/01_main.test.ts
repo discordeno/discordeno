834788e978eb962cd238ad4d105bd85b2a3d4d67
ayntee <ayyantee@gmail.com>
2021-01-15 15:00:38 +0400

    https://open.spotify.com/track/3ZWc3Bm8eUMRRJhSWKHHeV 🔥🔥

--- tests/01_main.test.ts
+++ tests/01_main.test.ts
@@ -6,6 +6,11 @@ if (!token) throw new Error("Token is not provided");
 startBot({
   token,
   intents: [Intents.GUILD_MESSAGES, Intents.GUILDS],
+  eventHandlers: {
+    debug(data) {
+      console.log(Deno.inspect(data, { depth: 1 }));
+    },
+  },
 });
 
 // Default options for tests
