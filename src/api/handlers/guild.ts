92cd926b1aabd8c4f6f418cc7446aa35a101c65d
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-19 22:14:23 +0100

    update comment

--- src/api/handlers/guild.ts
+++ src/api/handlers/guild.ts
@@ -595,7 +595,7 @@ export async function unban(guildID: string, id: string) {
   return RequestManager.delete(endpoints.GUILD_BAN(guildID, id));
 }
 
-/** Returns the guild preview object for the given id. If the user is not in the guild, then the guild must be Discoverable. */
+/** Returns the guild preview object for the given id. If the bot is not in the guild, then the guild must be Discoverable. */
 export function getGuildPreview(guildID: string) {
   // TODO: add check if guild is Discoverable?
   return RequestManager.get(endpoints.GUILD_PREVIEW(guildID));
