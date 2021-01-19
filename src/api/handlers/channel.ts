bcf39bf4ed54af2f78bedbdaf025677ef90f7080
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-19 18:03:53 +0100

    add period
    Co-authored-by: Ayyan <ayyantee@gmail.com>

--- src/api/handlers/channel.ts
+++ src/api/handlers/channel.ts
@@ -125,7 +125,7 @@ export async function getPins(channelID: string) {
 /** 
  * Trigger a typing indicator for the specified channel. Generally bots should **NOT** implement this route. 
  * However, if a bot is responding to a command and expects the computation to take a few seconds, 
- * this endpoint may be called to let the user know that the bot is processing their message
+ * this endpoint may be called to let the user know that the bot is processing their message.
  */
 export function triggerTypingIndicator(channelID: string) {
   return RequestManager.post(endpoints.CHANNEL_TYPING(channelID));
