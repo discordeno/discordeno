3991261652bdd17fda021b706a3ce21adccb54f6
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-20 12:06:23 +0100

    return a var

--- src/api/handlers/member.ts
+++ src/api/handlers/member.ts
@@ -285,8 +285,10 @@ export async function editBotNickname(
   const hasPerm = await botHasPermission(guildID, ["CHANGE_NICKNAME"]);
   if (!hasPerm) throw new Error(Errors.MISSING_CHANGE_NICKNAME);
 
-  return (await RequestManager.patch(
+  const newNickname = (await RequestManager.patch(
     endpoints.USER_NICK(guildID),
     { nick: nickname },
   )).nick as string;
+
+  return newNickname;
 }
