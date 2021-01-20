871277b0aeea68fb1b6236c1dd698d2556916de5
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-20 17:45:24 +0100

    feat(handlers): add getVoiceRegions

--- src/api/handlers/guild.ts
+++ src/api/handlers/guild.ts
@@ -731,8 +731,13 @@ export function leaveGuild(guildID: string) {
   return RequestManager.delete(endpoints.GUILD_LEAVE(guildID));
 }
 
+/** Returns an array of voice regions that can be used when creating servers. */
+export function getVoiceRegions() {
+  return RequestManager.get(endpoints.VOICE_REGIONS);
+}
+
 /** Returns a list of voice region objects for the guild. Unlike the similar /voice route, this returns VIP servers when the guild is VIP-enabled. */
-export function getVoiceRegions(guildID: string) {
+export function getGuildVoiceRegions(guildID: string) {
   return RequestManager.get(endpoints.GUILD_REGIONS(guildID));
 }
 
