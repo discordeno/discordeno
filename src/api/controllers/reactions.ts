f5b302b1f00d6f976163ca11c2fc625ccf9170f9
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-22 09:33:50 +0100

    fix(controllers): cache message reaction remove emoji

--- src/api/controllers/reactions.ts
+++ src/api/controllers/reactions.ts
@@ -130,8 +130,25 @@ export async function handleInternalMessageReactionRemoveAll(
   eventHandlers.reactionRemoveAll?.(data.d as BaseMessageReactionPayload);
 }
 
-export function handleInternalMessageReactionRemoveEmoji(data: DiscordPayload) {
+export async function handleInternalMessageReactionRemoveEmoji(
+  data: DiscordPayload,
+) {
   if (data.t !== "MESSAGE_REACTION_REMOVE_EMOJI") return;
+
+  const payload = data.d as MessageReactionRemoveEmojiPayload;
+  const message = await cacheHandlers.get("messages", payload.message_id);
+
+  if (message?.reactions) {
+    message.reactions = message.reactions?.filter(
+      (reaction) =>
+        !(
+          reaction.emoji.id === payload.emoji.id &&
+          reaction.emoji.name === payload.emoji.name
+        ),
+    );
+
+    await cacheHandlers.set("messages", payload.message_id, message);
+  }
 
   eventHandlers.reactionRemoveEmoji?.(
     data.d as MessageReactionRemoveEmojiPayload,
