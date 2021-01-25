8c580d50fe38e9e09b14b88d8dcb65518e6bd1c1
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-25 12:44:23 +0100

    fixe that

--- src/util/permissions.ts
+++ src/util/permissions.ts
@@ -61,13 +61,8 @@ export async function botHasPermission(
   // Check if the bot is the owner of the guild, if it is, returns true
   if (guild.ownerID === botID) return true;
 
-  const member = (await cacheHandlers.get("members", botID))?.guilds.get(
-    guildID,
-  );
-  if (!member) return false;
-
   // The everyone role is not in member.roles
-  const permissionBits = [...member.roles, guild.id]
+  const permissionBits = [...member.guilds.get(guildID)?.roles || [], guild.id]
     .map((id) => guild.roles.get(id)!)
     // Remove any edge case undefined
     .filter((r) => r)
