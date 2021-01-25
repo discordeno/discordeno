bce7682fd740df0b9cc04f6cac4f4fe83e5f7640
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-25 20:59:51 +0100

    Update gateway.ts

--- src/api/handlers/gateway.ts
+++ src/api/handlers/gateway.ts
@@ -4,7 +4,9 @@ import { endpoints } from "../../util/constants.ts";
 
 /** Get the bots Gateway metadata that can help during the operation of large or sharded bots. */
 export function getGatewayBot() {
-  return RequestManager.get(
+  const result = RequestManager.get(
     endpoints.GATEWAY_BOT,
   ) as Promise<DiscordBotGatewayData>;
+
+  return result;
 }
