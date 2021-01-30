476475ff876c2d61e5cab5d9e7319fc83c5cca5a
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-30 18:29:42 +0100

    Update roles.ts

--- src/api/controllers/roles.ts
+++ src/api/controllers/roles.ts
@@ -29,6 +29,9 @@ export async function handleInternalGuildRoleDelete(data: DiscordPayload) {
   if (!guild) return;
 
   const cachedRole = guild.roles.get(payload.role_id)!;
+  if (cachedRole) eventHandlers.roleDelete?.(guild, cachedRole);
+
+  guild.roles.delete(payload.role_id);
 
   // For bots without GUILD_MEMBERS member.roles is never updated breaking permissions checking.
   cacheHandlers.forEach("members", (member) => {
@@ -43,9 +46,6 @@ export async function handleInternalGuildRoleDelete(data: DiscordPayload) {
       cacheHandlers.set("members", member.id, member);
     });
   });
-
-  guild.roles.delete(payload.role_id);
-  eventHandlers.roleDelete?.(guild, cachedRole);
 }
 
 export async function handleInternalGuildRoleUpdate(data: DiscordPayload) {
