8ab42a6b95fd0a5d1c9cf8dc9d3742d428705c73
ayntee <ayyantee@gmail.com>
2021-02-01 12:27:13 +0400

    chore: update deps (#496)

--- src/interactions/deps.ts
+++ src/interactions/deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.81.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.85.0/http/server.ts";
 export { verify } from "https://unpkg.com/@evan/wasm@0.0.25/target/ed25519/deno.js";
