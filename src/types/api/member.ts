b58ee217441c0487e0ef012792f12a9d1d905df2
Skillz4Killz <admin@ezlgg.com>
2021-02-06 18:26:09 -0500

    feat: add base user interface

--- src/types/api/member.ts
+++ src/types/api/member.ts
@@ -1,5 +1,16 @@
+export interface DiscordBaseUser {
+  /** the user's id */
+  id: string;
+  /** the user's username, not unique across the platform */
+  username: string;
+  /** the user's 4-digit discord-tag */
+  discriminator: string;
+  /** the user's avatar hash */
+  avatar: string | null;
+}
+
 /** https://discord.com/developers/docs/resources/user#users-resource */
-export interface DiscordUser {
+export interface DiscordUser extends DiscordBaseUser {
   /** the user's id */
   id: string;
   /** the user's username, not unique across the platform */
