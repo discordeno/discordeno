3620d5b1790b830992c512cdcc0a755d11e5a9ad
Skillz4Killz <admin@ezlgg.com>
2021-02-06 18:08:00 -0500

    Add channel types

---
+++ src/types/api/channel.ts
@@ -0,0 +1,17 @@
+/** https://discord.com/developers/docs/resources/channel#channel-object-channel-types */
+export enum ChannelTypes {
+  /** a text channel within a server */
+  GUILD_TEXT,
+  /** a direct message between users */
+  DM,
+  /** a voice channel within a server */
+  GUILD_VOICE,
+  /** a direct message between multiple users */
+  GROUP_DM,
+  /** an organizational category that contains up to 50 channels */
+  GUILD_CATEGORY,
+  /** a channel that users can follow and crosspost into their own server */
+  GUILD_NEWS,
+  /** a channel in which game developers can sell their game on Discord */
+  GUILD_STORE,
+}
