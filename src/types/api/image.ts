29dde4f0a55edb27d44f587317fa9292397426b3
Skillz4Killz <admin@ezlgg.com>
2021-02-06 18:26:42 -0500

    feat: image typings

---
+++ src/types/api/image.ts
@@ -0,0 +1,3 @@
+/** https://discord.com/developers/docs/reference#image-formatting */
+export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048;
+export type ImageFormats = "jpg" | "jpeg" | "png" | "webp" | "gif";
