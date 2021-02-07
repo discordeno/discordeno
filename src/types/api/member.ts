536f289e2c9df64f93e742cbe4bc24eaa62cf3b2
Skillz4Killz <admin@ezlgg.com>
2021-02-06 19:15:23 -0500

    add more member types

--- src/types/api/member.ts
+++ src/types/api/member.ts
@@ -63,3 +63,27 @@ export enum DiscordPremiumTypes {
   NITRO_CLASSIC = 1,
   NITRO = 2,
 }
+
+/** https://discord.com/developers/docs/resources/guild#guild-member-object-guild-member-structure */
+export interface DiscordBaseMember {
+  /** this users guild nickname */
+  nick: string | null;
+  /** array of role payload ids */
+  roles: string[];
+  /** when the user joined the guild */
+  joined_at: string;
+  /** when the user started boosting the guild */
+  premium_since?: string | null;
+  /** whether the user is deafened in voice channels */
+  deaf: boolean;
+  /** whether the user is muted in voice channels */
+  mute: boolean;
+  /** whether the user has not yet passed the guild's Membership Screening requirements */
+  pending?: boolean;
+}
+
+/** https://discord.com/developers/docs/resources/guild#guild-member-object-guild-member-structure */
+export interface DiscordMember {
+  /** the user this guild member represents */
+  user?: DiscordUser;
+}
