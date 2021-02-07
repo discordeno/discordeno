d8b9b4c1e3580a2a9c7d2cf5777c41950f8134ea
Skillz4Killz <admin@ezlgg.com>
2021-02-06 19:15:43 -0500

    add more to mod.ts

--- src/types/api/mod.ts
+++ src/types/api/mod.ts
@@ -1,5 +1,6 @@
 export * from "./auditlog.ts";
 export * from "./channel.ts";
+export * from "./embed.ts";
 export * from "./emoji.ts";
 // export * from "./guild.ts";
 export * from "./image.ts";
@@ -7,6 +8,7 @@ export * from "./integration.ts";
 // export * from "./interaction.ts";
 // export * from "./invite.ts";
 export * from "./member.ts";
+export * from "./message.ts";
 // export * from "./oauth2.ts";
 // export * from "./options.ts";
 export * from "./permission.ts";
