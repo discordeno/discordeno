009b61ee35d9e702a8df31211bf628b08fb91741
ayntee <ayyantee@gmail.com>
2021-02-26 13:02:28 +0400

    test(core): close the websocket connection

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -13,6 +13,7 @@ import {
   cache,
   Channel,
   channelOverwriteHasPermission,
+  closeWS,
   createGuildChannel,
   createGuildRole,
   createServer,
@@ -348,8 +349,8 @@ Deno.test({
 
 // Forcefully exit the Deno process once all tests are done.
 Deno.test({
-  name: "exit the process forcefully after all the tests are done\n",
+  name: "close the websocket connection",
   fn() {
-    Deno.exit();
+    closeWS();
   },
 });
