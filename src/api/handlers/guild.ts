9043d1649c21a3372fe4c8a0948b1bf84192182a
ayntee <ayyantee@gmail.com>
2021-03-06 23:42:27 +0400

    Update src/api/handlers/guild.ts

--- src/api/handlers/guild.ts
+++ src/api/handlers/guild.ts
@@ -565,7 +565,7 @@ export async function pruneMembers(
 
   const result = await RequestManager.post(
     endpoints.GUILD_PRUNE(guildID),
-    { ...options, include_roles: roles.join(",") },
+    { ...options, include_roles: roles },
   );
 
   return result;
