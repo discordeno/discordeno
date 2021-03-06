f9f2eb89fb8e8a28f02fc0ce5b1aaff630b09941
ayntee <ayyantee@gmail.com>
2021-03-06 21:32:38 +0400

    feat(types/message): add fail_if_not_exists to Reference interface

--- src/types/message.ts
+++ src/types/message.ts
@@ -196,13 +196,20 @@ export interface Application {
   bot?: UserPayload;
 }
 
+/** https://discord.com/developers/docs/resources/channel#message-object-message-reference-structure */
 export interface Reference {
-  /** The id of the originating message */
-  "message_id"?: string;
-  /** The id of the originating message's channel */
-  "channel_id": string;
-  /** The id of the originating message's guild */
-  "guild_id"?: string;
+  /** ID of the originating message */
+  message_id?: string;
+  /**
+   * ID of the originating message's channel
+   * 
+   * NOTE: `channel_id` is optional when creating a reply, but will always be present when receiving an event/response that includes this data model
+   */
+  channel_id?: string;
+  /** ID of the originating message's guild */
+  guild_id?: string;
+  /** When sending, whether to error if the referenced message doesn't exist instead of sending as a normal (non-reply) message, default true */
+  fail_if_not_exists?: boolean;
 }
 
 export enum MessageFlags {
@@ -376,4 +383,20 @@ export enum MessageStickerFormat {
   PNG = 1,
   APNG,
   LOTTIE,
+}
+
+/** https://discord.com/developers/docs/resources/channel#message-object-message-reference-structure */
+export interface DiscordMessageReference {
+  /** ID of the originating message */
+  message_id?: string;
+  /**
+   * ID of the originating message's channel
+   * 
+   * NOTE: `channel_id` is optional when creating a reply, but will always be present when receiving an event/response that includes this data model
+   */
+  channel_id?: string;
+  /** ID of the originating message's guild */
+  guild_id?: string;
+  /** When sending, whether to error if the referenced message doesn't exist instead of sending as a normal (non-reply) message, default true */
+  fail_if_not_exists?: boolean;
 }
