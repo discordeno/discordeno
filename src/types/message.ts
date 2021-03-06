83bb1e81ed64046879aae5629b901edaba20d184
ayntee <ayyantee@gmail.com>
2021-03-06 21:33:25 +0400

    Update src/types/message.ts

--- src/types/message.ts
+++ src/types/message.ts
@@ -384,19 +384,3 @@ export enum MessageStickerFormat {
   APNG,
   LOTTIE,
 }
-
-/** https://discord.com/developers/docs/resources/channel#message-object-message-reference-structure */
-export interface DiscordMessageReference {
-  /** ID of the originating message */
-  message_id?: string;
-  /**
-   * ID of the originating message's channel
-   * 
-   * NOTE: `channel_id` is optional when creating a reply, but will always be present when receiving an event/response that includes this data model
-   */
-  channel_id?: string;
-  /** ID of the originating message's guild */
-  guild_id?: string;
-  /** When sending, whether to error if the referenced message doesn't exist instead of sending as a normal (non-reply) message, default true */
-  fail_if_not_exists?: boolean;
-}
