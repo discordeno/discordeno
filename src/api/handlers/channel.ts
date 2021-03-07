d0f6975f30a3d8eed1b96ea1b3fecfb86dcade13
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-07 22:52:41 +0100

    add perm check

--- src/api/handlers/channel.ts
+++ src/api/handlers/channel.ts
@@ -133,6 +133,30 @@ export async function getPins(channelID: string) {
  * this endpoint may be called to let the user know that the bot is processing their message.
  */
 export async function startTyping(channelID: string) {
+  const channel = await cacheHandlers.get("channels", channelID);
+  // If the channel is cached, we can do extra checks/safety
+  if (channel) {
+    if (
+      [
+        ChannelTypes.DM,
+        ChannelTypes.GUILD_NEWS,
+        ChannelTypes.GUILD_TEXT,
+      ].includes(channel.type)
+    ) {
+      throw new Error(Errors.CHANNEL_NOT_TEXT_BASED);
+    }
+
+    const hasSendMessagesPerm = await botHasChannelPermissions(
+      channelID,
+      ["SEND_MESSAGES"],
+    );
+    if (
+      !hasSendMessagesPerm
+    ) {
+      throw new Error(Errors.MISSING_SEND_MESSAGES);
+    }
+  }
+
   const result = await RequestManager.post(endpoints.CHANNEL_TYPING(channelID));
 
   return result;
