b511a0d39ff1f0126c969c7e14b95beb598c2e38
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-07 16:21:58 +0100

    fix(types/webhook): update interaction response types (#610)
    * add the changes from dc
    
    * Update src/types/webhook.ts
    
    * Update src/interactions/types/interactions.ts
    
    Co-authored-by: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
    Co-authored-by: ayntee <ayyantee@gmail.com>

--- src/types/webhook.ts
+++ src/types/webhook.ts
@@ -198,14 +198,10 @@ export interface SlashCommandCallbackData {
 export enum InteractionResponseType {
   /** ACK a `Ping` */
   PONG = 1,
-  /** ACK a command without sending a message, eating the user's input */
-  ACKNOWLEDGE = 2,
-  /** respond with a message, eating the user's input */
-  CHANNEL_MESSAGE = 3,
-  /** respond with a message, showing the user's input */
+  /** Respond with a message, showing the user's input */
   CHANNEL_MESSAGE_WITH_SOURCE = 4,
-  /** ACK a command without sending a message, showing the user's input */
-  ACK_WITH_SOURCE = 5,
+  /** ACK an interaction and edit to a response later, the user sees a loading state */
+  DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
 }
 
 // TODO: remove this interface for v11
