b20a8691db092fb66e048f0683b198825d2e8328
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-08 22:20:37 +0100

    fix ready event controller

--- src/api/controllers/misc.ts
+++ src/api/controllers/misc.ts
@@ -1,4 +1,9 @@
-import { eventHandlers, setApplicationID, setBotID } from "../../bot.ts";
+import {
+  eventHandlers,
+  lastShardID,
+  setApplicationID,
+  setBotID,
+} from "../../bot.ts";
 import {
   DiscordPayload,
   IntegrationCreateUpdateEvent,
@@ -19,6 +24,7 @@ import { initialMemberLoadQueue } from "../structures/guild.ts";
 import { structures } from "../structures/mod.ts";
 import { cacheHandlers } from "./cache.ts";
 
+const loadingShards = new Set<number>();
 /** This function is the internal handler for the ready event. Users can override this with controllers if desired. */
 export async function handleInternalReady(
   data: DiscordPayload,
@@ -32,46 +38,77 @@ export async function handleInternalReady(
 
   // Triggered on each shard
   eventHandlers.shardReady?.(shardID);
-  if (payload.shard && shardID === payload.shard[1] - 1) {
-    const loadedAllGuilds = async () => {
-      const guildsMissing = async () => {
-        for (const g of payload.guilds) {
-          if (!(await cacheHandlers.has("guilds", g.id))) return true;
-        }
-        return false;
-      };
+  const now = Date.now();
 
-      if (await guildsMissing()) {
-        setTimeout(loadedAllGuilds, 2000);
-      } else {
-        // The bot has already started, the last shard is resumed, however.
-        if (cache.isReady) return;
-
-        cache.isReady = true;
-        eventHandlers.ready?.();
-
-        // All the members that came in on guild creates should now be processed 1 by 1
-        for (const [guildID, members] of initialMemberLoadQueue.entries()) {
-          await Promise.all(
-            members.map(async (member) => {
-              const memberStruct = await structures.createMemberStruct(
-                member,
-                guildID,
-              );
-
-              return cacheHandlers.set(
-                "members",
-                memberStruct.id,
-                memberStruct,
-              );
-            }),
-          );
+  loadingShards.add(shardID);
+  async function checkReady() {
+    async function guildsMissing() {
+      const missingGuildIDs = new Set<string>();
+      for (const g of payload.guilds) {
+        if (!(await cacheHandlers.has("guilds", g.id))) {
+          missingGuildIDs.add(g.id);
         }
       }
-    };
 
-    setTimeout(loadedAllGuilds, 2000);
+      if (missingGuildIDs.size) {
+        payload.guilds = payload.guilds.filter((unavailableGuild) =>
+          !missingGuildIDs.has(unavailableGuild.id)
+        );
+
+        return true;
+      }
+
+      return false;
+    }
+
+    async function loaded() {
+      loadingShards.delete(shardID);
+
+      // The bot has already started, the last shard is resumed, however.
+      if (cache.isReady) return;
+
+      if (shardID === lastShardID - 1) {
+        if (loadingShards.size) {
+          setTimeout(loaded, 2000);
+        } else {
+          cache.isReady = true;
+          eventHandlers.ready?.();
+
+          // All the members that came in on guild creates should now be processed 1 by 1
+          for (const [guildID, members] of initialMemberLoadQueue.entries()) {
+            await Promise.allSettled(
+              members.map(async (member) => {
+                const memberStruct = await structures.createMemberStruct(
+                  member,
+                  guildID,
+                );
+
+                return cacheHandlers.set(
+                  "members",
+                  memberStruct.id,
+                  memberStruct,
+                );
+              }),
+            );
+          }
+        }
+      }
+    }
+
+    if (await guildsMissing()) {
+      if (Date.now() - now > 10000) {
+        loadingShards.delete(shardID);
+        eventHandlers.shardFailedToLoad?.(shardID, payload.guilds);
+        loaded();
+      } else {
+        setTimeout(checkReady, 2000);
+      }
+    } else {
+      loaded();
+    }
   }
+
+  setTimeout(checkReady, 2000);
 
   // Wait 5 seconds to spawn next shard
   await delay(5000);
