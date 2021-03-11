e9697e341d4f4d993f293d47ba54c12132a05dbc
ayntee <ayyantee@gmail.com>
2021-03-11 18:55:23 +0400

    messages

---
+++ src/api/controllers/MESSAGE_DELETE.ts
@@ -0,0 +1,16 @@
+import { eventHandlers } from "../../bot.ts";
+import { DiscordPayload, MessageDeletePayload } from "../../types/mod.ts";
+import { cacheHandlers } from "./cache.ts";
+
+export async function handleMessageDelete(data: DiscordPayload) {
+  const payload = data.d as MessageDeletePayload;
+  const channel = await cacheHandlers.get("channels", payload.channel_id);
+  if (!channel) return;
+
+  eventHandlers.messageDelete?.(
+    { id: payload.id, channel },
+    await cacheHandlers.get("messages", payload.id),
+  );
+
+  await cacheHandlers.delete("messages", payload.id);
+}
---
+++ src/api/controllers/MESSAGE_UPDATE.ts
@@ -0,0 +1,36 @@
+import { eventHandlers } from "../../bot.ts";
+import { DiscordPayload, MessageCreateOptions } from "../../types/mod.ts";
+import { structures } from "../structures/mod.ts";
+import { cacheHandlers } from "./cache.ts";
+
+export async function handleMessageUpdate(data: DiscordPayload) {
+  const payload = data.d as MessageCreateOptions;
+  const channel = await cacheHandlers.get("channels", payload.channel_id);
+  if (!channel) return;
+
+  const cachedMessage = await cacheHandlers.get("messages", payload.id);
+  if (!cachedMessage) return;
+
+  const oldMessage = {
+    attachments: cachedMessage.attachments,
+    content: cachedMessage.content,
+    embeds: cachedMessage.embeds,
+    editedTimestamp: cachedMessage.editedTimestamp,
+    tts: cachedMessage.tts,
+    pinned: cachedMessage.pinned,
+  };
+
+  // Messages with embeds can trigger update but they wont have edited_timestamp
+  if (
+    !payload.edited_timestamp ||
+    (cachedMessage.content === payload.content)
+  ) {
+    return;
+  }
+
+  const message = await structures.createMessageStruct(payload);
+
+  await cacheHandlers.set("messages", payload.id, message);
+
+  eventHandlers.messageUpdate?.(message, oldMessage);
+}
