2de5c5b8cd4be613e50643c9a3a5c583751a7685
Skillz <gankstarsleague@gmail.com>
2021-03-11 13:22:53 -0500

    1 source for deleting messages

--- src/api/handlers/message/delete_message.ts
+++ src/api/handlers/message/delete_message.ts
@@ -1,32 +1,35 @@
 import { botID } from "../../../bot.ts";
 import { RequestManager } from "../../../rest/request_manager.ts";
 import { endpoints } from "../../../util/constants.ts";
-import { botHasChannelPermissions } from "../../../util/permissions.ts";
 import { delay } from "../../../util/utils.ts";
 
-/** Delete a message */
+/** Delete a message with the channel id and message id only. */
 export async function deleteMessage(
-  message: Message,
+  channelID: string,
+  messageID: string,
   reason?: string,
   delayMilliseconds = 0,
 ) {
-  if (message.author.id !== botID) {
-    // This needs to check the channels permission not the guild permission
-    const hasManageMessages = await botHasChannelPermissions(
-      message.channelID,
-      ["MANAGE_MESSAGES"],
-    );
-    if (
-      !hasManageMessages
-    ) {
-      throw new Error(Errors.MISSING_MANAGE_MESSAGES);
+  const message = await cacheHandlers.get("messages", messageID);
+  if (message) {
+    if (message.author.id !== botID) {
+      // This needs to check the channels permission not the guild permission
+      const hasManageMessages = await botHasChannelPermissions(
+        message.channelID,
+        ["MANAGE_MESSAGES"],
+      );
+      if (
+        !hasManageMessages
+      ) {
+        throw new Error(Errors.MISSING_MANAGE_MESSAGES);
+      }
     }
   }
 
   if (delayMilliseconds) await delay(delayMilliseconds);
 
   const result = await RequestManager.delete(
-    endpoints.CHANNEL_MESSAGE(message.channelID, message.id),
+    endpoints.CHANNEL_MESSAGE(channelID, messageID),
     { reason },
   );
 
