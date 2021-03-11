1fd04da6c6fda30e953c7eb422a02613a9835fea
ayntee <ayyantee@gmail.com>
2021-03-11 17:41:34 +0400

    refactor(controllers): separate controller into files

---
+++ src/api/controllers/CHANNEL_DELETE.ts
@@ -0,0 +1,40 @@
+import { eventHandlers } from "../../bot.ts";
+import {
+  ChannelCreatePayload,
+  ChannelTypes,
+  DiscordPayload,
+} from "../../types/mod.ts";
+import { cacheHandlers } from "./cache.ts";
+
+export async function handleInternalChannelDelete(data: DiscordPayload) {
+  const payload = data.d as ChannelCreatePayload;
+
+  const cachedChannel = await cacheHandlers.get("channels", payload.id);
+  if (!cachedChannel) return;
+
+  if (cachedChannel.type === ChannelTypes.GUILD_VOICE && payload.guild_id) {
+    const guild = await cacheHandlers.get("guilds", payload.guild_id);
+
+    if (guild) {
+      return Promise.all(guild.voiceStates.map(async (vs, key) => {
+        if (vs.channelID !== payload.id) return;
+
+        // Since this channel was deleted all voice states for this channel should be deleted
+        guild.voiceStates.delete(key);
+
+        const member = await cacheHandlers.get("members", vs.userID);
+        if (!member) return;
+
+        eventHandlers.voiceChannelLeave?.(member, vs.channelID);
+      }));
+    }
+  }
+
+  await cacheHandlers.delete("channels", payload.id);
+  cacheHandlers.forEach("messages", (message) => {
+    if (message.channelID === payload.id) {
+      cacheHandlers.delete("messages", message.id);
+    }
+  });
+  eventHandlers.channelDelete?.(cachedChannel);
+}
---
+++ src/api/controllers/GUILD_BAN_REMOVE.ts
@@ -0,0 +1,12 @@
+import { eventHandlers } from "../../bot.ts";
+import { DiscordPayload, GuildBanPayload } from "../../types/mod.ts";
+import { cacheHandlers } from "./cache.ts";
+
+export async function handleInternalGuildBanRemove(data: DiscordPayload) {
+  const payload = data.d as GuildBanPayload;
+  const guild = await cacheHandlers.get("guilds", payload.guild_id);
+  if (!guild) return;
+
+  const member = await cacheHandlers.get("members", payload.user.id);
+  eventHandlers.guildBanRemove?.(guild, payload.user, member);
+}
