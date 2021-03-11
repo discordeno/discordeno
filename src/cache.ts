c4f8b2586dbdaa217c6948394c32364d41dd8600
ayntee <ayyantee@gmail.com>
2021-03-11 20:15:03 +0400

    done

--- src/cache.ts
+++ src/cache.ts
@@ -1,9 +1,9 @@
 // deno-lint-ignore-file require-await no-explicit-any prefer-const
 
-import { PresenceUpdatePayload } from "../../types/mod.ts";
-import { cache } from "../../util/cache.ts";
-import { Collection } from "../../util/collection.ts";
-import { Channel, Guild, Member, Message } from "../structures/mod.ts";
+import { PresenceUpdatePayload } from "./types/mod.ts";
+import { cache } from "./util/cache.ts";
+import { Collection } from "./util/collection.ts";
+import type { Channel, Guild, Member, Message } from "./structures/mod.ts";
 
 export type TableName =
   | "guilds"
