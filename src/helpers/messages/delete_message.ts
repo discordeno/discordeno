201d9fd2f426dcfa5e0ee7be9aa56fb2ada31c95
ayntee <ayyantee@gmail.com>
2021-03-13 08:59:16 -0500

    changes

--- src/helpers/messages/delete_message.ts
+++ src/helpers/messages/delete_message.ts
@@ -1,28 +1,27 @@
 import { botID } from "../../bot.ts";
 import { cacheHandlers } from "../../cache.ts";
 import { RequestManager } from "../../rest/request_manager.ts";
-import { Errors } from "../../types/errors.ts";
 import { endpoints } from "../../util/constants.ts";
 import { requireBotChannelPermissions } from "../../util/permissions.ts";
 import { delay } from "../../util/utils.ts";
 
 /** Delete a message with the channel id and message id only. */
 export async function deleteMessage(
+  channelID: string,
   messageID: string,
   reason?: string,
   delayMilliseconds = 0,
 ) {
   const message = await cacheHandlers.get("messages", messageID);
-  if (!message) throw new Error(Errors.MESSAGE_NOT_FOUND);
 
-  if (message.author.id !== botID) {
+  if (message && message.author.id !== botID) {
     await requireBotChannelPermissions(message.channelID, ["MANAGE_MESSAGES"]);
   }
 
   if (delayMilliseconds) await delay(delayMilliseconds);
 
   const result = await RequestManager.delete(
-    endpoints.CHANNEL_MESSAGE(message.channelID, messageID),
+    endpoints.CHANNEL_MESSAGE(channelID, messageID),
     { reason },
   );
 
