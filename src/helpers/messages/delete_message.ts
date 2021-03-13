c5dc09e3bc9ca586cf755f51d038e346177a39f7
ayntee <ayyantee@gmail.com>
2021-03-13 08:42:34 -0500

    refactor(helpers): rename & replace deleteMessageByID() with deleteMessage()

--- src/helpers/messages/delete_message.ts
+++ src/helpers/messages/delete_message.ts
@@ -1,25 +1,28 @@
 import { botID } from "../../bot.ts";
+import { cacheHandlers } from "../../cache.ts";
 import { RequestManager } from "../../rest/request_manager.ts";
-import { Message } from "../../structures/mod.ts";
+import { Errors } from "../../types/errors.ts";
 import { endpoints } from "../../util/constants.ts";
 import { requireBotChannelPermissions } from "../../util/permissions.ts";
 import { delay } from "../../util/utils.ts";
 
-/** Delete a message */
+/** Delete a message with the channel id and message id only. */
 export async function deleteMessage(
-  message: Message,
+  messageID: string,
   reason?: string,
   delayMilliseconds = 0,
 ) {
+  const message = await cacheHandlers.get("messages", messageID);
+  if (!message) throw new Error(Errors.MESSAGE_NOT_FOUND);
+
   if (message.author.id !== botID) {
-    // This needs to check the channels permission not the guild permission
     await requireBotChannelPermissions(message.channelID, ["MANAGE_MESSAGES"]);
   }
 
   if (delayMilliseconds) await delay(delayMilliseconds);
 
   const result = await RequestManager.delete(
-    endpoints.CHANNEL_MESSAGE(message.channelID, message.id),
+    endpoints.CHANNEL_MESSAGE(message.channelID, messageID),
     { reason },
   );
 
