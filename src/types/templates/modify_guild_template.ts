139d74f173fd417fcc3f1c25e8c7262a38d3644e
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 23:14:48 +0200

    types: add ModifyGuildTemplate type

---
+++ src/types/templates/modify_guild_template.ts
@@ -0,0 +1,6 @@
+export interface ModifyGuildTemplate {
+  /** Name of the template (1-100 characters) */
+  name?: string;
+  /** Description of the template (0-120 characters) */
+  description?: string | null;
+}
