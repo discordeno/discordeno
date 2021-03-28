1dff8ef1a6fc81ec963532c2c4c43f7887c256ef
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 11:42:17 +0200

    add the old one back

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,26 +1,117 @@
-export type CamelCase<T extends PropertyKey> = T extends string
-  ? string extends T ? string
-  : T extends `${infer F}_${infer R}` ? `${F}${Capitalize<CamelCase<R>>}`
-  : T
-  : T;
+type UpperCaseCharacters =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
+
+type WordSeparators = "-" | "_" | " ";
+
+type SplitIncludingDelimiters<
+  Source extends string,
+  Delimiter extends string,
+> = Source extends "" ? []
+  : Source extends `${infer FirstPart}${Delimiter}${infer SecondPart}` ? (
+    Source extends `${FirstPart}${infer UsedDelimiter}${SecondPart}`
+      ? UsedDelimiter extends Delimiter
+        ? Source extends `${infer FirstPart}${UsedDelimiter}${infer SecondPart}`
+          ? [
+            ...SplitIncludingDelimiters<FirstPart, Delimiter>,
+            UsedDelimiter,
+            ...SplitIncludingDelimiters<SecondPart, Delimiter>,
+          ]
+        : never
+      : never
+      : never
+  )
+  : [Source];
+type StringPartToDelimiterCase<
+  StringPart extends string,
+  UsedWordSeparators extends string,
+  UsedUpperCaseCharacters extends string,
+  Delimiter extends string,
+> = StringPart extends UsedWordSeparators ? Delimiter
+  : StringPart extends UsedUpperCaseCharacters
+    ? `${Delimiter}${Lowercase<StringPart>}`
+  : StringPart;
+type StringArrayToDelimiterCase<
+  Parts extends any[],
+  UsedWordSeparators extends string,
+  UsedUpperCaseCharacters extends string,
+  Delimiter extends string,
+> = Parts extends [`${infer FirstPart}`, ...infer RemainingParts]
+  ? `${StringPartToDelimiterCase<
+    FirstPart,
+    UsedWordSeparators,
+    UsedUpperCaseCharacters,
+    Delimiter
+  >}${StringArrayToDelimiterCase<
+    RemainingParts,
+    UsedWordSeparators,
+    UsedUpperCaseCharacters,
+    Delimiter
+  >}`
+  : "";
+type DelimiterCase<Value, Delimiter extends string> = Value extends string
+  ? StringArrayToDelimiterCase<
+    SplitIncludingDelimiters<Value, WordSeparators | UpperCaseCharacters>,
+    WordSeparators,
+    UpperCaseCharacters,
+    Delimiter
+  >
+  : Value;
+type InnerCamelCaseStringArray<Parts extends any[], PreviousPart> =
+  Parts extends [`${infer FirstPart}`, ...infer RemainingParts]
+    ? FirstPart extends undefined ? ""
+    : FirstPart extends ""
+      ? InnerCamelCaseStringArray<RemainingParts, PreviousPart>
+    : `${PreviousPart extends "" ? FirstPart
+      : Capitalize<FirstPart>}${InnerCamelCaseStringArray<
+      RemainingParts,
+      FirstPart
+    >}`
+    : "";
+type CamelCaseStringArray<Parts extends string[]> = Parts extends
+  [`${infer FirstPart}`, ...infer RemainingParts] ? Uncapitalize<
+  `${FirstPart}${InnerCamelCaseStringArray<RemainingParts, FirstPart>}`
+>
+  : never;
+type Split<S extends string, D extends string> = string extends S ? string[]
+  : S extends "" ? []
+  : S extends `${infer T}${D}${infer U}` ? [T, ...Split<U, D>]
+  : [S];
+
+export type SnakeCase<Value> = DelimiterCase<Value, "_">;
+
+export type CamelCase<K> = K extends string
+  ? CamelCaseStringArray<Split<K, WordSeparators>>
+  : K;
 
 export type CamelCaseProps<T> = {
   [K in keyof T as CamelCase<K>]: CamelCaseProps<T[K]>;
 };
 
-export type SnakeCase<T extends PropertyKey> = string extends T ? string
-  : T extends `${infer F}${infer U}${infer R}`
-    ? `${F extends Uppercase<F> ? "_" : ""}${Lowercase<F>}${U extends
-      Uppercase<U> ? "_" : ""}${Lowercase<U>}${SnakeCase<R>}`
-  : T extends `${infer F}${infer R}`
-    ? `${F extends Uppercase<F> ? "_" : ""}${Lowercase<F>}${SnakeCase<R>}`
-  : "";
-
-export type SnakeCaseProps<T> = T extends readonly any[]
-  ? { [K in keyof T]: SnakeCaseProps<T[K]> }
-  : T extends object ? {
-    [K in keyof T as SnakeCase<Extract<K, string>>]: SnakeCaseProps<
-      T[K]
-    >;
-  }
-  : T;
+export type SnakeCaseProps<T> = {
+  [K in keyof T as SnakeCase<K>]: SnakeCaseProps<T[K]>;
+};
