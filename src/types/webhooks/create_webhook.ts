26ec455ef07bf4cfdc1679155d7c54a5ce7db523
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 19:41:14 +0200

    redundant

--- src/types/webhooks/create_webhook.ts
+++ src/types/webhooks/create_webhook.ts
@@ -1,11 +1,6 @@
-import { SnakeCaseProps } from "../util.ts";
-
 export interface CreateWebhook {
   /** Name of the webhook (1-80 characters) */
   name: string;
   /** Image for the default webhook avatar */
   avatar: string | null;
 }
-
-/** https://discord.com/developers/docs/resources/webhook#create-webhook-json-params */
-export type DiscordCreateWebhook = SnakeCaseProps<CreateWebhook>;
