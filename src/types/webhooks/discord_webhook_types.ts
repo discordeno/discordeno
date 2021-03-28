b8b7d5fcbfb0546efd9e80c9bf0bc94feef95fb6
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 19:49:17 +0200

    typoooo

--- src/types/webhooks/discord_webhook_types.ts
+++ src/types/webhooks/discord_webhook_types.ts
@@ -1,6 +1,6 @@
 /** https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-types */
 export enum DiscordWebhookTypes {
-  /** Incomming Webhooks can post messages to channels with a generated token */
+  /** Incoming Webhooks can post messages to channels with a generated token */
   Incoming = 1,
   /** Channel Follower Webhooks are internal webhooks used with Channel Following to post new messages into channels */
   ChannelFollower,
