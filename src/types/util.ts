9249ee19a277eea8293046b798a6fde8c061a315
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 11:35:21 +0200

    better names

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,25 +1,25 @@
-export type CamelizeString<T extends PropertyKey> = T extends string
+export type CamelCase<T extends PropertyKey> = T extends string
   ? string extends T ? string
-  : T extends `${infer F}_${infer R}` ? `${F}${Capitalize<CamelizeString<R>>}`
+  : T extends `${infer F}_${infer R}` ? `${F}${Capitalize<CamelCase<R>>}`
   : T
   : T;
 
-export type Camelize<T> = {
-  [K in keyof T as CamelizeString<K>]: Camelize<T[K]>;
+export type CamelCaseProps<T> = {
+  [K in keyof T as CamelCase<K>]: CamelCaseProps<T[K]>;
 };
 
-type SnakelizeString<T extends PropertyKey> = string extends T ? string
+export type SnakeCase<T extends PropertyKey> = string extends T ? string
   : T extends `${infer F}${infer U}${infer R}`
     ? `${F extends Uppercase<F> ? "_" : ""}${Lowercase<F>}${U extends
-      Uppercase<U> ? "_" : ""}${Lowercase<U>}${SnakelizeString<R>}`
+      Uppercase<U> ? "_" : ""}${Lowercase<U>}${SnakeCase<R>}`
   : T extends `${infer F}${infer R}`
-    ? `${F extends Uppercase<F> ? "_" : ""}${Lowercase<F>}${SnakelizeString<R>}`
+    ? `${F extends Uppercase<F> ? "_" : ""}${Lowercase<F>}${SnakeCase<R>}`
   : "";
 
-type Snakelize<T> = T extends readonly any[]
-  ? { [K in keyof T]: Snakelize<T[K]> }
+export type SnakeCaseProps<T> = T extends readonly any[]
+  ? { [K in keyof T]: SnakeCaseProps<T[K]> }
   : T extends object ? {
-    [K in keyof T as SnakelizeString<Extract<K, string>>]: Snakelize<
+    [K in keyof T as SnakeCase<Extract<K, string>>]: SnakeCaseProps<
       T[K]
     >;
   }
