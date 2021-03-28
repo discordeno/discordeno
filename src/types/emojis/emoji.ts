70861c955a41460509c5a4fcc34ffdb882d00129
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 23:05:56 +0200

    feat(types/emojis): Emoji

---
+++ src/types/emojis/emoji.ts
@@ -0,0 +1,23 @@
+import { SnakeCaseProps } from "../util.ts";
+
+export interface Emoji {
+  /** Emoji id */
+  id: string | null;
+  /** Emoji name (can only be null in reaction emoji objects) */
+  name: string | null;
+  /** Roles this emoji is whitelisted to */
+  roles?: string[];
+  /** User that created this emoji */
+  user?: User;
+  /** Whether this emoji must be wrapped in colons */
+  requireColons?: boolean;
+  /** Whether this emoji is managed */
+  managed?: boolean;
+  /** Whether this emoji is animated */
+  animated?: boolean;
+  /** Whether this emoji can be used, may be false due to loss of Server Boosts */
+  available?: boolean;
+}
+
+/** https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure */
+export type DiscordEmoji = SnakeCaseProps<Emoji>;
