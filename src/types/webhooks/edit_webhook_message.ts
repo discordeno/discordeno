430de0eff4e6f797312935794f6fcbbad413a3dc
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 19:37:28 +0200

    feat(types/webhooks): EditWebhookMessage

---
+++ src/types/webhooks/edit_webhook_message.ts
@@ -0,0 +1,15 @@
+import { Embed } from "../embeds/embed.ts";
+import { AllowedMentions } from "../messages/allowed_mentions.ts";
+import { SnakeCaseProps } from "../util.ts";
+
+export interface EditWebhookMessage {
+  /** The message contents (up to 2000 characters) */
+  content?: string | null;
+  /** Embedded `rich` content */
+  embeds?: Embed[] | null;
+  /** Allowed mentions for the message */
+  allowedMentions?: AllowedMentions | null;
+}
+
+/** https://discord.com/developers/docs/resources/webhook#edit-webhook-message-jsonform-params */
+export type DiscordEditWebhookMessage = SnakeCaseProps<EditWebhookMessage>;
