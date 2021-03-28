3f40aeb75fd159e01ec1cca67e76d92dedb78c06
ayntee <ayyantee@gmail.com>
2021-03-28 20:26:07 +0400

    types: add user types (#700)
    * types: add user types
    
    * Add CreateGroupDM

---
+++ src/types/users/create_group_dm.ts
@@ -0,0 +1,11 @@
+import { SnakeCaseProps } from "../util.ts";
+
+export interface CreateGroupDM {
+  /** Access tokens of users that have granted your app the `gdm.join` scope */
+  accessTokens: string[];
+  /** A dictionary of user ids to their respective nicknames */
+  nicks: Record<string, string>;
+}
+
+/** https://discord.com/developers/docs/resources/user#create-group-dm */
+export type DiscordCreateGroupDM = SnakeCaseProps<CreateGroupDM>;
