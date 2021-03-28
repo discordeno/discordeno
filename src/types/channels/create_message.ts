e6530ab13e813888f4e0c367a72807f2aca44658
ayntee <ayyantee@gmail.com>
2021-03-28 15:41:50 +0400

    types(channels): add ModifyChannel and CreateMessage

---
+++ src/types/channels/create_message.ts
@@ -0,0 +1,21 @@
+import { Embed } from "../embeds/embed.ts";
+import { AllowedMentions } from "../messages/allowed_mentions.ts";
+import { SnakeCaseProps } from "../util.ts";
+
+export interface CreateMessage {
+  /** The message contents (up to 2000 characters) */
+  content?: string;
+  /** A nonce that can be used for optimistic message sending */
+  nonce?: number | string;
+  /** true if this is a TTS message */
+  tts?: boolean;
+  /** Embedded `rich` content */
+  embed?: Embed;
+  /** Allowed mentions for a message */
+  allowedMentions?: AllowedMentions;
+  /** Include to make your message a reply */
+  messageReference?: MessageReference;
+}
+
+/** https://discord.com/developers/docs/resources/channel#create-message */
+export type DiscordCreateMessage = SnakeCaseProps<CreateMessage>;
