52c5cb6cafd8309a74dca2a211d947b94c314906
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-28 19:37:34 +0200

    feat(types/webhooks): ExecuteWebhook

---
+++ src/types/webhooks/execute_webhook.ts
@@ -0,0 +1,25 @@
+import { Embed } from "../embeds/embed.ts";
+import { AllowedMentions } from "../messages/allowed_mentions.ts";
+import { SnakeCaseProps } from "../util.ts";
+
+export interface ExecuteWebhook {
+  /** Waits for server confirmation of message send before response, and returns the created message body (defaults to `false`; when `false` a message that is not saved does not return an error) */
+  wait?: boolean;
+  /** The message contents (up to 2000 characters) */
+  content?: string;
+  /** Override the default username of the webhook */
+  username?: string;
+  /** Override the default avatar of the webhook */
+  avatarUrl?: string;
+  /** True if this is a TTS message */
+  tts?: boolean;
+  /** The contents of the file being sent */
+  file: FileContent | FileContent[];
+  /** Embedded `rich` content */
+  embeds: Embed[];
+  /** Allowed mentions for the message */
+  allowedMentions: AllowedMentions;
+}
+
+/** https://discord.com/developers/docs/resources/webhook#execute-webhook */
+export type DiscordExecuteWebhook = SnakeCaseProps<ExecuteWebhook>;
