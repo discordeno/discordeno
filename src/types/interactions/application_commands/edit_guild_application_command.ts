917e7603a334b991960a17949b2bcaa0e28c7a3a
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-29 14:11:22 +0200

    types: add Interaction EditGuildApplicationCommand

---
+++ src/types/interactions/application_commands/edit_guild_application_command.ts
@@ -0,0 +1,16 @@
+import { SnakeCaseProps } from "../../util.ts";
+import { ApplicationCommandOption } from "./application_command_option.ts";
+
+export interface EditGuildApplicationCommand {
+  /** 1-31 character name matching `^[\w-]{1,32}$` */
+  name?: string;
+  /** 1-100 character description */
+  description?: string;
+  /** The parameters for the command */
+  options?: ApplicationCommandOption[] | null;
+}
+
+/** https://discord.com/developers/docs/interactions/slash-commands#edit-guild-application-command-json-params */
+export type DiscordEditGuildApplicationCommand = SnakeCaseProps<
+  EditGuildApplicationCommand
+>;
