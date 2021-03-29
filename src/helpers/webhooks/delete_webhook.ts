e37cb2151e68e7da33b1dcd873dd73b3aadd933e
ayntee <ayyantee@gmail.com>
2021-03-29 19:31:06 +0400

    Update src/helpers/webhooks/delete_webhook.ts
    Co-authored-by: ITOH <72305210+itohatweb@users.noreply.github.com>

--- src/helpers/webhooks/delete_webhook.ts
+++ src/helpers/webhooks/delete_webhook.ts
@@ -6,7 +6,7 @@ import { requireBotChannelPermissions } from "../../util/permissions.ts";
 export async function deleteWebhook(channelId: string, webhookId: string) {
   await requireBotChannelPermissions(channelId, ["MANAGE_WEBHOOKS"]);
 
-  const result = await RequestManager.delete(endpoints.WEBHOOK_Id(webhookId));
+  const result = await RequestManager.delete(endpoints.WEBHOOK_ID(webhookId));
 
   return result;
 }
