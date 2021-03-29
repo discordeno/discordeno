d1ef961f3965ccdac680fddbd28333e2dfc2ea1f
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-29 14:10:54 +0200

    types: add Interaction CreateGlobalApplicationCommand

---
+++ src/types/interactions/application_commands/create_global_application_command.ts
@@ -0,0 +1,15 @@
+import { SnakeCaseProps } from "../../util.ts";
+
+export interface CreateGlobalApplicationCommand {
+  /** 1-31 character name matching `^[\w-]{1,32}$` */
+  name: string;
+  /** 1-100 character description */
+  description: string;
+  /** The parameters for the command */
+  options?: ApplicationCommandOption[];
+}
+
+/** https://discord.com/developers/docs/interactions/slash-commands#create-global-application-command-json-params */
+export type DiscordCreateGlobalApplicationCommand = SnakeCaseProps<
+  CreateGlobalApplicationCommand
+>;
