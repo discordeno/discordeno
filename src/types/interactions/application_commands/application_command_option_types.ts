fb6ad65d8c82b97367a959d639b33eec9b251793
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-29 14:10:44 +0200

    types: add Interaction ApplicationCommandOptionTypes

---
+++ src/types/interactions/application_commands/application_command_option_types.ts
@@ -0,0 +1,11 @@
+/** https://discord.com/developers/docs/interactions/slash-commands#applicationcommandoptiontype */
+export enum ApplicationCommandOptionTypes {
+  SUB_COMMAND = 1,
+  SUB_COMMAND_GROUP,
+  STRING,
+  INTEGER,
+  BOOLEAN,
+  USER,
+  CHANNEL,
+  ROLE,
+}
