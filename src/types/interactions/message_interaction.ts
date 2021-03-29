2b14e0c7a271be6d159445a1f154effe720090c9
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-29 14:12:03 +0200

    types: add Interaction MessageInteraction

---
+++ src/types/interactions/message_interaction.ts
@@ -0,0 +1,16 @@
+import { SnakeCaseProps } from "../util.ts";
+import { InteractionTypes } from "./interaction_types.ts";
+
+export interface MessageInteraction {
+  /** Id of the interaction */
+  id: string;
+  /** The type of interaction */
+  type: InteractionTypes;
+  /** The name of the ApplicationCommand */
+  name: string;
+  /** The user who invoked the interaction */
+  user: User;
+}
+
+/** https://discord.com/developers/docs/interactions/slash-commands#messageinteraction */
+export type DiscordMessageInteraction = SnakeCaseProps<MessageInteraction>;
