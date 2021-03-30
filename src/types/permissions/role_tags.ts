79be200943431bd9dd4f5f7a49cdb90f0f72ca1a
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-30 10:10:53 +0200

    types: add role types (#717)
    * move perm types
    
    * types: add role types
    
    * types: add roletags types
    
    Co-authored-by: ayntee <ayyantee@gmail.com>

---
+++ src/types/permissions/role_tags.ts
@@ -0,0 +1,13 @@
+import { SnakeCaseProps } from "../util.ts";
+
+export interface RoleTags {
+  /** The id of the bot this role belongs to */
+  botId?: string;
+  /** The id of the integration this role belongs to */
+  integrationId?: string;
+  /** Whether this is the guild's premium subscriber role */
+  premiumSubscriber?: null;
+}
+
+/** https://discord.com/developers/docs/topics/permissions#role-object-role-tags-structure */
+export type DiscordRoleTags = SnakeCaseProps<RoleTags>;
