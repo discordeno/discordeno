3a7ac5aeded51fc13bf2735f551d3807bd5fab56
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-31 11:17:42 +0200

    Update src/types/guilds/modify_current_user_nick.ts
    Co-authored-by: ayntee <ayyantee@gmail.com>

--- src/types/guilds/modify_current_user_nick.ts
+++ src/types/guilds/modify_current_user_nick.ts
@@ -1,11 +1,7 @@
-import { SnakeCaseProps } from "../util.ts";
-
 export interface ModifyCurrentUserNick {
   /** Value to set users nickname to. Requires the CHANGENICKNAME permission */
   nick?: string | null;
 }
 
 /** https://discord.com/developers/docs/resources/guild#modify-current-user-nick */
-export type DiscordModifyCurrentUserNick = SnakeCaseProps<
-  ModifyCurrentUserNick
->;
+export type DiscordModifyCurrentUserNick = ModifyCurrentUserNick;
