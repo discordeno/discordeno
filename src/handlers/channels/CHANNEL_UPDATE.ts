dd21008f90e94e94d5fbd7a1a45f16e411caa3f8
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-31 19:51:46 +0200

    fix(handlers): add missing imports (#723)
    Co-authored-by: ayntee <ayyantee@gmail.com>

--- src/handlers/channels/CHANNEL_UPDATE.ts
+++ src/handlers/channels/CHANNEL_UPDATE.ts
@@ -1,6 +1,7 @@
 import { eventHandlers } from "../../bot.ts";
 import { cacheHandlers } from "../../cache.ts";
 import { structures } from "../../structures/mod.ts";
+import { DiscordChannel, DiscordGatewayPayload } from "../../types/mod.ts";
 
 export async function handleChannelUpdate(data: DiscordGatewayPayload) {
   const payload = data.d as DiscordChannel;
