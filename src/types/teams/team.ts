f47d92245ada49fc1b7e31a64021cb6e102c54f3
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-31 16:51:02 +0200

    add team types

---
+++ src/types/teams/team.ts
@@ -0,0 +1,16 @@
+import { SnakeCaseProps } from "../util.ts";
+import { TeamMember } from "./team_member.ts";
+
+export interface Team {
+  /** A hash of the image of the team's icon */
+  icon: string | null;
+  /** The unique id of the team */
+  id: string;
+  /** The members of the team */
+  members: TeamMember[];
+  /** The user id of the current team owner */
+  ownerUserId: string;
+}
+
+/** https://discord.com/developers/docs/topics/teams#data-models-team-object */
+export type DiscordTeam = SnakeCaseProps<Team>;
