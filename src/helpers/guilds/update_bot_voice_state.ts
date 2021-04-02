f3b4ce9fc13c4f6a89e05acfb62e110449b954dc
ayntee <ayyantee@gmail.com>
2021-04-02 17:23:46 +0400

    idk

--- src/helpers/guilds/update_bot_voice_state.ts
+++ src/helpers/guilds/update_bot_voice_state.ts
@@ -1,4 +1,12 @@
-import { UpdateSelfVoiceState } from "../../types/guilds/update_self_voice_state.ts";
+import { RequestManager } from "../../rest/request_manager.ts";
+import {
+  DiscordUpdateSelfVoiceState,
+  UpdateSelfVoiceState,
+} from "../../types/guilds/update_self_voice_state.ts";
+import { camelKeysToSnakeCase } from "../../util/utils.ts";
 
 export function updateBotVoiceState(data: UpdateSelfVoiceState) {
+  const payload: DiscordUpdateSelfVoiceState = camelKeysToSnakeCase(data);
+
+  RequestManager.patch();
 }
