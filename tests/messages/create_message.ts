38196e1dbf4bac2009672c42fc68702071115ac7
TriForMine <quentin.nicolini@hotmail.fr>
2021-04-09 21:17:11 +0200

    tests: add create/delete/edit message tests

---
+++ tests/messages/create_message.ts
@@ -0,0 +1,23 @@
+import { cache, delay, sendMessage } from "../../mod.ts";
+import { defaultTestOptions, tempData } from "../ws/start_bot.ts";
+import { assertExists } from "../deps.ts";
+
+Deno.test({
+  name: "[message] send a new message",
+  async fn() {
+    const message = await sendMessage(tempData.channelId, "Hello World!");
+
+    // Assertions
+    assertExists(message);
+
+    // Delay the execution by 5 seconds to allow MESSAGE_CREATE event to be processed
+    await delay(5000);
+
+    if (!cache.messages.has(message.id)) {
+      throw new Error(
+        "The message seemed to be sent but it was not cached.",
+      );
+    }
+  },
+  ...defaultTestOptions,
+});
---
+++ tests/messages/delete_message.ts
@@ -0,0 +1,63 @@
+import { cache, delay, deleteMessage, sendMessage } from "../../mod.ts";
+import { defaultTestOptions, tempData } from "../ws/start_bot.ts";
+import { assertExists } from "../deps.ts";
+
+Deno.test({
+  name: "[message] delete a message without a reason.",
+  async fn() {
+    const message = await sendMessage(tempData.channelId, "Hello World!");
+
+    // Assertions
+    assertExists(message);
+    // Delay the execution by 5 seconds to allow MESSAGE_CREATE event to be processed
+    await delay(5000);
+    // Make sure the message was created.
+    if (!cache.messages.has(message.id)) {
+      throw new Error(
+        "The message seemed to be sent but it was not cached.",
+      );
+    }
+
+    // Delete the message now without a reason
+    await deleteMessage(tempData.channelId, message.id);
+    // Wait 5 seconds to give it time for MESSAGE_DELETE event
+    await delay(5000);
+    // Make sure it is gone from cache
+    if (cache.messages.has(message.id)) {
+      throw new Error(
+        "The message should have been deleted but it is still in cache.",
+      );
+    }
+  },
+  ...defaultTestOptions,
+});
+
+Deno.test({
+  name: "[message] delete a message with a reason.",
+  async fn() {
+    const message = await sendMessage(tempData.channelId, "Hello World!");
+
+    // Assertions
+    assertExists(message);
+    // Delay the execution by 5 seconds to allow MESSAGE_CREATE event to be processed
+    await delay(5000);
+    // Make sure the message was created.
+    if (!cache.messages.has(message.id)) {
+      throw new Error(
+        "The message seemed to be sent but it was not cached.",
+      );
+    }
+
+    // Delete the message now without a reason
+    await deleteMessage(tempData.channelId, message.id, "with a reason");
+    // Wait 5 seconds to give it time for MESSAGE_DELETE event
+    await delay(5000);
+    // Make sure it is gone from cache
+    if (cache.messages.has(message.id)) {
+      throw new Error(
+        "The message should have been deleted but it is still in cache.",
+      );
+    }
+  },
+  ...defaultTestOptions,
+});
