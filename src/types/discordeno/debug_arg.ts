6b6b6d4cac0a04872b350e6abd3b06fc75a24045
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-10 12:59:31 +0200

    add

---
+++ src/types/discordeno/debug_arg.ts
@@ -0,0 +1,26 @@
+export interface DebugArg {
+  /** Red is for errors or urgent issues. Yellow is for warnings/alerts. Green is for actions being taken. Blue is for  */
+  type?:
+    | "gatewayIdentify"
+    | "error"
+    | "globallyRateLimited"
+    | "requestCreate"
+    | "requestSuccess"
+    | "requestFetch"
+    | "requestFetched"
+    | "requestMembersProcessing"
+    | "gatewayHeartbeat"
+    | "gatewayHeartbeatStopped"
+    | "shardCreate"
+    | "gatewayInvalidSession"
+    | "gatewayReconnect"
+    | "gatewayResume"
+    | "gatewayResumed"
+    | "wsClose"
+    | "wsError"
+    | "wsReconnect"
+    | "missingShard"
+    // TODO: remove this
+    | "loop";
+  data: unknown;
+}
