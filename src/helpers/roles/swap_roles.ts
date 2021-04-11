5e6d749ffab63579384ae3c5d2fb08e5dc7c28bc
TriForMine <quentin.nicolini@hotmail.fr>
2021-04-11 20:41:15 +0200

    tests: add roles tests (#822)

--- src/helpers/roles/swap_roles.ts
+++ src/helpers/roles/swap_roles.ts
@@ -3,13 +3,13 @@ import { endpoints } from "../../util/constants.ts";
 import { requireBotGuildPermissions } from "../../util/permissions.ts";
 
 /** Modify the positions of a set of role objects for the guild. Requires the MANAGE_ROLES permission. */
-export async function swapRoles(guildId: string, rolePositons: PositionSwap) {
+export async function swapRoles(guildId: string, rolePositions: PositionSwap) {
   await requireBotGuildPermissions(guildId, ["MANAGE_ROLES"]);
 
   const result = await rest.runMethod(
     "patch",
     endpoints.GUILD_ROLES(guildId),
-    rolePositons,
+    rolePositions,
   );
 
   return result;
