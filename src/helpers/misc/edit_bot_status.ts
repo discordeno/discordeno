af3dbf15648bd2340674bb48dd32806b5caa231a
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-04-12 11:17:55 -0400

    refactor: move edit_bot_status to another file in helpers (#838)

---
+++ src/helpers/misc/edit_bot_status.ts
@@ -0,0 +1,23 @@
+import { eventHandlers } from "../../bot.ts";
+import { DiscordGatewayOpcodes } from "../../types/codes/gateway_opcodes.ts";
+import { StatusUpdate } from "../../types/gateway/status_update.ts";
+import { ws } from "../../ws/ws.ts";
+
+export function editBotStatus(data: Omit<StatusUpdate, "afk" | "since">) {
+  ws.shards.forEach((shard) => {
+    eventHandlers.debug?.(
+      "loop",
+      `Running forEach loop in editBotStatus function.`
+    );
+    shard.ws.send(
+      JSON.stringify({
+        op: DiscordGatewayOpcodes.StatusUpdate,
+        d: {
+          since: null,
+          afk: false,
+          ...data,
+        },
+      })
+    );
+  });
+}
