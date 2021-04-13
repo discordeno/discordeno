5b518fc1d720fd7fda3b668f21a084043d5d74c3
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-13 22:32:36 +0200

    https://github.com/discord/discord-api-docs/pull/2087

--- src/types/integration/create_guild_integration.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateGuildIntegration {
-  /** The integration type */
-  type: string;
-  /** The integration id */
-  id: string;
-}
-
-/** https://discord.com/developers/docs/resources/guild#create-guild-integration-json-params */
-export type DiscordCreateGuildIntegration = CreateGuildIntegration;
