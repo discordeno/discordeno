56b66c496ef28a401a9a54183e132565727580cb
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-13 22:56:13 +0200

    https://github.com/discord/discord-api-docs/pull/2620

--- src/types/guilds/mfa_levels.ts
+++ src/types/guilds/mfa_levels.ts
@@ -1,5 +1,7 @@
 /** https://discord.com/developers/docs/resources/guild#guild-object-mfa-level */
 export enum DiscordMfaLevels {
+  /** Guild has no MFA/2FA requirement for moderation actions */
   NONE,
+  /** Guild has a 2FA requirement for moderation actions */
   ELEVATED,
 }
--- src/types/guilds/premium_tiers.ts
+++ src/types/guilds/premium_tiers.ts
@@ -1,7 +1,11 @@
 /** https://discord.com/developers/docs/resources/guild#guild-object-premium-tier */
 export enum DiscordPremiumTiers {
+  /** Guild has not unlocked any Server Boost perks */
   NONE,
+  /** Guild has unlocked Server Boost level 1 perks */
   TIER_1,
+  /** Guild has unlocked Server Boost level 2 perks */
   TIER_2,
+  /** Guild has unlocked Server Boost level 3 perks */
   TIER_3,
 }
