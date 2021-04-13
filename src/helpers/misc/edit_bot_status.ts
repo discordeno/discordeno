b93dc0963f70fcb2f957710cee3f5dcfe07b1806
TriForMine <quentin.nicolini@hotmail.fr>
2021-04-13 09:31:45 +0200

    fix(helpers): fix import StatusUpdate as type (#842)
    * fix(helpers): add missing export for editBotStatus
    
    * fix(helpers): fix import type

--- src/helpers/misc/edit_bot_status.ts
+++ src/helpers/misc/edit_bot_status.ts
@@ -1,6 +1,6 @@
 import { eventHandlers } from "../../bot.ts";
 import { DiscordGatewayOpcodes } from "../../types/codes/gateway_opcodes.ts";
-import { StatusUpdate } from "../../types/gateway/status_update.ts";
+import type { StatusUpdate } from "../../types/gateway/status_update.ts";
 import { ws } from "../../ws/ws.ts";
 
 export function editBotStatus(data: Omit<StatusUpdate, "afk" | "since">) {
