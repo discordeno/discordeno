8f0a3d564d64221cd596a7b4dddb185cec9501d5
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-13 22:59:21 +0200

    https://github.com/discord/discord-api-docs/pull/2789

--- src/types/gateway/status_update.ts
+++ src/types/gateway/status_update.ts
@@ -5,8 +5,8 @@ import { DiscordStatusTypes } from "./status_types.ts";
 export interface StatusUpdate {
   /** Unix time (in milliseconds) of when the client went idle, or null if the client is not idle */
   since: number | null;
-  /** null, or the user's activities */
-  activities: Activity[] | null;
+  /** The user's activities */
+  activities: Activity[];
   /** The user's new status */
   status: DiscordStatusTypes;
   /** Whether or not the client is afk */
