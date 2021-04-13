c358d4f932235df28c92690fd6ad8f84808184d6
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-13 21:58:57 +0200

    use proper invite types

--- src/helpers/invites/create_invite.ts
+++ src/helpers/invites/create_invite.ts
@@ -1,12 +1,12 @@
 import { rest } from "../../rest/rest.ts";
-import { InviteCreate } from "../../types/invites/invite_create.ts";
+import { CreateChannelInvite } from "../../types/invites/create_channel_invite.ts";
 import { endpoints } from "../../util/constants.ts";
 import { requireBotChannelPermissions } from "../../util/permissions.ts";
 
 /** Creates a new invite for this channel. Requires CREATE_INSTANT_INVITE */
 export async function createInvite(
   channelId: string,
-  options: InviteCreate,
+  options: CreateChannelInvite,
 ) {
   await requireBotChannelPermissions(channelId, ["CREATE_INSTANT_INVITE"]);
 
