121e0711e9fa022113f142ab43751219848fc893
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-13 21:40:57 +0200

    https://github.com/discord/discord-api-docs/pull/1776

--- src/types/guilds/modify_guild_channel_position.ts
+++ src/types/guilds/modify_guild_channel_position.ts
@@ -1,8 +1,13 @@
+// TODO: most likely it is but check if lockPositions and parentId really are optional
 export interface ModifyGuildChannelPositions {
   /** Channel id */
   id: string;
   /** Sorting position of the channel */
   position: number | null;
+  /** Syncs the permission overwrites with the new parent, if moving to a new category */
+  lockPositions?: boolean | null;
+  /** The new parent ID for the channel that is moved */
+  parentId?: string | null;
 }
 
 /** https://discord.com/developers/docs/resources/guild#modify-guild-channel-positions */
