9bb4a649d943bad9f6f4b98681f33bab995d9c04
TriForMine <quentin.nicolini@hotmail.fr>
2021-04-13 23:14:20 +0200

    tests: add invites tests

--- src/helpers/invites/get_invite.ts
+++ src/helpers/invites/get_invite.ts
@@ -3,7 +3,7 @@ import { DiscordInvite, Invite } from "../../types/invites/invite.ts";
 import { endpoints } from "../../util/constants.ts";
 import { snakeKeysToCamelCase } from "../../util/utils.ts";
 
-/** Returns an invite for the given code. */
+/** Returns an invite for the given code or throws an error if the invite doesn't exists. */
 export async function getInvite(inviteCode: string) {
   const result: DiscordInvite = await rest.runMethod(
     "get",
