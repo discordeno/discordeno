ef88a57a38c74df4ff23badc66a0f3414e69cd3a
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-04-14 08:47:46 -0400

    Update src/helpers/channels/clone_channel.ts
    Co-authored-by: ITOH <72305210+itohatweb@users.noreply.github.com>

--- src/helpers/channels/clone_channel.ts
+++ src/helpers/channels/clone_channel.ts
@@ -10,7 +10,7 @@ export async function cloneChannel(channelId: string, reason?: string) {
     channelId
   );
   //Return undefined if channel is not cached (unsure about error handling)
-  if (!channelToClone) return;
+  if (!channelToClone) throw new Error(Errors.CHANNEL_NOT_FOUND);
 
   //If "name" is undefined as specified by types
   channelToClone.name ??= "new-channel";
