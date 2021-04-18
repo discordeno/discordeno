819712677b0c81874950d25fd8e4ade1ac841a49
itohatweb <itohatweb@users.noreply.github.com>
2021-04-18 19:31:33 +0000

    Commit from GitHub Actions (Lint)

--- src/helpers/discovery/get_discovery_categories.ts
+++ src/helpers/discovery/get_discovery_categories.ts
@@ -19,7 +19,7 @@ export async function getDiscoveryCategories() {
       (category) => [
         category.id,
         snakeKeysToCamelCase<DiscoveryCategory>(category),
-      ]
+      ],
     ),
   );
 }
