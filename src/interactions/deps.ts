59dbc8ed068e13ab4786b53be769f26f91f6e1e2
ayntee <ayyantee@gmail.com>
2021-04-19 11:51:05 +0400

    Use nacl for signature verification and remove server.ts (included in template)

--- src/interactions/deps.ts
+++ src/interactions/deps.ts
@@ -1,2 +1,1 @@
-export { serve } from "https://deno.land/std@0.90.0/http/server.ts";
-export { verify } from "https://unpkg.com/@evan/wasm@0.0.50/target/ed25519/deno.js";
+export { sign } from "https://unpkg.com/@evan/wasm@0.0.50/target/nacl/deno.js";
