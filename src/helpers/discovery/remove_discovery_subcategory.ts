6a29304f20ee74349a53abf8d8c09351525e56e2
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-20 17:21:18 +0200

    Update remove_discovery_subcategory.ts

--- src/helpers/discovery/remove_discovery_subcategory.ts
+++ src/helpers/discovery/remove_discovery_subcategory.ts
@@ -3,7 +3,7 @@ import { endpoints } from "../../util/constants.ts";
 import { requireBotGuildPermissions } from "../../util/permissions.ts";
 
 /** Removes a discovery subcategory from the guild. Requires the MANAGE_GUILD permission. Returns a 204 No Content on success. */
-export async function deleteDiscoverySubcategory(
+export async function removeDiscoverySubcategory(
   guildId: string,
   categoryId: number,
 ) {
