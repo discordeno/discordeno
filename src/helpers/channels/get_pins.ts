3812025506df85dfdd67c5e6522598adda17374b
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-23 23:27:39 +0200

    Update get_pins.ts

--- src/helpers/channels/get_pins.ts
+++ src/helpers/channels/get_pins.ts
@@ -1,14 +1,14 @@
 import { rest } from "../../rest/rest.ts";
 import { structures } from "../../structures/mod.ts";
-import { DiscordMessage } from "../../types/messages/message.ts";
+import { Message } from "../../types/messages/message.ts";
 import { endpoints } from "../../util/constants.ts";
 
 /** Get pinned messages in this channel. */
 export async function getPins(channelId: string) {
-  const result = (await rest.runMethod(
+  const result = await rest.runMethod<Message[]>(
     "get",
     endpoints.CHANNEL_PINS(channelId),
-  )) as DiscordMessage[];
+  );
 
   return Promise.all(
     result.map((res) => structures.createDiscordenoMessage(res)),
