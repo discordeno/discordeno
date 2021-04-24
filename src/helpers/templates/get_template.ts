973ec219cc9cae8efadbc7230d935a1022abbffd
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-24 19:42:11 +0200

    templates

--- src/helpers/templates/get_template.ts
+++ src/helpers/templates/get_template.ts
@@ -1,14 +1,11 @@
 import { rest } from "../../rest/rest.ts";
-import { DiscordTemplate, Template } from "../../types/templates/template.ts";
+import { Template } from "../../types/templates/template.ts";
 import { endpoints } from "../../util/constants.ts";
-import { snakeKeysToCamelCase } from "../../util/utils.ts";
 
 /** Returns the guild template if it exists */
 export async function getTemplate(templateCode: string) {
-  const template = await rest.runMethod<DiscordTemplate>(
+  return await rest.runMethod<Template>(
     "get",
     endpoints.GUILD_TEMPLATE(templateCode),
   );
-
-  return snakeKeysToCamelCase<Template>(template);
 }
