c0251e4b41e83702c28954f5cfe3445040f9b42e
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-24 19:37:40 +0200

    Update delete_server.ts

--- src/helpers/guilds/delete_server.ts
+++ src/helpers/guilds/delete_server.ts
@@ -3,8 +3,9 @@ import { endpoints } from "../../util/constants.ts";
 
 /** Delete a guild permanently. User must be owner. Returns 204 No Content on success. Fires a Guild Delete Gateway event.
  */
-export async function deleteServer(guildId: string): Promise<undefined> {
-  const result = await rest.runMethod("delete", endpoints.GUILDS_BASE(guildId));
-
-  return result;
+export async function deleteServer(guildId: string) {
+  return await rest.runMethod<undefined>(
+    "delete",
+    endpoints.GUILDS_BASE(guildId),
+  );
 }
