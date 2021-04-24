ad22bd32701d499da9d01fceb3ea8a214667f563
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-24 19:37:38 +0200

    Update create_guild.ts

--- src/helpers/guilds/create_guild.ts
+++ src/helpers/guilds/create_guild.ts
@@ -3,17 +3,17 @@ import { cacheHandlers } from "../../cache.ts";
 import { rest } from "../../rest/rest.ts";
 import { structures } from "../../structures/mod.ts";
 import { CreateGuild } from "../../types/guilds/create_guild.ts";
-import { DiscordGuild } from "../../types/guilds/guild.ts";
+import { Guild } from "../../types/guilds/guild.ts";
 import { endpoints } from "../../util/constants.ts";
 import { getMember } from "../members/get_member.ts";
 
 /** Create a new guild. Returns a guild object on success. Fires a Guild Create Gateway event. This endpoint can be used only by bots in less than 10 guilds. */
 export async function createGuild(options: CreateGuild) {
-  const result = (await rest.runMethod(
+  const result = await rest.runMethod<Guild>(
     "post",
     endpoints.GUILDS,
     options,
-  )) as DiscordGuild;
+  );
 
   const guild = await structures.createDiscordenoGuild(result, 0);
   // MANUALLY CACHE THE GUILD
