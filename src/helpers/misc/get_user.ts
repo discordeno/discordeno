d7015df65fb05a6f2b9167a40e5044159d7177b3
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-24 19:39:40 +0200

    misc

--- src/helpers/misc/get_user.ts
+++ src/helpers/misc/get_user.ts
@@ -1,12 +1,8 @@
 import { rest } from "../../rest/rest.ts";
-import { DiscordUser } from "../../types/users/user.ts";
 import { User } from "../../types/users/user.ts";
 import { endpoints } from "../../util/constants.ts";
-import { snakeKeysToCamelCase } from "../../util/utils.ts";
 
 /** This function will return the raw user payload in the rare cases you need to fetch a user directly from the API. */
 export async function getUser(userId: string) {
-  const result: User = await rest.runMethod("get", endpoints.USER(userId));
-
-  return snakeKeysToCamelCase<DiscordUser>(result);
+  return await rest.runMethod<User>("get", endpoints.USER(userId));
 }
