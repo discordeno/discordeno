a84ede2a001725d6e3ff32db678e1ddb91944fad
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-29 09:04:02 +0200

    wrong converter

--- src/helpers/commands/batch_edit_slash_command_permissions.ts
+++ src/helpers/commands/batch_edit_slash_command_permissions.ts
@@ -2,7 +2,7 @@ import { applicationId } from "../../bot.ts";
 import { rest } from "../../rest/rest.ts";
 import { ApplicationCommandPermissions } from "../../types/interactions/application_command_permissions.ts";
 import { endpoints } from "../../util/constants.ts";
-import { snakeKeysToCamelCase } from "../../util/utils.ts";
+import { camelKeysToSnakeCase } from "../../util/utils.ts";
 
 /** Batch edits permissions for all commands in a guild. Takes an array of partial GuildApplicationCommandPermissions objects including `id` and `permissions`. */
 export async function batchEditSlashCommandPermissions(
@@ -12,6 +12,6 @@ export async function batchEditSlashCommandPermissions(
   return await rest.runMethod(
     "put",
     endpoints.COMMANDS_PERMISSIONS(applicationId, guildId),
-    snakeKeysToCamelCase(options),
+    camelKeysToSnakeCase(options),
   );
 }
