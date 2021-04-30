fb5d0fa82e0ff35839aa94f73cbc099fbf228cf6
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-30 19:44:55 +0200

    remove Discord* types for misc

--- src/types/misc/activity_timestamps.ts
+++ src/types/misc/activity_timestamps.ts
@@ -1,9 +1,7 @@
+/** https://discord.com/developers/docs/topics/gateway#activity-object-activity-timestamps */
 export interface ActivityTimestamps {
   /** Unix time (in milliseconds) of when the activity started */
   start?: number;
   /** Unix time (in milliseconds) of when the activity ends */
   end?: number;
 }
-
-/** https://discord.com/developers/docs/topics/gateway#activity-object-activity-timestamps */
-export type DiscordactivityTimestamps = ActivityTimestamps;
