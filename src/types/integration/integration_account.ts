cc97ccad0351d517311639a8a5d2bdce84a75b97
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-30 19:34:30 +0200

    remove Discord* types for integrations

--- src/types/integration/integration_account.ts
+++ src/types/integration/integration_account.ts
@@ -1,9 +1,7 @@
+/** https://discord.com/developers/docs/resources/guild#integration-account-object-integration-account-structure */
 export interface IntegrationAccount {
   /** Id of the account */
   id: string;
   /** Name of the account */
   name: string;
 }
-
-/** https://discord.com/developers/docs/resources/guild#integration-account-object-integration-account-structure */
-export type DiscordIntegrationAccount = IntegrationAccount;
--- src/types/integration/integration_create_update.ts
+++ src/types/integration/integration_create_update.ts
@@ -1,12 +1,7 @@
-import { SnakeCasedPropertiesDeep } from "../util.ts";
 import { Integration } from "./integration.ts";
 
+/** https://github.com/discord/discord-api-docs/blob/master/docs/topics/Gateway.md#integration-create-event-additional-fields */
 export interface IntegrationCreateUpdate extends Integration {
   /** Id of the guild */
   guildId: string;
 }
-
-/** https://github.com/discord/discord-api-docs/blob/master/docs/topics/Gateway.md#integration-create-event-additional-fields */
-export type DiscordIntegrationCreateUpdate = SnakeCasedPropertiesDeep<
-  IntegrationCreateUpdate
->;
