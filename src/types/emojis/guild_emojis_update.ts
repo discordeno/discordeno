6fc14ff2181b27f4782df5f790d57cff491da03a
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-04-30 19:22:45 +0200

    remove Discord* types from emojis

--- src/types/emojis/guild_emojis_update.ts
+++ src/types/emojis/guild_emojis_update.ts
@@ -1,14 +1,9 @@
 import { Emoji } from "../emojis/emoji.ts";
-import { SnakeCasedPropertiesDeep } from "../util.ts";
 
+/** https://discord.com/developers/docs/topics/gateway#guild-emojis-update */
 export interface GuildEmojisUpdate {
   /** id of the guild */
   guildId: string;
   /** Array of emojis */
   emojis: Emoji[];
 }
-
-/** https://discord.com/developers/docs/topics/gateway#guild-emojis-update */
-export type DiscordGuildEmojisUpdate = SnakeCasedPropertiesDeep<
-  GuildEmojisUpdate
->;
