dc2a3eb9f00d021b951d2f458d02207026e65309
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-01 11:18:16 +0200

    add: ListPublicArchivedThreads

---
+++ src/types/channels/list_public_archived_threads.ts
@@ -0,0 +1,7 @@
+export interface ListPublicArchivedThreads {
+  // TODO: convert unix to ISO9601 timestamp
+  /** Returns threads before this timestamp. UNIX or ISO8601 timestamp */
+  before?: number | string;
+  /** Optional maximum number of threads to return */
+  limit?: number;
+}
