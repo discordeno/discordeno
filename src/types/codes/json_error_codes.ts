3a5745143793b513ef8887444c40955c367f5e67
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-01 11:53:27 +0200

    add: new thread error codes

--- src/types/codes/json_error_codes.ts
+++ src/types/codes/json_error_codes.ts
@@ -78,6 +78,10 @@ export enum DiscordJsonErrorCodes {
   InvalidApiVersionProvided = 50041,
   CannotDeleteAChannelRequiredForCommunityGuilds = 50074,
   InvalidStickerSent = 50081,
+  TriedToPerformAnOperationOnAnArchivedThreadSuchAsEditingAMessageOrAddingAUserToTheThread =
+    50083,
+  InvalidThreadNotificationSettings,
+  BeforeValueIsEarlierThanTheThreadCreationDate,
   ReqctionWasBlocked = 90001,
   ApiResourceIsCurrentlyOverloadedTryAgainALittleLater = 130000,
 }
