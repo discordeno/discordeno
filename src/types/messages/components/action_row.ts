9124534938bdfc2f0e8413b35de0f82ad5345e30
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-01 10:19:50 -0400

    Update src/types/messages/components/action_row.ts
    Co-authored-by: ITOH <72305210+itohatweb@users.noreply.github.com>

--- src/types/messages/components/action_row.ts
+++ src/types/messages/components/action_row.ts
@@ -1,6 +1,7 @@
 import { SnakeCasedPropertiesDeep } from "../../util.ts";
 import { ButtonComponent } from "./button_component.ts";
 
+// TODO: add docs link
 export interface ActionRow {
   /** Action rows are a group of buttons. */
   type: 1;
