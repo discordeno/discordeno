863595f4259a572e5cbc391391f8d736ebfeb8a1
Skillz <gankstarsleague@gmail.com>
2021-05-01 10:25:35 -0400

    fix message components enum

--- src/types/messages/components/message_component_types.ts
+++ src/types/messages/components/message_component_types.ts
@@ -1,6 +1,9 @@
-export enum MessageComponentTypes {
+export enum DiscordMessageComponentTypes {
   /** A row of components at the bottom of a message */
   ActionRow = 1,
   /** A button! */
   Button,
 }
+
+export type MessageComponentTypes = DiscordMessageComponentTypes;
+export const MessageComponentTypes = DiscordMessageComponentTypes;
