82f070884a99d7d1e1516ad9731055f81131e1e7
Skillz <gankstarsleague@gmail.com>
2021-05-01 11:23:43 -0400

    adds new response type

--- src/types/interactions/interaction_response_types.ts
+++ src/types/interactions/interaction_response_types.ts
@@ -8,6 +8,8 @@ export enum DiscordInteractionResponseTypes {
   DeferredChannelMessageWithSource = 5,
   /** It has no data fields. You can send this type **only in response to a button interaction .** It will acknowledge the interaction and update the button to a loading state. */
   DeferredMessageUpdate,
+  /** Can be sent in response to a button interaction to immediately update the message to which the button was attached. For example, to remove buttons on a message in response to a button click */
+  UpdateMessage,
 }
 
 export type InteractionResponseTypes = DiscordInteractionResponseTypes;
