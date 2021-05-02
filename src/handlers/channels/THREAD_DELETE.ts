df03334464a10e7610ad82b06014c5e3ff6d33e6
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-02 21:06:36 +0200

    use correct event handler function

--- src/handlers/channels/THREAD_DELETE.ts
+++ src/handlers/channels/THREAD_DELETE.ts
@@ -20,5 +20,5 @@ export async function handleThreadDelete(data: DiscordGatewayPayload) {
     }
   });
 
-  eventHandlers.channelDelete?.(cachedChannel);
+  eventHandlers.threadDelete?.(cachedChannel);
 }
