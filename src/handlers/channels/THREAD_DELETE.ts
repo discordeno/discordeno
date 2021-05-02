a094a1e7bd04520293f4e3cfbec8683db1ffbfb0
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-02 20:59:16 +0200

    add: THREAD_DELETE event handler

---
+++ src/handlers/channels/THREAD_DELETE.ts
@@ -0,0 +1,24 @@
+import { eventHandlers } from "../../bot.ts";
+import { cacheHandlers } from "../../cache.ts";
+import { Channel } from "../../types/channels/channel.ts";
+import { DiscordGatewayPayload } from "../../types/gateway/gateway_payload.ts";
+
+export async function handleThreadDelete(data: DiscordGatewayPayload) {
+  const payload = data.d as Channel;
+
+  const cachedChannel = await cacheHandlers.get("channels", payload.id);
+  if (!cachedChannel) return;
+
+  await cacheHandlers.delete("channels", payload.id);
+  cacheHandlers.forEach("messages", (message) => {
+    eventHandlers.debug?.(
+      "loop",
+      `Running forEach messages loop in CHANNEL_DELTE file.`,
+    );
+    if (message.channelId === payload.id) {
+      cacheHandlers.delete("messages", message.id);
+    }
+  });
+
+  eventHandlers.channelDelete?.(cachedChannel);
+}
