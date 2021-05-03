49eb18d848bc6f4ed7ce41953e46e8e2b363a757
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-03 21:46:17 +0200

    Update THREAD_DELETE.ts

--- src/handlers/channels/THREAD_DELETE.ts
+++ src/handlers/channels/THREAD_DELETE.ts
@@ -2,20 +2,24 @@ import { eventHandlers } from "../../bot.ts";
 import { cacheHandlers } from "../../cache.ts";
 import { Channel } from "../../types/channels/channel.ts";
 import { DiscordGatewayPayload } from "../../types/gateway/gateway_payload.ts";
+import { snowflakeToBigint } from "../../util/bigint.ts";
 
 export async function handleThreadDelete(data: DiscordGatewayPayload) {
   const payload = data.d as Channel;
 
-  const cachedChannel = await cacheHandlers.get("channels", payload.id);
+  const cachedChannel = await cacheHandlers.get(
+    "channels",
+    snowflakeToBigint(payload.id),
+  );
   if (!cachedChannel) return;
 
-  await cacheHandlers.delete("channels", payload.id);
+  await cacheHandlers.delete("channels", snowflakeToBigint(payload.id));
   cacheHandlers.forEach("messages", (message) => {
     eventHandlers.debug?.(
       "loop",
       `Running forEach messages loop in CHANNEL_DELTE file.`,
     );
-    if (message.channelId === payload.id) {
+    if (message.channelId === snowflakeToBigint(payload.id)) {
       cacheHandlers.delete("messages", message.id);
     }
   });
