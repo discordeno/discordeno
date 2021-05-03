781a3e71c073ce4c99d1e680443a0e2990f3a974
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-03 21:46:30 +0200

    Update THREAD_UPDATE.ts

--- src/handlers/channels/THREAD_UPDATE.ts
+++ src/handlers/channels/THREAD_UPDATE.ts
@@ -3,10 +3,14 @@ import { cacheHandlers } from "../../cache.ts";
 import { structures } from "../../structures/mod.ts";
 import { Channel } from "../../types/channels/channel.ts";
 import { DiscordGatewayPayload } from "../../types/gateway/gateway_payload.ts";
+import { snowflakeToBigint } from "../../util/bigint.ts";
 
 export async function handleThreadUpdate(data: DiscordGatewayPayload) {
   const payload = data.d as Channel;
-  const oldChannel = await cacheHandlers.get("channels", payload.id);
+  const oldChannel = await cacheHandlers.get(
+    "channels",
+    snowflakeToBigint(payload.id),
+  );
   if (!oldChannel) return;
 
   const discordenoChannel = await structures.createDiscordenoChannel(payload);
