9e05a0c4fa0111ecd7e9d3b3b9859658ceaa972b
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-03 21:46:46 +0200

    add: new thread events

--- src/handlers/mod.ts
+++ src/handlers/mod.ts
@@ -2,6 +2,12 @@ import { handleChannelCreate } from "./channels/CHANNEL_CREATE.ts";
 import { handleChannelDelete } from "./channels/CHANNEL_DELETE.ts";
 import { handleChannelPinsUpdate } from "./channels/CHANNEL_PINS_UPDATE.ts";
 import { handleChannelUpdate } from "./channels/CHANNEL_UPDATE.ts";
+import { handleThreadCreate } from "./channels/THREAD_CREATE.ts";
+import { handleThreadDelete } from "./channels/THREAD_DELETE.ts";
+import { handleThreadListSync } from "./channels/THREAD_LIST_SYNC.ts";
+import { handleThreadMembersUpdate } from "./channels/THREAD_MEMBERS_UPDATE.ts";
+import { handleThreadMemberUpdate } from "./channels/THREAD_MEMBER_UPDATE.ts";
+import { handleThreadUpdate } from "./channels/THREAD_UPDATE.ts";
 import { handleApplicationCommandCreate } from "./commands/APPLICATION_COMMAND_CREATE.ts";
 import { handleApplicationCommandDelete } from "./commands/APPLICATION_COMMAND_DELETE.ts";
 import { handleApplicationCommandUpdate } from "./commands/APPLICATION_COMMAND_UPDATE.ts";
@@ -77,6 +83,12 @@ export {
   handleMessageUpdate,
   handlePresenceUpdate,
   handleReady,
+  handleThreadCreate,
+  handleThreadDelete,
+  handleThreadListSync,
+  handleThreadMembersUpdate,
+  handleThreadMemberUpdate,
+  handleThreadUpdate,
   handleTypingStart,
   handleUserUpdate,
   handleVoiceServerUpdate,
@@ -92,6 +104,12 @@ export let handlers = {
   CHANNEL_DELETE: handleChannelDelete,
   CHANNEL_PINS_UPDATE: handleChannelPinsUpdate,
   CHANNEL_UPDATE: handleChannelUpdate,
+  THREAD_CREATE: handleThreadCreate,
+  THREAD_UPDATE: handleThreadUpdate,
+  THREAD_DELETE: handleThreadDelete,
+  THREAD_LIST_SYNC: handleThreadListSync,
+  THREAD_MEMBER_UPDATE: handleThreadMemberUpdate,
+  THREAD_MEMBERS_UPDATE: handleThreadMembersUpdate,
   // commands
   APPLICATION_COMMAND_CREATE: handleApplicationCommandCreate,
   APPLICATION_COMMAND_DELETE: handleApplicationCommandDelete,
