d87d66649eff3270420ba3ed1bbcda4afed0f6c4
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-04 21:02:16 +0200

    big to small

--- src/types/guilds/system_channel_flags.ts
+++ src/types/guilds/system_channel_flags.ts
@@ -1,11 +1,11 @@
 /** https://discord.com/developers/docs/resources/guild#guild-object-system-channel-flags */
 export enum DiscordSystemChannelFlags {
   /** Suppress member join notifications */
-  SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0,
+  SuppressJoinNotifications = 1 << 0,
   /** Suppress server boost notifications */
-  SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1,
+  SuppressPremiumSubscriptions = 1 << 1,
   /** Suppress server setup tips */
-  SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2,
+  SuppressGuildReminderNotifications = 1 << 2,
 }
 
 export type SystemChannelFlags = DiscordSystemChannelFlags;
