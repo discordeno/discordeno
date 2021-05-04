38b8ea5532f5995b969f92e4a2ee6f7ca11820cf
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-04 22:06:40 +0200

    fix: imports use type

--- src/helpers/webhooks/get_webhook_message.ts
+++ src/helpers/webhooks/get_webhook_message.ts
@@ -1,6 +1,6 @@
 import { rest } from "../../rest/rest.ts";
 import { structures } from "../../structures/mod.ts";
-import { Message } from "../../types/messages/message.ts";
+import type { Message } from "../../types/messages/message.ts";
 import { endpoints } from "../../util/constants.ts";
 
 /** Returns a previousy-sent webhook message from the same token. Returns a message object on success. */
