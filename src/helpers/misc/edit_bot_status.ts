fb8035c4eff3a42e531a8ebb0583636f6667debe
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-05-06 15:28:42 +0200

    change: move closeWS & sendShardMessage to ws object

--- src/helpers/misc/edit_bot_status.ts
+++ src/helpers/misc/edit_bot_status.ts
@@ -1,7 +1,6 @@
 import { eventHandlers } from "../../bot.ts";
 import { DiscordGatewayOpcodes } from "../../types/codes/gateway_opcodes.ts";
 import type { StatusUpdate } from "../../types/gateway/status_update.ts";
-import { sendShardMessage } from "../../ws/send_shard_message.ts";
 import { ws } from "../../ws/ws.ts";
 
 export function editBotStatus(data: Omit<StatusUpdate, "afk" | "since">) {
@@ -11,7 +10,7 @@ export function editBotStatus(data: Omit<StatusUpdate, "afk" | "since">) {
       `Running forEach loop in editBotStatus function.`,
     );
 
-    sendShardMessage(shard, {
+    ws.sendShardMessage(shard, {
       op: DiscordGatewayOpcodes.StatusUpdate,
       d: {
         since: null,
