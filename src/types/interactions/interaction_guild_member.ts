d645b99287b8f082a7172c504becad9478f5e17f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-06 15:55:22 +0000

    fix interaction.member type

--- src/types/interactions/interaction_guild_member.ts
+++ src/types/interactions/interaction_guild_member.ts
@@ -1,7 +1,7 @@
-import { GuildMember } from "../members/guild_member.ts";
+import { GuildMemberWithUser } from "../members/guild_member.ts";
 
 /** https://discord.com/developers/docs/resources/guild#guild-member-object */
-export interface InteractionGuildMember extends GuildMember {
+export interface InteractionGuildMember extends GuildMemberWithUser {
   /** Total permissions of the member in the channel, including overrides, returned when in the interaction object */
   permissions: string;
 }
