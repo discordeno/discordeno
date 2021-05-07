a32f5b5ece59706e9e47bb8f13b29a6d66553736
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-07 12:57:36 +0000

    convert hashes to big ints + bools

--- src/helpers/guilds/guild_banner_url.ts
+++ src/helpers/guilds/guild_banner_url.ts
@@ -1,16 +1,30 @@
 import type { DiscordImageFormat } from "../../types/misc/image_format.ts";
 import type { DiscordImageSize } from "../../types/misc/image_size.ts";
 import { endpoints } from "../../util/constants.ts";
+import { iconBigintToHash } from "../../util/hash.ts";
 import { formatImageURL } from "../../util/utils.ts";
 
 /** The full URL of the banner from Discords CDN. Undefined if no banner is set. */
 export function guildBannerURL(
   id: bigint,
-  banner?: string,
-  size: DiscordImageSize = 128,
-  format?: DiscordImageFormat,
+  options: {
+    banner?: string | bigint;
+    size?: DiscordImageSize;
+    format?: DiscordImageFormat;
+    animated?: boolean;
+  },
 ) {
-  return banner
-    ? formatImageURL(endpoints.GUILD_BANNER(id, banner), size, format)
+  return options.banner
+    ? formatImageURL(
+      endpoints.GUILD_BANNER(
+        id,
+        typeof options.banner === "string" ? options.banner : iconBigintToHash(
+          options.banner,
+          options.animated ?? true,
+        ),
+      ),
+      options.size || 128,
+      options.format,
+    )
     : undefined;
 }
