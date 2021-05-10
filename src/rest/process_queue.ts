8192dd1f24a938f210f1c15987bacfe80a3deb5f
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-10 17:36:24 +0400

    fix(rest): reject a string instead of object

--- src/rest/process_queue.ts
+++ src/rest/process_queue.ts
@@ -119,10 +119,7 @@ export async function processQueue(id: string) {
             break;
         }
 
-        queuedRequest.request.reject?.({
-          status: response.status,
-          body: JSON.stringify({ error }),
-        });
+        queuedRequest.request.reject?.(`[${response.status}] ${error}`);
 
         // If Rate limited should not remove from queue
         if (response.status !== 429) queue.shift();
