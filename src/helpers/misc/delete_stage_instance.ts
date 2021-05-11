31536b03a3196306655f541ee2aaa32a54d7fe64
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-11 23:50:44 +0400

    Update src/helpers/misc/delete_stage_instance.ts
    Co-authored-by: ITOH <to@itoh.at>

--- src/helpers/misc/delete_stage_instance.ts
+++ src/helpers/misc/delete_stage_instance.ts
@@ -3,8 +3,7 @@ import { endpoints } from "../../util/constants.ts";
 
 /** Deletes the Stage instance. Requires the user to be a moderator of the Stage channel. */
 export async function deleteStageInstance(channelId: bigint) {
-  // deno-lint-ignore ban-types
-  return await rest.runMethod<{}>(
+  return await rest.runMethod<undefined>(
     "delete",
     endpoints.STAGE_INSTANCE(channelId),
   );
