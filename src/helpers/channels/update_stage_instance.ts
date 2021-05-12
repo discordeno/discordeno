14e0eaac99b96ad5451df54de5fa00656c432c6e
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-12 13:56:57 +0400

    Add permissions checl

--- src/helpers/channels/update_stage_instance.ts
+++ src/helpers/channels/update_stage_instance.ts
@@ -3,9 +3,25 @@ import { Errors } from "../../types/discordeno/errors.ts";
 import { StageInstance } from "../../types/channels/stage_instance.ts";
 import { endpoints } from "../../util/constants.ts";
 import { validateLength } from "../../util/validate_length.ts";
+import { cacheHandlers } from "../../cache.ts";
+import { requireBotChannelPermissions } from "../../util/permissions.ts";
+import { ChannelTypes } from "../../types/channels/channel_types.ts";
 
 /** Updates fields of an existing Stage instance. Requires the user to be a moderator of the Stage channel. */
 export async function updateStageInstance(channelId: bigint, topic: string) {
+  const channel = await cacheHandlers.get("channels", channelId);
+  if (!channel) throw new Error(Errors.CHANNEL_NOT_FOUND);
+
+  if (channel.type !== ChannelTypes.GuildStageVoice) {
+    throw new Error(Errors.CHANNEL_NOT_STAGE_VOICE);
+  }
+
+  await requireBotChannelPermissions(channel, [
+    "MOVE_MEMBERS",
+    "MUTE_MEMBERS",
+    "MANAGE_CHANNELS",
+  ]);
+
   if (
     !validateLength(topic, {
       min: 1,
