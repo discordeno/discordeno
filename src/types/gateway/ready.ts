5f2e0973e5866b8d011e545e1a2a9eed40015146
ITOH <to@itoh.at>
2021-05-15 11:40:53 +0200

    fix: application object got moved

--- src/types/gateway/ready.ts
+++ src/types/gateway/ready.ts
@@ -1,5 +1,5 @@
 import { UnavailableGuild } from "../guilds/unavailable_guild.ts";
-import { Application } from "../oauth2/application.ts";
+import { Application } from "../applications/application.ts";
 import { User } from "../users/user.ts";
 import { SnakeCasedPropertiesDeep } from "../util.ts";
 
