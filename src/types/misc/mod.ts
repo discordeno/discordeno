3bd93409706b61698487fd150908c5290b465a94
itohatweb <itohatweb@users.noreply.github.com>
2021-05-15 10:28:28 +0000

    Commit from GitHub Actions (Lint)

--- src/types/misc/mod.ts
+++ src/types/misc/mod.ts
@@ -1,3 +1,3 @@
-export * from "./image_format.ts"
-export * from "./image_size.ts"
-export * from "./typing_start.ts"
+export * from "./image_format.ts";
+export * from "./image_size.ts";
+export * from "./typing_start.ts";
