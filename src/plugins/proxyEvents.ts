727a0597b50c275095684e841d6da58dc3917d5c
ITOH <to@itoh.at>
2021-05-16 22:07:21 +0200

    don't import from mod.ts

--- src/plugins/proxyEvents.ts
+++ src/plugins/proxyEvents.ts
@@ -1,10 +1,15 @@
-import { EventHandlers, overloadEventHandlers, _eventHandlers } from "../../mod.ts";
 import type { EventEmitter } from "https://deno.land/std@0.96.0/node/events.ts";
+import { _eventHandlers, overloadEventHandlers } from "../bot.ts";
+import { EventHandlers } from "../types/discordeno/eventHandlers.ts";
 
 export function proxyEvent(emitter: EventEmitter) {
-    overloadEventHandlers(new Proxy(_eventHandlers, {
-        get(target, prop: keyof EventHandlers) {
-            return target[prop] !== undefined ? target[prop] : ((...args: unknown[]) => emitter.emit(prop, ...args));
-        },
-    }));
+  overloadEventHandlers(
+    new Proxy(_eventHandlers, {
+      get(target, prop: keyof EventHandlers) {
+        return target[prop] !== undefined
+          ? target[prop]
+          : ((...args: unknown[]) => emitter.emit(prop, ...args));
+      },
+    }),
+  );
 }
