9ce1cfaf8857cc99194323f44efc2404ae9160fc
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-16 11:38:06 +0400

    feat: add stage instances events

---
+++ src/handlers/channels/STAGE_INSTANCE_UPDATE.ts
@@ -0,0 +1,26 @@
+import { eventHandlers } from "../../bot.ts";
+import { cacheHandlers } from "../../cache.ts";
+import type { StageInstance } from "../../types/channels/stage_instance.ts";
+import type { DiscordGatewayPayload } from "../../types/gateway/gateway_payload.ts";
+import { snowflakeToBigint } from "../../util/bigint.ts";
+
+export async function handleStageInstanceUpdate(data: DiscordGatewayPayload) {
+  const { channelId, guildId, ...rest } = data.d as StageInstance;
+
+  const cachedGuild = await cacheHandlers.get(
+    "guilds",
+    snowflakeToBigint(guildId),
+  );
+  if (!cachedGuild) return;
+  const cachedChannel = await cacheHandlers.get(
+    "channels",
+    snowflakeToBigint(channelId),
+  );
+  if (!cachedChannel) return;
+
+  eventHandlers.stageInstanceUpdate?.({
+    ...rest,
+    channel: cachedChannel,
+    guild: cachedGuild,
+  });
+}
