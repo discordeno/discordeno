a518b5b7d41a5e330c42b79190a62023fd24e38b
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-16 13:16:01 +0400

    Update src/types/discordeno/stage_instance.ts

--- src/types/discordeno/stage_instance.ts
+++ src/types/discordeno/stage_instance.ts
@@ -1,6 +1,6 @@
 import type { DiscordenoChannel } from "../../structures/channel.ts";
 import type { DiscordenoGuild } from "../../structures/guild.ts";
-import { StageInstance } from "../channels/stage_instance.ts";
+import type { StageInstance } from "../channels/stage_instance.ts";
 
 export interface DiscordenoStageInstance
   extends Omit<StageInstance, "guildId" | "channelId"> {
