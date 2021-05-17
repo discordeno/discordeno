3b4c7b86c8facb7d7ba4531de019689939724376
rigormorrtiss <rigormorrtiss@users.noreply.github.com>
2021-05-17 08:17:32 +0000

    Commit from GitHub Actions (Lint)

--- src/plugins/proxyEvents.ts
+++ src/plugins/proxyEvents.ts
@@ -1,11 +1,15 @@
-import { overloadEventHandlers, _eventHandlers } from "../bot.ts";
+import { _eventHandlers, overloadEventHandlers } from "../bot.ts";
 import type { EventHandlerFunctions } from "../types/discordeno/eventHandlers.ts";
 import type { EventEmitter } from "https://deno.land/std@0.96.0/node/events.ts";
 
 export function proxyEvent(emitter: EventEmitter) {
-  overloadEventHandlers(new Proxy(_eventHandlers, {
-    get(target, prop: keyof EventHandlerFunctions) {
-      return target[prop] !== undefined ? target[prop] : ((...args: unknown[]) => emitter.emit(prop, ...args));
-    },
-  }));
+  overloadEventHandlers(
+    new Proxy(_eventHandlers, {
+      get(target, prop: keyof EventHandlerFunctions) {
+        return target[prop] !== undefined
+          ? target[prop]
+          : ((...args: unknown[]) => emitter.emit(prop, ...args));
+      },
+    }),
+  );
 }
