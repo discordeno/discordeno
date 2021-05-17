9382e2e921729935835c7f8f4b9e915276c1dca8
QuantumlyTangled <ethoarx@gmail.com>
2021-05-17 14:43:33 +0200

    fix: event handlers reference

--- src/plugins/proxyEvents.ts
+++ src/plugins/proxyEvents.ts
@@ -1,9 +1,9 @@
-import { overloadEventHandlers, _eventHandlers } from "../bot.ts";
+import { eventHandlers, replaceEventHandlers } from "../bot.ts";
 import type { EventHandlerFunctions } from "../types/discordeno/eventHandlers.ts";
 import type { EventEmitter } from "https://deno.land/std@0.96.0/node/events.ts";
 
 export function proxyEvent(emitter: EventEmitter) {
-  overloadEventHandlers(new Proxy(_eventHandlers, {
+  replaceEventHandlers(new Proxy(eventHandlers, {
     get(target, prop: keyof EventHandlerFunctions) {
       return target[prop] !== undefined ? target[prop] : ((...args: unknown[]) => emitter.emit(prop, ...args));
     },
