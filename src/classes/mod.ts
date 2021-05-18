ed97b95bf6c939a010f00e0cbfb865f62b300894
Skillz <gankstarsleague@gmail.com>
2021-05-18 11:29:30 -0400

    add exports

--- src/classes/mod.ts
+++ src/classes/mod.ts
@@ -1,3 +1,4 @@
+export * from './structures/mod.ts';
 export * from "./Client.ts";
 export * from "./types/mod.ts";
 export * from "./structures/mod.ts";
--- src/classes/structures/mod.ts
+++ src/classes/structures/mod.ts
@@ -1,2 +1,4 @@
+export * from "./guilds/mod.ts";
+export * from "./Base.ts";
 export * from "./GatewayManager.ts";
 export * from "./RestManager.ts";
