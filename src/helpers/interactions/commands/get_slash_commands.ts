4c98b73cd0baf61be8bc314d871312229fff5ec1
ITOH <to@itoh.at>
2021-05-21 18:40:25 +0200

    change: use bigint for ids

--- src/helpers/interactions/commands/get_slash_commands.ts
+++ src/helpers/interactions/commands/get_slash_commands.ts
@@ -1,6 +1,7 @@
 import { applicationId } from "../../../bot.ts";
 import { rest } from "../../../rest/rest.ts";
 import type { ApplicationCommand } from "../../../types/interactions/commands/application_command.ts";
+import { snowflakeToBigint } from "../../../util/bigint.ts";
 import { Collection } from "../../../util/collection.ts";
 import { endpoints } from "../../../util/constants.ts";
 
@@ -11,5 +12,10 @@ export async function getSlashCommands(guildId?: bigint) {
     guildId ? endpoints.COMMANDS_GUILD(applicationId, guildId) : endpoints.COMMANDS(applicationId)
   );
 
-  return new Collection(result.map((command) => [command.name, command]));
+  return new Collection(
+    result.map((command) => [
+      command.name,
+      { ...command, id: snowflakeToBigint(command.id), applicationId: snowflakeToBigint(command.applicationId) },
+    ])
+  );
 }
