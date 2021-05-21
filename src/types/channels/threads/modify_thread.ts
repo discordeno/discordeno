2521aecd9bd18b94af76f4ac6e61655184036868
ITOH <to@itoh.at>
2021-05-21 18:34:41 +0200

    change(types): ModifyThread.locked has been renamed to onlyModeratorsUnarchive

--- src/types/channels/threads/modify_thread.ts
+++ src/types/channels/threads/modify_thread.ts
@@ -7,7 +7,7 @@ export interface ModifyThread {
   /** Duration in minutes to automatically archive the thread after recent activity */
   autoArchiveDuration?: 60 | 1440 | 4320 | 10080;
   /** When a thread is locked, only users with `MANAGE_THREADS` can unarchive it */
-  locked?: boolean;
+  onlyModeratorsUnarchive?: boolean;
   /** Amount of seconds a user has to wait before sending another message (0-21600); bots, as well as users with the permission `MANAGE_MESSAGES`, `MANAGE_THREAD` or `MANAGE_CHANNEL` are unaffected */
   rateLimitPerUser?: number;
 }
