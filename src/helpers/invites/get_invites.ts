97e37da29a5b3adfcd0affd8e07c045588c9d591
ITOH <to@itoh.at>
2021-05-22 17:57:56 +0200

    fix: getInvites return type

--- src/helpers/invites/get_invites.ts
+++ src/helpers/invites/get_invites.ts
@@ -1,5 +1,5 @@
 import { rest } from "../../rest/rest.ts";
-import type { Invite } from "../../types/invites/invite.ts";
+import type { InviteMetadata } from "../../types/invites/invite_metadata.ts";
 import { Collection } from "../../util/collection.ts";
 import { endpoints } from "../../util/constants.ts";
 import { requireBotGuildPermissions } from "../../util/permissions.ts";
@@ -8,7 +8,7 @@ import { requireBotGuildPermissions } from "../../util/permissions.ts";
 export async function getInvites(guildId: bigint) {
   await requireBotGuildPermissions(guildId, ["MANAGE_GUILD"]);
 
-  const result = await rest.runMethod<Invite[]>("get", endpoints.GUILD_INVITES(guildId));
+  const result = await rest.runMethod<InviteMetadata[]>("get", endpoints.GUILD_INVITES(guildId));
 
   return new Collection(result.map((invite) => [invite.code, invite]));
 }
