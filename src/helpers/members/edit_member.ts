1f0446f42d4b05a5b9d70ab06fa5ed35e96a9686
ITOH <to@itoh.at>
2021-05-25 09:12:13 +0200

    remove unnecessary null check

--- src/helpers/members/edit_member.ts
+++ src/helpers/members/edit_member.ts
@@ -23,7 +23,7 @@ export async function editMember(guildId: bigint, memberId: bigint, options: Mod
 
   if (options.roles) requiredPerms.add("MANAGE_ROLES");
 
-  if (options.mute !== undefined || options.deaf !== undefined || options.channelId || options.channelId === null) {
+  if (options.mute !== undefined || options.deaf !== undefined || options.channelId !== undefined) {
     const memberVoiceState = (await cacheHandlers.get("guilds", guildId))?.voiceStates.get(memberId);
 
     if (!memberVoiceState?.channelId) {
