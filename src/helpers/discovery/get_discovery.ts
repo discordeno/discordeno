283dbf6695d1ad77c7319ee519d315f30fb4af67
rigormorrtiss <rigormorrtiss@gmail.com>
2021-05-26 18:18:13 +0400

    Use requireBotGuildPermissions()

--- src/helpers/discovery/get_discovery.ts
+++ src/helpers/discovery/get_discovery.ts
@@ -1,14 +1,11 @@
 import { rest } from "../../rest/rest.ts";
-import { Errors } from "../../types/discordeno/errors.ts";
 import type { DiscoveryMetadata } from "../../types/discovery/discovery_metadata.ts";
 import { endpoints } from "../../util/constants.ts";
-import { botHasGuildPermissions } from "../../util/permissions.ts";
+import { requireBotGuildPermissions } from "../../util/permissions.ts";
 
 /** Returns the discovery metadata object for the guild. Requires the `MANAGE_GUILD` permission. */
 export async function getDiscovery(guildId: bigint) {
-  if (!(await botHasGuildPermissions(guildId, ["MANAGE_GUILD"]))) {
-    throw new Error(Errors.MISSING_MANAGE_GUILD);
-  }
+  await requireBotGuildPermissions(guildId, ["MANAGE_GUILD"]);
 
   return await rest.runMethod<DiscoveryMetadata>("get", endpoints.DISCOVERY_METADATA(guildId));
 }
