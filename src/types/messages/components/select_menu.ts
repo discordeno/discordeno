e6faf72cc204a8ca74633d22a7924db68fe5dfd5
Skillz4Killz <Skillz4Killz@users.noreply.github.com>
2021-05-28 11:35:05 +0000

    change: prettier code

--- src/types/messages/components/select_menu.ts
+++ src/types/messages/components/select_menu.ts
@@ -1,14 +1,14 @@
 import { SelectOption } from "./select_option.ts";
 
 export interface SelectMenuComponent {
-    /** A custom identifier for this component. Maximum 100 characters. */
-    customId: string;
-    /** A custom placeholder text if nothing is selected. Maximum 100 characters. */
-    placeholder?: string;
-    /** The minimum number of items that must be selected. Default 1. Between 1-25. */
-    minValues?: number;
-    /** The maximum number of items that can be selected. Default 1. Between 1-25. */
-    maxValues?: number;
-    /** The choices! Maximum of 25 items. */
-    options: SelectOption[];
+  /** A custom identifier for this component. Maximum 100 characters. */
+  customId: string;
+  /** A custom placeholder text if nothing is selected. Maximum 100 characters. */
+  placeholder?: string;
+  /** The minimum number of items that must be selected. Default 1. Between 1-25. */
+  minValues?: number;
+  /** The maximum number of items that can be selected. Default 1. Between 1-25. */
+  maxValues?: number;
+  /** The choices! Maximum of 25 items. */
+  options: SelectOption[];
 }
