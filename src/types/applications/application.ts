8eba93a8779ebc5cdada36878ad41ded292453f7
rigormorrtiss <rigormorrtiss@gmail.com>
2021-06-03 08:08:38 +0400

    fix(types/applications): change Application#owner to optional

--- src/types/applications/application.ts
+++ src/types/applications/application.ts
@@ -23,7 +23,7 @@ export interface Application {
   /** The url of the app's privacy policy */
   privacyPolicyUrl?: string;
   /** Partial user object containing info on the owner of the application */
-  owner: Partial<User>;
+  owner?: Partial<User>;
   /** If this application is a game sold on Discord, this field will be the summary field for the store page of its primary sku */
   summary: string;
   /** The hex encoded key for verification in interactions and the GameSDK's GetTicket */
