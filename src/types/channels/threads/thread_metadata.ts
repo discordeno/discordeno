7b9da2cb1fce79ef11a9ccf0be4acb7f48404244
ITOH <to@itoh.at>
2021-06-04 16:20:27 +0200

    change: ThreadMetadata#archiverId has been removed

--- src/types/channels/threads/thread_metadata.ts
+++ src/types/channels/threads/thread_metadata.ts
@@ -1,8 +1,6 @@
 export interface ThreadMetadata {
   /** Whether the thread is archived */
   archived: boolean;
-  /** Id of the user that last archived or unarchived the thread */
-  archiverId?: string;
   /** Duration in minutes to automatically archive the thread after recent activity */
   autoArchiveDuration: 60 | 1440 | 4320 | 10080;
   // TODO(threads): channel struct should convert this to a unixx
