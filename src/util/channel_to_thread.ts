f2177e8ca6ac076c48b107ba085a0cffa4754ff3
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-06-13 13:34:18 +0000

    cache.threads

---
+++ src/util/channel_to_thread.ts
@@ -0,0 +1,20 @@
+import { Channel } from "../types/channels/channel.ts";
+import { snowflakeToBigint } from "./bigint.ts";
+
+export function channelToThread(channel: Channel) {
+  return {
+    id: snowflakeToBigint(channel.id),
+    channelId: snowflakeToBigint(channel.parentId!),
+    memberCount: channel.memberCount,
+    messageCount: channel.messageCount,
+    archived: channel.threadMetadata?.archived || false,
+    archiveTimestamp: channel.threadMetadata?.archiveTimestamp
+      ? Date.parse(channel.threadMetadata.archiveTimestamp)
+      : undefined,
+    archiverId: channel.threadMetadata?.archiverId ? snowflakeToBigint(channel.threadMetadata.archiverId) : undefined,
+    autoArchiveDuration: channel.threadMetadata?.autoArchiveDuration || 0,
+    locked: channel.threadMetadata?.locked || false,
+  };
+}
+
+export type Thread = ReturnType<typeof channelToThread>
