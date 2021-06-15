7784450f046176c594c2d99da2cb9eb0da4350b8
rigormorrtiss <rigormorrtiss@gmail.com>
2021-06-15 13:11:47 +0400

    fix(types): mark id, userId as optional in ThreadMember

--- src/types/channels/threads/thread_member.ts
+++ src/types/channels/threads/thread_member.ts
@@ -1,8 +1,8 @@
 export interface ThreadMember {
   /** The id of the thread */
-  id: string;
+  id?: string;
   /** The id of the user */
-  userId: string;
+  userId?: string;
   /** The time the current user last joined the thread */
   joinTimestamp: string;
   /** Any user-thread settings, currently only used for notifications */
