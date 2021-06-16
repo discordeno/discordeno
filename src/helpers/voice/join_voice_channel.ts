3b0e92592986c6c7c39db239104c376dab142f34
rigormorrtiss <rigormorrtiss@gmail.com>
2021-06-16 22:16:50 +0400

    .

--- src/helpers/voice/join_voice_channel.ts
+++ src/helpers/voice/join_voice_channel.ts
@@ -1,20 +1,19 @@
 import { DiscordGatewayOpcodes } from "../../types/codes/gateway_opcodes.ts";
 import type { UpdateVoiceState } from "../../types/voice/update_voice_state.ts";
 import { requireBotChannelPermissions } from "../../util/permissions.ts";
-import { sendShardMessage } from "../../ws/send_shard_message.ts";
 import { calculateShardId } from "../../util/calculate_shard_id.ts";
 import { snakelize } from "../../util/utils.ts";
+import { ws } from "../../ws/ws.ts";
 
 export async function connectToVoiceChannel(
   guildId: bigint,
   channelId: bigint,
-  options?: Omit<UpdateVoiceState, "guildId" | "channelId">
+  options: Omit<UpdateVoiceState, "guildId" | "channelId">
 ) {
-  if (!options?.selfDeaf) options?.selfDeaf = true;
-
+  if (options && !options.selfDeaf) options.selfDeaf ??= true;
   await requireBotChannelPermissions(channelId, ["CONNECT", "VIEW_CHANNEL"]);
 
-  sendShardMessage(calculateShardId(guildId), {
+  ws.sendShardMessage(calculateShardId(guildId), {
     op: DiscordGatewayOpcodes.VoiceStateUpdate,
     d: snakelize<UpdateVoiceState>({ guildId, channelId, ...options }),
   });
