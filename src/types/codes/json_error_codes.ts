515074acf7cdd693e59047ca1944f797008fd759
rigormorrtiss <rigormorrtiss@gmail.com>
2021-07-07 08:44:54 +0400

    refactor(types): fix typo in DiscordJsonErrorCodes enum

--- src/types/codes/json_error_codes.ts
+++ src/types/codes/json_error_codes.ts
@@ -33,7 +33,7 @@ export enum DiscordJsonErrorCodes {
   UnknownInteraction = 10062,
   UnknownApplicationCommand = 10063,
   UnknownApplicationCommandPermissions = 10066,
-  UnknownGuildMemberVerficationForm = 10068,
+  UnknownGuildMemberVerificationForm = 10068,
   BotsCannotUseThisEndpoint = 20001,
   OnlyBotsCanUseThisEndpoint,
   ExplicitContentCannotBeSentToTheDesiredRecipient = 20009,
