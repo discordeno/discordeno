5886a19989b98eb727e236997331243e2156aa95
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-07-22 18:35:32 +0000

    fix: allow type in upsert

--- src/types/interactions/commands/edit_global_application_command.ts
+++ src/types/interactions/commands/edit_global_application_command.ts
@@ -1,4 +1,5 @@
 import { ApplicationCommandOption } from "./application_command_option.ts";
+import { ApplicationCommandTypes } from "./application_command_types.ts";
 
 /** https://discord.com/developers/docs/interactions/slash-commands#edit-global-application-command-json-params */
 export interface EditGlobalApplicationCommand {
@@ -6,6 +7,8 @@ export interface EditGlobalApplicationCommand {
   name?: string;
   /** 1-100 character description */
   description?: string;
+  /** The type of the command */
+  type?: ApplicationCommandTypes;
   /** The parameters for the command */
   options?: ApplicationCommandOption[] | null;
 }
