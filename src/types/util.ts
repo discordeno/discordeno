5bb6b2626ebc9f59ca364cca5efb5a2213017191
ITOH <to@itoh.at>
2021-08-15 12:03:14 +0200

    add(types): MakeRequired util

--- src/types/util.ts
+++ src/types/util.ts
@@ -151,3 +151,5 @@ export type CamelCasedPropertiesDeep<Value> = Value extends Function
     };
 
 export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+
+export type MakeRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
