3037cb1be57ea9c85606409b365309eb2641cb4f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-09-12 19:26:13 +0000

    Revert "fix: potential fix for #1036"
    This reverts commit 69c18b8c4c0f9a7683c3a7ddd4d9eb4bc38d99ab.

--- src/handlers/interactions/INTERACTION_CREATE.ts
+++ src/handlers/interactions/INTERACTION_CREATE.ts
@@ -1,44 +1,15 @@
 import { eventHandlers } from "../../bot.ts";
 import { cacheHandlers } from "../../cache.ts";
-import { createDiscordenoMessage, DiscordenoMessage } from "../../structures/message.ts";
 import { structures } from "../../structures/mod.ts";
 import type { DiscordGatewayPayload } from "../../types/gateway/gateway_payload.ts";
-import type { BigInteraction, Interaction } from "../../types/interactions/interaction.ts";
+import type { Interaction } from "../../types/interactions/interaction.ts";
 import type { GuildMemberWithUser } from "../../types/members/guild_member.ts";
 import { snowflakeToBigint } from "../../util/bigint.ts";
 
 export async function handleInteractionCreate(data: DiscordGatewayPayload) {
-  const basePayload = data.d as Interaction;
-
-  const payload = {
-    ...basePayload,
-    id: snowflakeToBigint(basePayload.id),
-    applicationId: snowflakeToBigint(basePayload.applicationId),
-    guildId: basePayload.guildId ? snowflakeToBigint(basePayload.guildId) : undefined,
-    channelId: basePayload.channelId ? snowflakeToBigint(basePayload.channelId) : undefined,
-    member: basePayload.member
-      ? {
-          ...basePayload.member,
-          roles: basePayload.member.roles.map((id) => snowflakeToBigint(id)),
-          user: {
-            ...basePayload.member.user,
-            id: snowflakeToBigint(basePayload.member.user.id),
-          },
-        }
-      : undefined,
-    user: basePayload.user
-      ? {
-          ...basePayload.user,
-          id: snowflakeToBigint(basePayload.user.id),
-        }
-      : undefined,
-    message: basePayload.message ? createDiscordenoMessage(basePayload.message) : undefined,
-  } as BigInteraction;
-
-  if (payload.member) payload.user = payload.member.user;
-
+  const payload = data.d as Interaction;
   const discordenoMember = payload.guildId
-    ? await structures.createDiscordenoMember(payload.member as GuildMemberWithUser, payload.guildId)
+    ? await structures.createDiscordenoMember(payload.member as GuildMemberWithUser, snowflakeToBigint(payload.guildId))
     : undefined;
 
   if (discordenoMember) {
--- src/types/interactions/interaction.ts
+++ src/types/interactions/interaction.ts
@@ -5,7 +5,6 @@ import { InteractionGuildMember } from "./interaction_guild_member.ts";
 import { DiscordInteractionTypes } from "./interaction_types.ts";
 import { SelectMenuData } from "../messages/components/select_data.ts";
 import { ButtonData } from "../messages/components/button_data.ts";
-import { DiscordenoMessage } from "../../structures/message.ts";
 
 /** https://discord.com/developers/docs/interactions/slash-commands#interaction */
 export type Interaction = PingInteraction | SlashCommandInteraction | ComponentInteraction;
@@ -48,30 +47,4 @@ export interface BaseInteraction<
   message?: Message;
 
   data?: D;
-}
-
-export interface BigInteraction
-  extends Omit<Interaction, "id" | "applicationId" | "guildId" | "channelId" | "member" | "user" | "message"> {
-  /** Id of the interaction */
-  id: bigint;
-  /** Id of the application this interaction is for */
-  applicationId: bigint;
-  /** The guild it was sent from */
-  guildId?: bigint;
-  /** The channel it was sent from */
-  channelId?: bigint;
-  /** Guild member data for the invoking user, including permissions */
-  member?: Omit<InteractionGuildMember, "roles" | "user"> & {
-    /** Array of role object ids */
-    roles: bigint[];
-    /** The user this guild member represents */
-    user: Omit<User, "id"> & {
-      /** The user's id */
-      id: bigint;
-    };
-  };
-  /** User object for the invoking user, if invoked in a DM */
-  user: Omit<User, "id"> & { id: bigint };
-  /** For the message the button was attached to */
-  message?: DiscordenoMessage;
 }
