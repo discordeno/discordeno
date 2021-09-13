7642eab7bd5bb7cb7e10a005b2c97160630feaf3
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-09-13 02:28:47 +0000

    fix: prevent sending dm to bot itself #1103

--- src/helpers/members/send_direct_message.ts
+++ src/helpers/members/send_direct_message.ts
@@ -1,13 +1,17 @@
+import { botId } from "../../bot.ts";
 import { cacheHandlers } from "../../cache.ts";
 import { rest } from "../../rest/rest.ts";
 import { structures } from "../../structures/mod.ts";
 import type { Channel } from "../../types/channels/channel.ts";
 import type { CreateMessage } from "../../types/messages/create_message.ts";
+import { Errors } from "../../types/mod.ts";
 import { endpoints } from "../../util/constants.ts";
 import { sendMessage } from "../messages/send_message.ts";
 
 /** Send a message to a users DM. Note: this takes 2 API calls. 1 is to fetch the users dm channel. 2 is to send a message to that channel. */
 export async function sendDirectMessage(memberId: bigint, content: string | CreateMessage) {
+  if (memberId === botId) throw new Error(Errors.YOU_CAN_NOT_DM_THE_BOT_ITSELF);
+  
   let dmChannel = await cacheHandlers.get("channels", memberId);
   if (!dmChannel) {
     // If not available in cache create a new one.
