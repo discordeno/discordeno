de2b12b2bbe0ba0d0d4d3842d3d5ee24f1d3be73
Skillz4Killz <Skillz4Killz@users.noreply.github.com>
2021-10-09 12:43:25 +0000

    change: prettier code

--- src/transformers/role.ts
+++ src/transformers/role.ts
@@ -1,22 +1,24 @@
 import { Bot } from "../bot.ts";
 import { Role } from "../types/mod.ts";
 
-export function transformRole(bot: Bot, payload: { role: Role } & {
-      guildId: bigint;
-    }) {
-      return {
-        // TODO: decide if its better to spread like this or do manually
-        ...payload,
-        // UNTRANSFORMED STUFF HERE
-        // TODO: decide if we should use spread above or do manually
-        // name: payload.role.name,
-        
-        // TRANSFORMED STUFF BELOW
-        id: bot.transformers.snowflake(payload.role.id),
-        botId: payload.role.tags?.botId ? bot.transformers.snowflake(payload.role.tags?.botId) : undefined,
-      }
-}
+export function transformRole(
+  bot: Bot,
+  payload: { role: Role } & {
+    guildId: bigint;
+  }
+) {
+  return {
+    // TODO: decide if its better to spread like this or do manually
+    ...payload,
+    // UNTRANSFORMED STUFF HERE
+    // TODO: decide if we should use spread above or do manually
+    // name: payload.role.name,
 
+    // TRANSFORMED STUFF BELOW
+    id: bot.transformers.snowflake(payload.role.id),
+    botId: payload.role.tags?.botId ? bot.transformers.snowflake(payload.role.tags?.botId) : undefined,
+  };
+}
 
 export interface DiscordenoRole extends Omit<Role, "tags" | "id" | "permissions"> {
   /** The role id */
