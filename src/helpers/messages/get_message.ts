7cfef637829a7d7ec432b0ee2a5209e36ba3d81d
TriForMine <TriForMine@users.noreply.github.com>
2021-10-15 12:54:39 +0000

    change: prettier code

--- src/helpers/messages/get_message.ts
+++ src/helpers/messages/get_message.ts
@@ -1,7 +1,7 @@
 import { cacheHandlers } from "../../cache.ts";
 import type { Message } from "../../types/messages/message.ts";
-import {Bot} from "../../bot.ts";
-import {SnakeCasedPropertiesDeep} from "../../types/util.ts";
+import { Bot } from "../../bot.ts";
+import { SnakeCasedPropertiesDeep } from "../../types/util.ts";
 
 /** Fetch a single message from the server. Requires VIEW_CHANNEL and READ_MESSAGE_HISTORY */
 export async function getMessage(bot: Bot, channelId: bigint, id: bigint) {
@@ -9,7 +9,11 @@ export async function getMessage(bot: Bot, channelId: bigint, id: bigint) {
     await bot.utils.requireBotChannelPermissions(channelId, ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]);
   }
 
-  const result = await bot.rest.runMethod<SnakeCasedPropertiesDeep<Message>>(bot.rest,"get", bot.constants.endpoints.CHANNEL_MESSAGE(channelId, id));
+  const result = await bot.rest.runMethod<SnakeCasedPropertiesDeep<Message>>(
+    bot.rest,
+    "get",
+    bot.constants.endpoints.CHANNEL_MESSAGE(channelId, id)
+  );
 
   return await bot.transformers.message(bot, result);
 }
