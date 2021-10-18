5c82abc3d23eaf007ad26317d058f3042efffbd8
TriForMine <quentin.nicolini@hotmail.fr>
2021-10-18 21:20:57 +0200

    Discovery

--- src/helpers/discovery/edit_discovery.ts
+++ src/helpers/discovery/edit_discovery.ts
@@ -1,13 +1,15 @@
-import { rest } from "../../rest/rest.ts";
 import type { DiscoveryMetadata } from "../../types/discovery/discovery_metadata.ts";
 import type { ModifyGuildDiscoveryMetadata } from "../../types/discovery/modify_guild_discovery_metadata.ts";
-import { endpoints } from "../../util/constants.ts";
-import { requireBotGuildPermissions } from "../../util/permissions.ts";
-import { snakelize } from "../../util/utils.ts";
+import {SnakeCasedPropertiesDeep} from "../../types/util.ts";
+import {Bot} from "../../bot.ts";
 
 /** Modify the discovery metadata for the guild. Requires the MANAGE_GUILD permission. Returns the updated discovery metadata object on success. */
-export async function editDiscovery(guildId: bigint, data: ModifyGuildDiscoveryMetadata) {
-  await requireBotGuildPermissions(guildId, ["MANAGE_GUILD"]);
+export async function editDiscovery(bot: Bot, guildId: bigint, data: ModifyGuildDiscoveryMetadata) {
+  await bot.utils.requireBotGuildPermissions(guildId, ["MANAGE_GUILD"]);
 
-  return await rest.runMethod<DiscoveryMetadata>("patch", endpoints.DISCOVERY_METADATA(guildId), snakelize(data));
+  return await bot.rest.runMethod<SnakeCasedPropertiesDeep<DiscoveryMetadata>>(bot.rest,"patch", bot.constants.endpoints.DISCOVERY_METADATA(guildId), {
+    primary_category_id: data.primaryCategoryId,
+    keywords: data.keywords,
+    emoji_discoverability_enabled: data.emojiDiscoverabilityEnabled,
+  });
 }
--- src/helpers/discovery/valid_discovery_term.ts
+++ src/helpers/discovery/valid_discovery_term.ts
@@ -1,9 +1,9 @@
-import { rest } from "../../rest/rest.ts";
 import type { ValidateDiscoverySearchTerm } from "../../types/discovery/validate_discovery_search_term.ts";
-import { endpoints } from "../../util/constants.ts";
+import {Bot} from "../../bot.ts";
+import {SnakeCasedPropertiesDeep} from "../../types/util.ts";
 
-export async function validDiscoveryTerm(term: string) {
-  const result = await rest.runMethod<ValidateDiscoverySearchTerm>("get", endpoints.DISCOVERY_VALID_TERM, { term });
+export async function validDiscoveryTerm(bot: Bot, term: string) {
+  const result = await bot.rest.runMethod<SnakeCasedPropertiesDeep<ValidateDiscoverySearchTerm>>("get", bot.constants.endpoints.DISCOVERY_VALID_TERM, { term });
 
   return result.valid;
 }
