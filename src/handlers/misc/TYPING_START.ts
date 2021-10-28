e30ffdd475c5210396bf27a1b2842df0c7c1d630
Skillz4Killz <Skillz4Killz@users.noreply.github.com>
2021-10-28 16:44:42 +0000

    change: prettier code

--- src/handlers/misc/TYPING_START.ts
+++ src/handlers/misc/TYPING_START.ts
@@ -7,8 +7,8 @@ export function handleTypingStart(bot: Bot, data: DiscordGatewayPayload) {
   const payload = data.d as SnakeCasedPropertiesDeep<TypingStart>;
 
   const guildId = payload.guild_id ? bot.transformers.snowflake(payload.guild_id) : undefined;
-  const userId = bot.transformers.snowflake(payload.user_id)
-  
+  const userId = bot.transformers.snowflake(payload.user_id);
+
   bot.events.typingStart(bot, {
     guildId,
     channelId: bot.transformers.snowflake(payload.channel_id),
