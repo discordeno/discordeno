72babf43ba783c34f79982f280ce99b5fb97f904
Quentin Nicolini <quentin.nicolini@hotmail.fr>
2021-10-30 13:39:47 +0200

    [Unit Test] messages: addReaction

---
+++ tests/helpers/messages/addReactions.ts
@@ -0,0 +1,26 @@
+import { Bot } from "../../../src/bot.ts";
+import { assertExists } from "../../deps.ts";
+import { delayUntil } from "../../utils.ts";
+
+export async function addReactionsTest(bot: Bot, channelId: bigint, t: Deno.TestContext) {
+  const message = await bot.helpers.sendMessage(channelId, "Hello World!");
+
+  // Assertions
+  assertExists(message);
+  // Delay the execution by to allow MESSAGE_CREATE event to be processed
+  await delayUntil(10000, () => bot.cache.messages.has(message.id));
+  // Make sure the message was created.
+  if (!bot.cache.messages.has(message.id)) {
+    throw new Error("The message seemed to be sent but it was not cached.");
+  }
+
+  // Delete the message now
+  await bot.helpers.addReactions(channelId, message.id, ['☑️', '❌']);
+
+  // Wait to give it time for MESSAGE_UPDATE event
+  await delayUntil(10000, async () => ((await bot.cache.messages.get(message.id))?.reactions?.length ?? 0) === 2);
+  // Make sure it is gone from cache
+  if ((await bot.cache.messages.get(message.id))?.reactions?.length === 2) {
+    throw new Error("The message should have got a reaction but it doesn't have any.");
+  }
+}
--- tests/mod.ts
+++ tests/mod.ts
@@ -6,6 +6,7 @@ import { delayUntil } from "./utils.ts";
 
 // CONDUCT LOCAL TESTS FIRST BEFORE RUNNING API TEST
 import "./local.ts";
+import {addReactionsTest} from "./helpers/messages/addReactions.ts";
 
 Deno.test("[Bot] - Starting Tests", async (t) => {
   // CHANGE TO TRUE WHEN DEBUGGING SANITIZATION ERRORS
@@ -101,6 +102,13 @@ Deno.test("[Bot] - Starting Tests", async (t) => {
           name: "[message] delete message with a reason",
           fn: async (t) => {
             await deleteMessageWithReasonTest(bot, channel.id, t);
+          },
+          ...sanitizeMode,
+        }),
+        t.step({
+          name: "[message] add reactions to a message",
+          fn: async (t) => {
+            await addReactionsTest(bot, channel.id, t);
           },
           ...sanitizeMode,
         }),
