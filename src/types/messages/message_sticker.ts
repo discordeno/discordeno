2e4531f3ba17a17a0bea504a173ef4678c46cdbb
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-08 20:32:07 +0000

    Closes #1549

--- src/types/messages/message_sticker.ts
+++ src/types/messages/message_sticker.ts
@@ -11,14 +11,8 @@ export interface MessageSticker {
   name: string;
   /** Description of the sticker */
   description: string;
-  /** For guild stickers, a unicode emoji representing the sticker's expression. For Nitro stickers, a comma-separated list of related expressions */
+  /** a unicode emoji representing the sticker's expression */
   tags: string;
-  /**
-   * Sticker asset hash
-   * Note: The URL for fetching sticker assets is currently private.
-   * @deprecated the value of the asset field will an empty string.
-   */
-  asset: string;
   /** Type of sticker format */
   formatType: DiscordMessageStickerFormatTypes;
   /**  Whether or not the sticker is available */
