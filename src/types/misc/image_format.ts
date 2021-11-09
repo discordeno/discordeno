48c29eeba709603bfc178d801111c142ba380d3a
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-09 00:55:03 +0000

    add followups

--- src/types/misc/image_format.ts
+++ src/types/misc/image_format.ts
@@ -1,2 +1,5 @@
-/** https://discord.com/developers/docs/reference#image-formatting */
-export type DiscordImageFormat = "jpg" | "jpeg" | "png" | "webp" | "gif";
+/**
+ * https://discord.com/developers/docs/reference#image-formatting
+ * json is only for stickers
+ */
+export type DiscordImageFormat = "jpg" | "jpeg" | "png" | "webp" | "gif" | "json";
