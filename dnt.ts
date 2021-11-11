9cdb922a7bca77c9214cf947d37bcef7def34f34
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-11 22:03:42 +0000

    prepare dnt

---
+++ dnt.ts
@@ -0,0 +1,21 @@
+import { build } from "https://deno.land/x/dnt@0.6.0/mod.ts";
+import { DISCORDENO_VERSION } from "./mod.ts";
+
+await Deno.remove("npm", { recursive: true }).catch((_) => {});
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  typeCheck: false,
+  declaration: true,
+  test: false,
+  package: {
+    name: "discordeno",
+    version: DISCORDENO_VERSION,
+  },
+  compilerOptions: { target: "ES2020" },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
