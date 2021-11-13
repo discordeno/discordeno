62ed4460bcef34b2bceaf8a171751e6f79a555cc
Yaikava <83710104+Yaikava@users.noreply.github.com>
2021-11-13 20:30:44 +0100

    Update wasm dep

--- src/ws/deps.ts
+++ src/ws/deps.ts
@@ -1,1 +1,1 @@
-export { decompress_with as decompressWith } from "https://unpkg.com/@evan/wasm@0.0.65/target/zlib/deno.js";
+export { decompress_with as decompressWith } from "https://unpkg.com/@evan/wasm@0.0.92/target/zlib/deno.js";
