fdd600e88b1573b286f39ae8f7216f2fb965ae96
ITOH <to@itoh.at>
2021-11-14 20:36:23 +0100

    Update banMember.ts

--- src/helpers/members/banMember.ts
+++ src/helpers/members/banMember.ts
@@ -2,7 +2,7 @@ import type { CreateGuildBan } from "../../types/guilds/create_guild_ban.ts";
 import type { Bot } from "../../bot.ts";
 
 /** Ban a user from the guild and optionally delete previous messages sent by the user. Requires the BAN_MEMBERS permission. */
-export async function ban(bot: Bot, guildId: bigint, id: bigint, options?: CreateGuildBan) {
+export async function banMember(bot: Bot, guildId: bigint, id: bigint, options?: CreateGuildBan) {
   return await bot.rest.runMethod<undefined>(
     bot.rest,
     "put",
