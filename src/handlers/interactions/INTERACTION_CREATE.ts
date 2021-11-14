ec80d8caf1370498553a252d32c0c6a7cde8e46c
ITOH <to@itoh.at>
2021-11-14 17:37:19 +0100

    add new interactions to unrepliedInteractions

--- src/handlers/interactions/INTERACTION_CREATE.ts
+++ src/handlers/interactions/INTERACTION_CREATE.ts
@@ -4,5 +4,6 @@ import type { Interaction } from "../../types/interactions/interaction.ts";
 import { SnakeCasedPropertiesDeep } from "../../types/util.ts";
 
 export async function handleInteractionCreate(bot: Bot, data: DiscordGatewayPayload) {
+  bot.cache.unrepliedInteractions.add(bot.transformers.snowflake((data.d as SnakeCasedPropertiesDeep<Interaction>).id));
   bot.events.interactionCreate(bot, bot.transformers.interaction(bot, data.d as SnakeCasedPropertiesDeep<Interaction>));
 }
