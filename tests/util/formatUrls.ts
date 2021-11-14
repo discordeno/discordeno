4c6b489670d0ec73dcee0bc24a9d9fc4e8372c8f
ITOH <to@itoh.at>
2021-11-14 18:53:20 +0100

    skillz.is.pro

--- tests/util/formatUrls.ts
+++ tests/util/formatUrls.ts
@@ -4,9 +4,9 @@ import { assertEquals } from "../deps.ts";
 Deno.test({
   name: "[utils] format image url",
   fn() {
-    assertEquals(formatImageURL(`https://skillz.com`), "https://skillz.com.jpg?size=128");
-    assertEquals(formatImageURL(`https://skillz.com`, 1024), "https://skillz.com.jpg?size=1024");
-    assertEquals(formatImageURL(`https://skillz.com`, 1024, "gif"), "https://skillz.com.gif?size=1024");
-    assertEquals(formatImageURL(`https://skillz.com`, undefined, "gif"), "https://skillz.com.gif?size=128");
+    assertEquals(formatImageURL(`https://skillz.is.pro`), "https://skillz.is.pro.jpg?size=128");
+    assertEquals(formatImageURL(`https://skillz.is.pro`, 1024), "https://skillz.is.pro.jpg?size=1024");
+    assertEquals(formatImageURL(`https://skillz.is.pro`, 1024, "gif"), "https://skillz.is.pro.gif?size=1024");
+    assertEquals(formatImageURL(`https://skillz.is.pro`, undefined, "gif"), "https://skillz.is.pro.gif?size=128");
   },
 });
