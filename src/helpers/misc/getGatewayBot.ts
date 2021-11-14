ddc67a284cdbbc46a0be5b852df2c12cedd429a3
itohatweb <itohatweb@users.noreply.github.com>
2021-11-14 17:31:51 +0000

    change: prettier code

--- src/helpers/misc/getGatewayBot.ts
+++ src/helpers/misc/getGatewayBot.ts
@@ -4,11 +4,7 @@ import { SnakeCasedPropertiesDeep } from "../../types/util.ts";
 
 /** Get the bots Gateway metadata that can help during the operation of large or sharded bots. */
 export async function getGatewayBot(bot: Bot): Promise<GetGatewayBot> {
-  const result = await bot.rest.runMethod<GetGatewayBot>(
-    bot.rest,
-    "get",
-    bot.constants.endpoints.GATEWAY_BOT
-  );
+  const result = await bot.rest.runMethod<GetGatewayBot>(bot.rest, "get", bot.constants.endpoints.GATEWAY_BOT);
 
   return {
     url: result.url,
