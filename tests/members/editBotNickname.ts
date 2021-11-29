1259bda5e0c514e709c062b7aa346bcd6ee68163
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-29 14:19:12 +0000

    fix: edit bot nickname name

--- tests/members/editBotNickname.ts
+++ tests/members/editBotNickname.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "../deps.ts";
 import { bot, guild } from "../mod.ts";
 
-Deno.test("editBotNickname", async function () {
+Deno.test("[misc] edit a bot's nickname", async function () {
   const nick = "lts20050703";
   const nickname = await bot.helpers.editBotNickname(guild.id, { nick });
   assertEquals(nickname, nick);
