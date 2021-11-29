fb18dfe5d10f5601d1ec30d9fb941784c163084f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-29 14:20:09 +0000

    fix: camelize response to discord

--- src/helpers/misc/editBotStatus.ts
+++ src/helpers/misc/editBotStatus.ts
@@ -11,7 +11,46 @@ export function editBotStatus(bot: Bot, data: Omit<StatusUpdate, "afk" | "since"
       d: {
         since: null,
         afk: false,
-        activities: data.activities,
+        activities: data.activities.map((activity) => ({
+          name: activity.name,
+          type: activity.type,
+          url: activity.url,
+          created_at: activity.createdAt,
+          timestamps: activity.timestamps
+            ? {
+                start: activity.timestamps.start,
+                end: activity.timestamps.end,
+              }
+            : undefined,
+          applicationId: activity.applicationId?.toString(),
+          details: activity.details,
+          state: activity.state,
+          emoji: activity.emoji
+            ? {
+                name: activity.emoji.name,
+                id: activity.emoji.id?.toString(),
+                animated: activity.emoji.animated,
+              }
+            : undefined,
+          party: activity.party
+            ? {
+                id: activity.party.id?.toString(),
+                size: activity.party.size,
+              }
+            : undefined,
+          assets: activity.assets
+            ? {
+                large_image: activity.assets.largeImage,
+                large_text: activity.assets.largeText,
+                small_image: activity.assets.smallImage,
+                small_text: activity.assets.smallText,
+              }
+            : undefined,
+          secrets: activity.secrets,
+          instance: activity.instance,
+          flags: activity.flags,
+          buttons: activity.buttons,
+        })),
         status: data.status,
       },
     });
