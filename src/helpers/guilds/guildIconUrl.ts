13f2ab30ca674c3ebec72a0eba31e30cb22c805e
a boi here <41384430+some-boi@users.noreply.github.com>
2021-12-14 16:05:11 +0100

    Update guildIconUrl.ts

--- src/helpers/guilds/guildIconUrl.ts
+++ src/helpers/guilds/guildIconUrl.ts
@@ -6,7 +6,7 @@ import type { Bot } from "../../bot.ts";
 export function guildIconURL(
   bot: Bot,
   id: bigint,
-  options: {
+  options?: {
     icon?: string | bigint;
     size?: ImageSize;
     format?: ImageFormat;
