6455e3727e85014b6f81603fee0ea8e8efbae020
a boi here <41384430+some-boi@users.noreply.github.com>
2021-12-20 19:02:55 +0100

    Update avatarUrl.ts

--- src/helpers/members/avatarUrl.ts
+++ src/helpers/members/avatarUrl.ts
@@ -8,19 +8,19 @@ export function avatarURL(
   userId: bigint,
   discriminator: number,
   options?: {
-    avatar: string | bigint | undefined;
+    avatar: bigint | undefined;
     size?: ImageSize;
     format?: ImageFormat;
   }
 ) {
-  return options.avatar
+  return options?.avatar
     ? bot.utils.formatImageURL(
         bot.constants.endpoints.USER_AVATAR(
           userId,
-          typeof options.avatar === "string" ? options.avatar : bot.utils.iconBigintToHash(options.avatar)
+          typeof options?.avatar === "string" ? options.avatar : bot.utils.iconBigintToHash(options?.avatar)
         ),
-        options.size || 128,
-        options.format
+        options?.size || 128,
+        options?.format
       )
     : bot.constants.endpoints.USER_DEFAULT_AVATAR(Number(discriminator) % 5);
 }
