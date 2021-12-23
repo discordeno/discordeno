502a5193ad1e0b9bb9b1c9150462f9811aaeabe1
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2021-12-23 10:02:06 +0700

    #1848, move `DiscordenoUser` and `transformUser` from `src/transformers/member.ts` to `/src/transformers/user.ts`
    #1848 Document audit logs for scheduled events
    
    move `DiscordenoUser` interface and `transformUser` function from `src/transformers/member.ts` to `/src/transformers/user.ts`
    
    add `export * from "./user.ts"` to /src/transformers/mod.ts

--- src/transformers/member.ts
+++ src/transformers/member.ts
@@ -1,43 +1,6 @@
 import type { Bot } from "../bot.ts";
 import type { GuildMember } from "../types/members/guildMember.ts";
-import type { PremiumTypes } from "../types/users/premiumTypes.ts";
-import type { User } from "../types/users/user.ts";
-import type { UserFlags } from "../types/users/userFlags.ts";
 import type { SnakeCasedPropertiesDeep } from "../types/util.ts";
-
-export interface DiscordenoUser {
-  id: bigint;
-  username: string;
-  discriminator: number;
-  avatar?: bigint;
-  bot?: boolean;
-  system?: boolean;
-  locale?: string;
-  verified?: boolean;
-  email?: string | null;
-  flags?: UserFlags;
-  mfaEnabled?: boolean;
-  premiumType?: PremiumTypes;
-  publicFlags?: UserFlags;
-}
-
-export function transformUser(bot: Bot, payload: SnakeCasedPropertiesDeep<User>): DiscordenoUser {
-  return {
-    id: bot.transformers.snowflake(payload.id || ""),
-    username: payload.username,
-    discriminator: Number(payload.discriminator),
-    avatar: payload.avatar ? bot.utils.iconHashToBigInt(payload.avatar) : undefined,
-    bot: payload.bot,
-    system: payload.system,
-    locale: payload.locale,
-    verified: payload.verified,
-    email: payload.email,
-    flags: payload.flags,
-    mfaEnabled: payload.mfa_enabled,
-    premiumType: payload.premium_type,
-    publicFlags: payload.public_flags,
-  };
-}
 
 export function transformMember(
   bot: Bot,
