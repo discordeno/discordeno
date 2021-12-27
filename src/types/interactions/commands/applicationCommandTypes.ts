c6595ffefee8394f87839e8f3cdf5d1473385584
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-12-27 11:03:55 -0700

    Add descriptions for ApplicationCommandTypes

--- src/types/interactions/commands/applicationCommandTypes.ts
+++ src/types/interactions/commands/applicationCommandTypes.ts
@@ -1,5 +1,8 @@
 export enum ApplicationCommandTypes {
+  /** A text-based command that shows up when a user types `/` */
   ChatInput = 1,
+  /** A UI-based command that shows up when you right click or tap on a user */
   User,
+  /** A UI-based command that shows up when you right click or tap on a message */
   Message,
 }
