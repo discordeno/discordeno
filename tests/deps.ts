6fdc03b24a28af03590176e48aed13cd5a22111d
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2021-12-28 22:00:20 +0700

    fix delete invite test

--- tests/deps.ts
+++ tests/deps.ts
@@ -4,7 +4,8 @@ export {
   assertExists,
   assertNotEquals,
   assertThrows,
-  assertThrowsAsync
+  assertThrowsAsync,
+  assertRejects,
 } from "https://deno.land/std@0.115.1/testing/asserts.ts";
 export * from "https://deno.land/x/discordeno_cache_plugin@0.0.21/mod.ts";
 export * from "https://deno.land/x/discordeno_permissions_plugin@0.0.15/mod.ts";
