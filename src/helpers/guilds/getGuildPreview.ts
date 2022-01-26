fa929935b710a79bbb67356abb5ad86d5a694c28
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-01-26 17:47:59 +0700

    update getGuildPreview

--- src/helpers/guilds/getGuildPreview.ts
+++ src/helpers/guilds/getGuildPreview.ts
@@ -20,5 +20,6 @@ export async function getGuildPreview(bot: Bot, guildId: bigint) {
     approximateMemberCount: result.approximate_member_count,
     approximatePresenceCount: result.approximate_presence_count,
     description: result.description ?? undefined,
+    stickers: result.stickers,
   };
 }
