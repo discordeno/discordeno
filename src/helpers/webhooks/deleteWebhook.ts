9e3195c3d4d4cda131b8394296d29219a4cb13f7
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-01-26 17:22:59 +0700

    BREAKING CHANGE remove unused deleteWebhook function channelId parameter

--- src/helpers/webhooks/deleteWebhook.ts
+++ src/helpers/webhooks/deleteWebhook.ts
@@ -1,6 +1,6 @@
 import type { Bot } from "../../bot.ts";
 
 /** Delete a webhook permanently. Requires the `MANAGE_WEBHOOKS` permission. Returns a undefined on success */
-export async function deleteWebhook(bot: Bot, channelId: bigint, webhookId: bigint, reason?: string) {
+export async function deleteWebhook(bot: Bot, webhookId: bigint, reason?: string) {
   await bot.rest.runMethod<undefined>(bot.rest, "delete", bot.constants.endpoints.WEBHOOK_ID(webhookId), { reason });
 }
