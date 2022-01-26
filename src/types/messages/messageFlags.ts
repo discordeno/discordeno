6b86250546de5caa9d009b525ac722b7ecb29ea4
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-01-26 16:49:59 +0700

    Document message flag FAILED_TO_MENTION_SOME_ROLES_IN_THREAD

--- src/types/messages/messageFlags.ts
+++ src/types/messages/messageFlags.ts
@@ -16,4 +16,6 @@ export enum MessageFlags {
   Empheral = 1 << 6,
   /** This message is an Interaction Response and the bot is "thinking" */
   Loading = 1 << 7,
+  /** This message failed to mention some roles and add their members to the thread */
+  FailedToMentionSomeRolesInThread = 1 << 8,
 }
