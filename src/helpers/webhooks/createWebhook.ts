b7d50a1ed4a10dc85d935a62bc3658a527597699
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-01-28 17:47:20 +0700

    Do not use spread syntax ... in createWebhook and editWebhook

--- src/helpers/webhooks/createWebhook.ts
+++ src/helpers/webhooks/createWebhook.ts
@@ -13,8 +13,9 @@ export async function createWebhook(bot: Bot, channelId: bigint, options: Create
     "post",
     bot.constants.endpoints.CHANNEL_WEBHOOKS(channelId),
     {
-      ...options,
+      name: options.name,
       avatar: options.avatar ? await bot.utils.urlToBase64(options.avatar) : undefined,
+      reason: options.reason,
     }
   );
 
--- src/helpers/webhooks/editWebhook.ts
+++ src/helpers/webhooks/editWebhook.ts
@@ -5,8 +5,10 @@ import type { Bot } from "../../bot.ts";
 /** Edit a webhook. Requires the `MANAGE_WEBHOOKS` permission. Returns the updated webhook object on success. */
 export async function editWebhook(bot: Bot, webhookId: bigint, options: ModifyWebhook) {
   const result = await bot.rest.runMethod<Webhook>(bot.rest, "patch", bot.constants.endpoints.WEBHOOK_ID(webhookId), {
-    ...options,
+    name: options.name,
+    avatar: options.avatar,
     channel_id: options.channelId,
+    reason: options.reason,
   });
 
   return bot.transformers.webhook(bot, result);
