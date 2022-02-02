4af0e51e63613a78f63ad2e4f0d55cfa3e2e37d9
ITOH <to@itoh.at>
2022-02-02 22:52:39 +0100

    Update docusaurus.config.js

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -25,13 +25,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
           editUrl: "https://github.com/discordeno/discordeno/tree/main/site/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/discordeno/discordeno/tree/main/site/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
