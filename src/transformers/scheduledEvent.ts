52d73f8fc83ec8c6c93f7349b4ae0c48ad9e299a
lts20050703 <87189679+lts20050703@users.noreply.github.com>
2022-02-02 11:04:57 +0700

    using undefined instead of null
    Co-authored-by: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- src/transformers/scheduledEvent.ts
+++ src/transformers/scheduledEvent.ts
@@ -29,7 +29,7 @@ export function transformScheduledEvent(
     entityType: payload.entity_type,
     userCount: payload.user_count || 0,
     location: payload.entity_metadata?.location,
-    image: payload.image ? bot.utils.iconHashToBigInt(payload.image) : null,
+    image: payload.image ? bot.utils.iconHashToBigInt(payload.image) : undefined,
   };
 }
 
@@ -65,5 +65,5 @@ export interface DiscordenoScheduledEvent {
   /** the number of users subscribed to the scheduled event */
   userCount: number;
   /** the cover image hash of the scheduled event */
-  image: bigint | null;
+  image?: bigint;
 }
