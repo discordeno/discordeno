f7a320d5d5c37dd47b305a0e795661af1ff196f3
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-02-07 00:32:04 +0000

    fix: update dnt and settings

--- dnt.ts
+++ dnt.ts
@@ -1,9 +1,63 @@
-import { build } from "https://deno.land/x/dnt@0.7.3/mod.ts";
+import { build } from "https://deno.land/x/dnt@0.17.0/mod.ts";
 
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 
 await build({
-  entryPoints: ["./mod.ts"],
+  shims: {
+    deno: true,
+    timers: true,
+    blob: true,
+    undici: true,
+    custom: [
+      {
+        package: {
+          name: "ws",
+          version: "^8.4.0",
+        },
+        globalNames: [
+          {
+            name: "WebSocket",
+            exportName: "default",
+          },
+        ],
+      },
+    ],
+  },
+  entryPoints: [
+    "./mod.ts",
+    {
+      name: "./rest",
+      path: "src/rest/mod.ts",
+    },
+    {
+      name: "./gateway",
+      path: "src/gateway/mod.ts",
+    },
+    {
+      name: "./types",
+      path: "src/types/mod.ts",
+    },
+    {
+      name: "./plugins",
+      path: "src/plugins/mod.ts",
+    },
+    {
+      name: "./cache-plugin",
+      path: "src/plugins/cache/mod.ts",
+    },
+    {
+      name: "./fileloader-plugin",
+      path: "src/plugins/fileloader/mod.ts",
+    },
+    {
+      name: "./helpers-plugin",
+      path: "src/plugins/helpers/mod.ts",
+    },
+    {
+      name: "./permissions-plugin",
+      path: "src/plugins/permissions/mod.ts",
+    },
+  ],
   outDir: "./npm",
   typeCheck: false,
   declaration: true,
@@ -11,6 +65,24 @@ await build({
   package: {
     name: "discordeno",
     version: Deno.args[0],
+    description: "Discordeno is simplistic, easy-to-use, versatile while being efficient and lightweight.",
+    keywords: [
+      "javascript",
+      "api",
+      "library",
+      "typescript",
+      "discord",
+      "discord-bot",
+      "discord-api",
+      "deno",
+      "discordeno",
+    ],
+    author: "Skillz4Killz",
+    license: "Apache License 2.0",
+    bugs: {
+      url: "https://github.com/discordeno/discordeno/issues",
+    },
+    homepage: "https://github.com/discordeno/discordeno#readme",
   },
   compilerOptions: { target: "ES2020" },
 });
---
+++ plugins/mod.ts
@@ -0,0 +1,4 @@
+export * from "./cache/mod.ts";
+export * from "./fileloader/mod.ts";
+export * from "./helpers/mod.ts";
+export * from "./permissions/mod.ts";
