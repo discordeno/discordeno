aee2e9f592b78a5ffe76d60de1053e80eea178cc
Yaikava <83710104+Yaikava@users.noreply.github.com>
2022-02-09 15:33:48 +0000

    fix - types - export inputTextComponent

--- src/types/messages/mod.ts
+++ src/types/messages/mod.ts
@@ -5,6 +5,7 @@ export * from "./components/actionRow.ts";
 export * from "./components/buttonComponent.ts";
 export * from "./components/buttonData.ts";
 export * from "./components/buttonStyles.ts";
+export * from "./components/inputTextComponent.ts";
 export * from "./components/messageComponentTypes.ts";
 export * from "./components/messageComponents.ts";
 export * from "./components/selectData.ts";
