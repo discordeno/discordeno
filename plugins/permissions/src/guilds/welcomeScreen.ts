5ba4ed773ebdf66efdd8b02c7c10eddfc7cd8654
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-02-11 06:35:07 +0000

    fix: require manage guild perm in welcome screen

--- plugins/permissions/src/guilds/welcomeScreen.ts
+++ plugins/permissions/src/guilds/welcomeScreen.ts
@@ -1,6 +1,16 @@
 import { BotWithCache } from "../../deps.ts";
 import { requireBotGuildPermissions } from "../permissions.ts";
 
+export function getWelcomeScreen(bot: BotWithCache) {
+  const getWelcomeScreenOld = bot.helpers.getWelcomeScreen;
+
+  bot.helpers.getWelcomeScreen = function (guildId) {
+    requireBotGuildPermissions(bot, guildId, ["MANAGE_GUILD"]);
+
+    return getWelcomeScreenOld(guildId);
+  };
+}
+
 export function editWelcomeScreen(bot: BotWithCache) {
   const editWelcomeScreenOld = bot.helpers.editWelcomeScreen;
 
