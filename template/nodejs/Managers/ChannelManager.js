8c68edcb24ae8b8334475189d90993d0bd6c464a
meister03 <root@_HOSTNAME_>
2022-02-11 22:35:16 +0100

    format files

--- template/nodejs/Managers/ChannelManager.js
+++ template/nodejs/Managers/ChannelManager.js
@@ -1,17 +1,17 @@
 const Channel = require("../Structures/Channel");
-class Channels{
-    constructor(client, data ={}, options = {}){
-        this.client = client;
-        
-        if(options.guild) this.guild  = options.guild;
-    }
+class Channels {
+  constructor(client, data = {}, options = {}) {
+    this.client = client;
 
-    async create(options = {}, reason){
-        return new Channel(this.client, options).create(options, reason);
-    }
+    if (options.guild) this.guild = options.guild;
+  }
 
-    forge(data = {}){
-        return new Channel(this.client, data)
-    }
+  async create(options = {}, reason) {
+    return new Channel(this.client, options).create(options, reason);
+  }
+
+  forge(data = {}) {
+    return new Channel(this.client, data);
+  }
 }
 module.exports = Channels;
