6c152c99bd1bb812dd5765924d5c419d03ea2756
ITOH <to@itoh.at>
2022-02-15 14:48:13 +0100

    deno fmt

--- template/nodejs/Structures/User.js
+++ template/nodejs/Structures/User.js
@@ -1,13 +1,13 @@
 const DestructObject = require("./DestructObject");
 
 class User extends DestructObject {
-    constructor(client, user = {}) {
-        super(user);
-        this.client = client;
-    }
+  constructor(client, user = {}) {
+    super(user);
+    this.client = client;
+  }
 
-    get tag() { 
-        return `#${this.username}#${this.discriminator}`
-    };
+  get tag() {
+    return `#${this.username}#${this.discriminator}`;
+  }
 }
 module.exports = User;
