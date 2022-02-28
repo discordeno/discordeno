97f2fbf9e77ac20e1567bd5aa11066f30b2a5438
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-02-28 13:40:01 -0500

    Update gateway/stopGateway.ts

--- gateway/stopGateway.ts
+++ gateway/stopGateway.ts
@@ -1,5 +1,6 @@
 /** Use this function to stop the gateway properly */
-export async function stopGateway(gateway: GatewayManager) {
+export async function stopGateway(gateway: GatewayManager, code = 3061, reason = "Discordeno Testing Finished! Do Not RESUME!") {
+
     // STOP WS
     gateway.shards.forEach((shard) => {
         clearInterval(shard.heartbeat.intervalId);
