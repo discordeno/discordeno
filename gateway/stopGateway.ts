87dd8635ca11ca0db1c2a88931562e6e8ad64629
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-03-02 06:00:11 +0000

    fix: stop gateway ts errors

--- gateway/stopGateway.ts
+++ gateway/stopGateway.ts
@@ -1,16 +1,21 @@
+import { delay } from "../util/utils.ts";
+import { GatewayManager } from "./gateway_manager.ts";
+
 /** Use this function to stop the gateway properly */
-export async function stopGateway(gateway: GatewayManager, code = 3061, reason = "Discordeno Testing Finished! Do Not RESUME!") {
+export async function stopGateway(
+  gateway: GatewayManager,
+  code = 3061,
+  reason = "Discordeno Testing Finished! Do Not RESUME!",
+) {
+  // STOP WS
+  gateway.shards.forEach((shard) => {
+    clearInterval(shard.heartbeat.intervalId);
+    gateway.closeWS(
+      shard.ws,
+      code,
+      reason,
+    );
+  });
 
-    // STOP WS
-    gateway.shards.forEach((shard) => {
-        clearInterval(shard.heartbeat.intervalId);
-        gateway.closeWS(
-          shard.ws,
-          code,
-
-          reason,
-        );
-      });
-
-    await delay(5000);
+  await delay(5000);
 }
