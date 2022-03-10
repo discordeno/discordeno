59ae7a16d53d49c67475ea9dcf265436334e2da8
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-03-10 21:57:43 +0000

    fix: cleanup stickers

--- types/discord.ts
+++ types/discord.ts
@@ -881,6 +881,24 @@ export interface DiscordStickerItem {
   format_type: StickerFormatTypes;
 }
 
+/** https://discord.com/developers/docs/resources/sticker#sticker-pack-object-sticker-pack-structure */
+export interface DiscordStickerPack {
+  /** id of the sticker pack */
+  id: string;
+  /** the stickers in the pack */
+  stickers: DiscordSticker[];
+  /** name of the sticker pack */
+  name: string;
+  /** id of the pack's SKU */
+  sku_id: string;
+  /** id of a sticker in the pack which is shown as the pack's icon */
+  cover_sticker_id?: string;
+  /** description of the sticker pack */
+  description: string;
+  /** id of the sticker pack's [banner image](https://discord.com/developers/docs/reference#image-formatting) */
+  banner_asset_id?: string;
+}
+
 export interface DiscordInteraction {
   /** Id of the interaction */
   id: string;
--- types/stickers/stickerFormatTypes.ts
+++
@@ -1,6 +0,0 @@
-/** https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-format-types */
-export enum StickerFormatTypes {
-  Png = 1,
-  Apng,
-  Lottie,
-}
