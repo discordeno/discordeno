18bf53ac6b95c0e197c20f46e9c34806e98f94c4
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-03-25 18:37:46 +0000

    fix: use latest kwik

--- template/bigbot/deps.ts
+++ template/bigbot/deps.ts
@@ -5,5 +5,5 @@ export * from "https://deno.land/x/discordeno@13.0.0-rc29/plugins/mod.ts";
 export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
 // Get data from .env files
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
-// export * from "https://deno.land/x/kwik@v1.2.0/mod.ts";
-export * from "https://raw.githubusercontent.com/TriForMine/kwik/4df9d84eefee7ff299cc65e0f31adb8dbee64cde/mod.ts";
+export * from "https://deno.land/x/kwik@v1.2.3/mod.ts";
+// export * from "https://raw.githubusercontent.com/TriForMine/kwik/4df9d84eefee7ff299cc65e0f31adb8dbee64cde/mod.ts";
