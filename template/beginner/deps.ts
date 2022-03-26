253781943ed434e4714a987dc9b4e2ff3fd1ef55
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-03-26 00:13:27 +0000

    release: rc31

--- template/beginner/deps.ts
+++ template/beginner/deps.ts
@@ -1,5 +1,5 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc30/mod.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc30/plugins/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc31/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc31/plugins/mod.ts";
 
 // Terminal Colors!
 export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
--- template/minimal/deps.ts
+++ template/minimal/deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc30/mod.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc30/plugins/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc31/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc31/plugins/mod.ts";
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
