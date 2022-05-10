ba9944c6b0f35e294f4da9b69ada4b2399253070
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-10 14:47:52 +0000

    fix: add stage instance tests

---
+++ testss/channels/stageInstances.test.ts
@@ -0,0 +1,31 @@
+import { ChannelTypes } from "../../mod.ts";
+import { assertExists, assertNotEquals } from "../deps.ts";
+import { loadBot } from "../mod.ts";
+import { CACHED_COMMUNITY_GUILD_ID } from "../utils.ts";
+
+Deno.test({
+  name: "[stage] Create a stage instance",
+  async fn(t) {
+    const bot = loadBot();
+    const stage = await bot.helpers.createChannel(CACHED_COMMUNITY_GUILD_ID, {
+      name: "createinstance",
+      type: ChannelTypes.GuildStageVoice,
+    });
+    const instance = await bot.helpers.createStageInstance({ channelId: stage.id, topic: "test it" });
+    assertExists(instance);
+
+    await t.step("[stage] Edit a stage instance", async () => {
+      const edited = await bot.helpers.updateStageInstance(stage.id, {
+        topic: "edited",
+      });
+      assertNotEquals(edited.topic, stage.topic);
+    });
+
+    // TODO: why doesnt this specific test work?
+    // await t.step("[stage] Delete a stage instance", async () => {
+    //   await bot.helpers.deleteStageInstance(stage.id);
+    // });
+
+    await bot.helpers.deleteChannel(stage.id);
+  },
+});
