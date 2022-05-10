ac1b8aa0a9a5bbc3d238f518d1c8059433bb1a4c
meister03 <69507874+meister03@users.noreply.github.com>
2022-05-10 14:48:16 +0200

    Update helpers/channels/forums/createForumPost.ts
    Use transformer
    
    Co-authored-by: LTS20050703 <87189679+lts20050703@users.noreply.github.com>

--- helpers/channels/forums/createForumPost.ts
+++ helpers/channels/forums/createForumPost.ts
@@ -31,61 +31,7 @@ export async function createForumPost(
         }
         : undefined,
       file: options.file,
-      components: options.components?.map((component) => ({
-        type: component.type,
-        components: component.components.map((subComponent) => {
-          if (subComponent.type === MessageComponentTypes.InputText) {
-            return {
-              type: subComponent.type,
-              style: subComponent.style,
-              custom_id: subComponent.customId,
-              label: subComponent.label,
-              placeholder: subComponent.placeholder,
-              min_length: subComponent.minLength ?? subComponent.required === false ? 0 : subComponent.minLength,
-              max_length: subComponent.maxLength,
-            };
-          }
-
-          if (subComponent.type === MessageComponentTypes.SelectMenu) {
-            return {
-              type: subComponent.type,
-              custom_id: subComponent.customId,
-              placeholder: subComponent.placeholder,
-              min_values: subComponent.minValues,
-              max_values: subComponent.maxValues,
-              options: subComponent.options.map((option) => ({
-                label: option.label,
-                value: option.value,
-                description: option.description,
-                emoji: option.emoji
-                  ? {
-                    id: option.emoji.id?.toString(),
-                    name: option.emoji.name,
-                    animated: option.emoji.animated,
-                  }
-                  : undefined,
-                default: option.default,
-              })),
-            };
-          }
-
-          return {
-            type: subComponent.type,
-            custom_id: subComponent.customId,
-            label: subComponent.label,
-            style: subComponent.style,
-            emoji: "emoji" in subComponent && subComponent.emoji
-              ? {
-                id: subComponent.emoji.id?.toString(),
-                name: subComponent.emoji.name,
-                animated: subComponent.emoji.animated,
-              }
-              : undefined,
-            url: "url" in subComponent ? subComponent.url : undefined,
-            disabled: "disabled" in subComponent ? subComponent.disabled : undefined,
-          };
-        }),
-      })),
+      components: options.components?.map((component) => bot.transformers.reverse.component(component)),
     }
   );
 
