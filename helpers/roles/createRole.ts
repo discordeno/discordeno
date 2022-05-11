00feb9aba234778ae2008c5b7189d63a1d2885a3
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-11 12:53:46 +0700

    update createRole

--- helpers/roles/createRole.ts
+++ helpers/roles/createRole.ts
@@ -1,15 +1,18 @@
 import type { Bot } from "../../bot.ts";
 import { DiscordRole } from "../../types/discord.ts";
 import { PermissionStrings } from "../../types/shared.ts";
+import { iconBigintToHash } from "../../util/hash.ts";
 
 /** Create a new role for the guild. Requires the MANAGE_ROLES permission. */
 export async function createRole(bot: Bot, guildId: bigint, options: CreateGuildRole, reason?: string) {
   const result = await bot.rest.runMethod<DiscordRole>(bot.rest, "post", bot.constants.endpoints.GUILD_ROLES(guildId), {
     name: options.name,
+    permissions: bot.utils.calculateBits(options?.permissions || []),
     color: options.color,
     hoist: options.hoist,
+    icon: options.icon ? iconBigintToHash(options.icon) : null,
+    unicode_emoji: options.unicodeEmoji,
     mentionable: options.mentionable,
-    permissions: bot.utils.calculateBits(options?.permissions || []),
     reason,
   });
 
@@ -28,8 +31,10 @@ export interface CreateGuildRole {
   color?: number;
   /** Whether the role should be displayed separately in the sidebar, default: false */
   hoist?: boolean;
-  /** Whether the role should be mentionable, default: false */
-  mentionable?: boolean;
+  /** the role's icon image (if the guild has the `ROLE_ICONS` feature) */
+  icon?: bigint;
   /** The role's unicode emoji (if the guild has the `ROLE_ICONS` feature) */
   unicodeEmoji?: string;
+  /** Whether the role should be mentionable, default: false */
+  mentionable?: boolean;
 }
