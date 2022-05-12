5ba529342bd5a1b001dea08125ed189f71075854
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-13 00:45:02 +0700

    fix getPruneCounts query string params

--- helpers/guilds/getPruneCount.ts
+++ helpers/guilds/getPruneCount.ts
@@ -3,23 +3,16 @@ import type { Bot } from "../../bot.ts";
 /** Check how many members would be removed from the server in a prune operation. Requires the KICK_MEMBERS permission */
 export async function getPruneCount(bot: Bot, guildId: bigint, options?: GetGuildPruneCountQuery) {
   if (options?.days && options.days < 1) throw new Error(bot.constants.Errors.PRUNE_MIN_DAYS);
-  if (options?.days && options.days > 30) {
-    throw new Error(bot.constants.Errors.PRUNE_MAX_DAYS);
-  }
+  if (options?.days && options.days > 30) throw new Error(bot.constants.Errors.PRUNE_MAX_DAYS);
 
-  let url = bot.constants.endpoints.GUILD_PRUNE(guildId);
-
-  if (options) {
-    url += "?";
-
-    if (options.days) url += `days=${options.days}`;
-    if (options.includeRoles) url += `&include_roles=${options.includeRoles}`;
-  }
+  const queries = [];
+  if (options?.days) queries.push(`days=${options.days}`);
+  if (options?.includeRoles) queries.push(`include_roles=${options.includeRoles}`);
 
   const result = await bot.rest.runMethod(
     bot.rest,
     "get",
-    url,
+    `${bot.constants.endpoints.GUILD_PRUNE(guildId)}${queries.length ? `?${queries.join("&")}` : ""}`,
   );
 
   return result.pruned as number;
@@ -30,5 +23,5 @@ export interface GetGuildPruneCountQuery {
   /** Number of days to count prune for (1 or more), default: 7 */
   days?: number;
   /** Role(s) to include, default: none */
-  includeRoles: string | string[];
+  includeRoles?: string | string[];
 }
