c3908e0a7b50c552947eed4897ace3b9669320c5
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-13 00:43:44 +0700

    fix getBans query string params

--- helpers/guilds/getBans.ts
+++ helpers/guilds/getBans.ts
@@ -4,13 +4,17 @@ import { DiscordBan } from "../../types/discord.ts";
 import { User } from "../../transformers/member.ts";
 
 /** Returns a list of ban objects for the users banned from this guild. Requires the BAN_MEMBERS permission. */
-export async function getBans(bot: Bot, guildId: bigint, options: GetBans) {
+export async function getBans(bot: Bot, guildId: bigint, options?: GetBans) {
+  const queries = [];
+
+  if (options?.limit) queries.push(`limit=${options.limit}`);
+  if (options?.after) queries.push(`after=${options.after}`);
+  if (options?.before) queries.push(`before=${options.before}`);
+
   const results = await bot.rest.runMethod<DiscordBan[]>(
     bot.rest,
     "get",
-    `${bot.constants.endpoints.GUILD_BANS(guildId)}?limit=${options.limit ?? 1000}&before=${
-      options.before ?? null
-    }&after=${options.after ?? null}`,
+    `${bot.constants.endpoints.GUILD_BANS(guildId)}${queries.length ? `?${queries.join("&")}` : ""}`,
   );
 
   return new Collection<bigint, { reason?: string; user: User }>(
