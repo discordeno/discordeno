453fd45ef7be41c4557a4d1bb61b4c5d406c321a
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-13 20:25:57 +0700

    localization updates (#2206)
    * localization updates
    
    Upstream: https://github.com/discord/discord-api-docs/commit/2f854d3fd675a9953d1bb8d6858a03c8a9642337
    
    Types: DiscordApplicationCommmand and DiscordApplicationCommandOption: Update documentation
    
    Types: DiscordActivityButton: Add documentation link
    
    Helper: upsertApplicationCommand: All fields are optional, but any fields provided will entirely overwrite the existing values of those fields.
    
    * revert unrelated changes
    
    * update SLASH_COMMANDS_NAME_REGEX
    
    * createApplicationCommand options: AtLeastOne instead of Partial
    
    Co-authored-by: ITOH <to@itoh.at>
    Co-authored-by: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- types/discord.ts
+++ types/discord.ts
@@ -1649,7 +1649,7 @@ export interface DiscordApplicationCommand {
   application_id: string;
   /** Guild id of the command, if not global */
   guild_id?: string;
-  /** 1-32 character name matching */
+  /** `ApplicationCommandTypes.ChatInput` command names must match the following regex `^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$` with the unicode flag set. If there is a lowercase variant of any letters used, you must use those. Characters with no lowercase variants and/or uncased letters are still allowed. `ApplicationCommandTypes.User` and `ApplicationCommandTypes.Message` commands may be mixed case and can include spaces. */
   name: string;
   /** Localization object for the `name` field. Values follow the same restrictions as `name` */
   name_localizations?: Localization | null;
@@ -1669,7 +1669,7 @@ export interface DiscordApplicationCommand {
 export interface DiscordApplicationCommandOption {
   /** Value of Application Command Option Type */
   type: ApplicationCommandOptionTypes;
-  /** 1-32 character name matching lowercase `^[\w-]{1,32}$` */
+  /** Command option name must match the following regex `^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$` with the unicode flag set. If there is a lowercase variant of any letters used, you must use those. Characters with no lowercase variants and/or uncased letters are still allowed. */
   name: string;
   /** Localization object for the `name` field. Values follow the same restrictions as `name` */
   name_localizations?: Localization | null;
