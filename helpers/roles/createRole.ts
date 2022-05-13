457d8e2454f2ac1c5b524069619d7158b2cbced8
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-13 15:27:05 +0700

    undo createRole changes https://github.com/discordeno/discordeno/pull/2195#pullrequestreview-971240431

--- helpers/roles/createRole.ts
+++ helpers/roles/createRole.ts
@@ -1,7 +1,6 @@
 import type { Bot } from "../../bot.ts";
 import { DiscordRole } from "../../types/discord.ts";
 import { PermissionStrings } from "../../types/shared.ts";
-import { iconBigintToHash } from "../../util/hash.ts";
 
 /** Create a new role for the guild. Requires the MANAGE_ROLES permission. */
 export async function createRole(bot: Bot, guildId: bigint, options: CreateGuildRole, reason?: string) {
@@ -10,7 +9,7 @@ export async function createRole(bot: Bot, guildId: bigint, options: CreateGuild
     permissions: bot.utils.calculateBits(options?.permissions || []),
     color: options.color,
     hoist: options.hoist,
-    icon: options.icon ? iconBigintToHash(options.icon) : null,
+    icon: options.icon,
     unicode_emoji: options.unicodeEmoji,
     mentionable: options.mentionable,
     reason,
@@ -32,7 +31,7 @@ export interface CreateGuildRole {
   /** Whether the role should be displayed separately in the sidebar, default: false */
   hoist?: boolean;
   /** the role's icon image (if the guild has the `ROLE_ICONS` feature) */
-  icon?: bigint;
+  icon?: string;
   /** The role's unicode emoji (if the guild has the `ROLE_ICONS` feature) */
   unicodeEmoji?: string;
   /** Whether the role should be mentionable, default: false */
