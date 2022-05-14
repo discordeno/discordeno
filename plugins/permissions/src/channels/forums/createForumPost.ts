c53aad2ea8f379bcaa37a2c7fa2657deea750b8a
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-14 18:03:17 +0700

    style: deno fmt (#2219)

--- plugins/permissions/src/channels/forums/createForumPost.ts
+++ plugins/permissions/src/channels/forums/createForumPost.ts
@@ -4,11 +4,11 @@ import { requireBotChannelPermissions } from "../../permissions.ts";
 export default function createForumPosts(bot: BotWithCache) {
   const createForumPostsOld = bot.helpers.createForumPosts;
 
-  bot.helpers.createForumPosts= async function (channelId, options) {
+  bot.helpers.createForumPosts = async function (channelId, options) {
     const channel = bot.channels.get(channelId);
 
     if (channel) {
-        await requireBotChannelPermissions(bot, channel, ["SEND_MESSAGES"]);
+      await requireBotChannelPermissions(bot, channel, ["SEND_MESSAGES"]);
     }
 
     return await createForumPostsOld(channelId, options);
