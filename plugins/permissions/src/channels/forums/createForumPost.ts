d6f2bb9b1d78562ad6de12268c4b05a9b42152d9
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-14 14:12:18 +0700

    revert: fix: plugins/permissions: channels/forums/createForumPosts

--- plugins/permissions/src/channels/forums/createForumPost.ts
+++ plugins/permissions/src/channels/forums/createForumPost.ts
@@ -2,13 +2,13 @@ import { BotWithCache } from "../../../deps.ts";
 import { requireBotChannelPermissions } from "../../permissions.ts";
 
 export default function createForumPosts(bot: BotWithCache) {
-  const createForumPostsOld = bot.helpers.createForumPost;
+  const createForumPostsOld = bot.helpers.createForumPosts;
 
-  bot.helpers.createForumPost = async function (channelId, options) {
+  bot.helpers.createForumPosts = async function (channelId, options) {
     const channel = bot.channels.get(channelId);
 
     if (channel) {
-      requireBotChannelPermissions(bot, channel, ["SEND_MESSAGES"]);
+      await requireBotChannelPermissions(bot, channel, ["SEND_MESSAGES"]);
     }
 
     return await createForumPostsOld(channelId, options);
