f1ca2b97241e1c04211272c1fb7a03a98395016f
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-14 14:19:48 +0700

    fix: getArchivedThreads options and return type

--- types/discord.ts
+++ types/discord.ts
@@ -1368,6 +1368,11 @@ export interface DiscordListActiveThreads {
   members: DiscordThreadMember[];
 }
 
+export interface DiscordListArchivedThreads extends DiscordListActiveThreads {
+  /** whether there are potentially additional threads that could be returned on a subsequent call */
+  has_more: boolean;
+}
+
 export interface DiscordThreadListSync {
   /** The id of the guild */
   guild_id: string;
