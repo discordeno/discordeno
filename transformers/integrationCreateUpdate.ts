41ca107585ccac9178ecf3c69464fb46d39b5ecf
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-05-15 17:40:06 +0700

    deno fmt

--- transformers/integrationCreateUpdate.ts
+++ transformers/integrationCreateUpdate.ts
@@ -4,9 +4,9 @@ import { Optionalize } from "../types/shared.ts";
 
 export function transformIntegrationCreateUpdate(bot: Bot, payload: DiscordIntegrationCreateUpdate) {
   const integration = {
-		guildId: bot.transformers.snowflake(payload.guild_id),
-		...bot.transformers.integration(bot, payload),
-	};
+    guildId: bot.transformers.snowflake(payload.guild_id),
+    ...bot.transformers.integration(bot, payload),
+  };
 
   return integration as Optionalize<typeof integration>;
 }
