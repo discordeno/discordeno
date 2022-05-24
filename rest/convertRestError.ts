3a5263c4e92661b3982deb42a048e5a167c12987
ITOH <to@itoh.at>
2022-05-24 22:01:44 +0200

    style: deno fmt

--- rest/convertRestError.ts
+++ rest/convertRestError.ts
@@ -1,7 +1,6 @@
-
 import { RestRequestRejection } from "./rest.ts";
 
 export function convertRestError(errorStack: Error, data: RestRequestRejection): Error {
-    errorStack.message = `[${data.status}] ${data.error}`;
-    return errorStack;
+  errorStack.message = `[${data.status}] ${data.error}`;
+  return errorStack;
 }
