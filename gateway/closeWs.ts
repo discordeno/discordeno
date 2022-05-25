e6a902e68e61db13640808d4c5eb69b9d46316eb
ITOH <to@itoh.at>
2022-05-25 21:06:00 +0200

    f

--- gateway/closeWs.ts
+++
@@ -1,6 +0,0 @@
-/** Use this function to close a ws connection properly */
-export function closeWS(ws: WebSocket, code?: number, reason?: string) {
-  if (ws.readyState !== WebSocket.OPEN) return;
-
-  ws.close(code, reason);
-}
--- gateway/deps.ts
+++
@@ -1,1 +0,0 @@
-export { decompress_with as decompressWith } from "https://unpkg.com/@evan/wasm@0.0.94/target/zlib/deno.js";
