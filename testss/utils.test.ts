7db661180f0ee07bf8874c6fea007448ffd1bbf2
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-27 22:55:51 +0000

    fix(tests): migrate guild url unit tests

--- testss/utils.test.ts
+++ testss/utils.test.ts
@@ -49,6 +49,42 @@ Deno.test("[emoji] Create an emoji url", async () => {
 });
 
 Deno.test({
+  name: "[guild] format a guild's icon url",
+  fn: async () => {
+    const bot = await loadBot();
+    assertEquals(
+      bot.helpers.guildIconURL(785384884197392384n, 3837424427068676005442449262648382018748n),
+      "https://cdn.discordapp.com/icons/785384884197392384/46f50fb412eab14ec455d5cf777154bc.jpg?size=128",
+    );
+  },
+});
+
+Deno.test({
+  name: "[guild] format a guild's banner url",
+  fn: async () => {
+    const bot = await loadBot();
+
+    assertEquals(
+      bot.helpers.guildBannerURL(613425648685547541n, {
+        banner: 3919584870146358272366452115178209474142n,
+      }),
+      "https://cdn.discordapp.com/banners/613425648685547541/84c4964c115c128fb9100952c3b4f65e.jpg?size=128",
+    );
+  },
+});
+
+Deno.test({
+  name: "[guild] format a guild's splash url",
+  fn: async () => {
+    const bot = await loadBot();
+    assertEquals(
+      bot.helpers.guildSplashURL(785384884197392384n, 3837424427068676005442449262648382018748n),
+      "https://cdn.discordapp.com/splashes/785384884197392384/46f50fb412eab14ec455d5cf777154bc.jpg?size=128",
+    );
+  },
+});
+
+Deno.test({
   name: "[utils] format image url",
   fn() {
     assertEquals(formatImageURL(`https://skillz.is.pro`), "https://skillz.is.pro.jpg?size=128");
