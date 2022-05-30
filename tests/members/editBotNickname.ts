d51cf52a8c9be4e5f8a0f8d0f0dacf5909e0b812
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-30 22:19:19 +0000

    fix(tests): remove unused old tests files

--- tests/members/editBotNickname.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "../deps.ts";
-import { bot, guild } from "../mod.ts";
-
-Deno.test("[misc] edit a bot's nickname", async function () {
-  const nick = "lts20050703";
-  const nickname = await bot.helpers.editBotNickname(guild.id, { nick });
-  assertEquals(nickname, nick);
-});
