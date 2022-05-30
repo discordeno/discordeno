84e553707e6961af3124fe75b31b214882a169c4
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-30 22:20:28 +0000

    fix(tests): add nickname of bot test

---
+++ testss/guilds/members/editBotNickname.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "../../deps.ts";
+import { loadBot } from "../../mod.ts";
+import { CACHED_COMMUNITY_GUILD_ID } from "../../utils.ts";
+
+Deno.test("[misc] edit a bot's nickname", async function () {
+  const bot = loadBot();
+
+  const nick = "lts20050703";
+  const nickname = await bot.helpers.editBotNickname(CACHED_COMMUNITY_GUILD_ID, { nick });
+  assertEquals(nickname, nick);
+});
