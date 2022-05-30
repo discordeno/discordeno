4ffddb31606352f3bfb324f95c1d606ed033b9c6
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-30 22:20:16 +0000

    fix(tests): add dm test

---
+++ testss/guilds/members/dms.test.ts
@@ -0,0 +1,11 @@
+import { assertExists } from "../../deps.ts";
+import { loadBot } from "../../mod.ts";
+
+Deno.test("[member] get dm channel and send a message", async () => {
+  const bot = loadBot();
+  // Itoh Alt ID
+  const channel = await bot.helpers.getDmChannel(750661528360845322n);
+  assertExists(channel?.id);
+  
+  await bot.helpers.sendMessage(channel.id, { content: "https://i.imgur.com/doG55NR.png" });
+});
