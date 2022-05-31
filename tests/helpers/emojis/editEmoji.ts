0e6e5d86493cf988f0f88321893e2e4e7b572d1f
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-31 14:21:51 +0000

    fix(tests): remove old tests folder

--- tests/helpers/emojis/editEmoji.ts
+++
@@ -1,5 +0,0 @@
-import { Bot } from "../../../bot.ts";
-import { assertExists } from "../../deps.ts";
-import { delayUntil } from "../../utils.ts";
-
-export async function editEmojiTest(guildId: bigint) {}
