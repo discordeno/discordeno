d3c1111b267515b842727f72ecbc20aa372e519e
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-31 13:38:34 +0000

    fix: fmt and cleanup

--- testss/guilds/members/dms.test.ts
+++ testss/guilds/members/dms.test.ts
@@ -6,6 +6,6 @@ Deno.test("[member] get dm channel and send a message", async () => {
   // Itoh Alt ID
   const channel = await bot.helpers.getDmChannel(750661528360845322n);
   assertExists(channel?.id);
-  
+
   await bot.helpers.sendMessage(channel.id, { content: "https://i.imgur.com/doG55NR.png" });
 });
