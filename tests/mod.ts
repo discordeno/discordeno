35920b56cf36650627d322577c2fad61ef912644
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-31 13:15:52 +0000

    fix(tests): remaining channel unit tests

--- tests/mod.ts
+++ tests/mod.ts
@@ -1,28 +1,3 @@
-import { deleteChannelOverwriteTests } from "./helpers/channels/deleteChannelOverwrite.ts";
-import { editChannelTests } from "./helpers/channels/editChannel.ts";
-
-Deno.test({
-  name: "[channel] delete a channel overwrite",
-  async fn(t) {
-    await deleteChannelOverwriteTests(guild.id);
-  },
-  ...sanitizeMode,
-});
-Deno.test({
-  name: "[channel] edit a channel w/o a reason",
-  async fn(t) {
-    await editChannelTests(guild.id, {});
-  },
-  ...sanitizeMode,
-});
-Deno.test({
-  name: "[channel] edit a channel w/ a reason",
-  async fn(t) {
-    await editChannelTests(guild.id, { reason: "Blame wolf" });
-  },
-  ...sanitizeMode,
-});
-
 // import "./channels/connectToVoice.ts";
 // import "./misc/editBotStatus.ts";
 
