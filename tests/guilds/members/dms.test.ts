e9e8fa9c25cd3732d3e49286239862399a093f5d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-05-31 15:12:44 +0000

    fix(test): add check of dm message exist

--- tests/guilds/members/dms.test.ts
+++ tests/guilds/members/dms.test.ts
@@ -7,5 +7,6 @@ Deno.test("[member] get dm channel and send a message", async () => {
   const channel = await bot.helpers.getDmChannel(750661528360845322n);
   assertExists(channel?.id);
 
-  await bot.helpers.sendMessage(channel.id, { content: "https://i.imgur.com/doG55NR.png" });
+  const message = await bot.helpers.sendMessage(channel.id, { content: "https://i.imgur.com/doG55NR.png" });
+  assertExists(message?.content);
 });
