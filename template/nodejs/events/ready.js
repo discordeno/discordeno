88d02b1c618cd305ff6d0286b82679af76d3841a
Tomato6966 <chris.pre03@gmail.com>
2022-06-13 09:56:59 +0200

    fixes not showing ready
    maxShards currently not a thing in the npm i cant get it in the console.log(client.gateway)

--- template/nodejs/events/ready.js
+++ template/nodejs/events/ready.js
@@ -2,8 +2,8 @@ const User = require("../Structures/User");
 module.exports = async (client, payload) => {
   client.user = new User(client, payload.user);
 
-  if (payload.shardId + 1 === client.gateway.maxShards) {
+  if (payload.shardId + 1 === client.gateway.manager.totalShards) {
     //All Shards are ready
-    console.log("Successfully connected to the gateway as " + client.user.tag);
+    console.log(`Successfully connected to the gateway as ${client.user.tag}`);
   }
 };
