ccd8506c815e93c7064afa8230f61add7f62bf9b
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-06-15 11:25:53 -0400

    Auto mod blame wolf (#2267)
    * feat(meister): add automod
    
    * fix(test); rest error with code
    
    * fix(rest): log error nicely
    
    * fix(test): disable benchmark test until gateway rewrite
    
    * fix(automod): enum should start with 1
    
    * fix(helper): better undefined handling of metadata
    
    * fix(transfomrers): automod transformers
    
    * fix(tests): add some automod tests
    
    * Update types/shared.ts
    
    Co-authored-by: meister03 <69507874+meister03@users.noreply.github.com>
    
    * fix: changes discord made recently
    
    * fix(fmt): i hate deno fmt but i love itoh
    
    Co-authored-by: meister03 <69507874+meister03@users.noreply.github.com>
    Co-authored-by: LTS20050703 <87189679+lts20050703@users.noreply.github.com>

---
+++ helpers/guilds/automod/mod.ts
@@ -0,0 +1,5 @@
+export * from "./getAutomodRule.ts";
+export * from "./getAutomodRules.ts";
+export * from "./createAutomodRule.ts";
+export * from "./editAutomodRule.ts";
+export * from "./deleteAutomodRule.ts";
