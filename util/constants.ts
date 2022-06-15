4580b72d0ee3368e93008e6772ade6b434eeccd6
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-06-15 21:12:31 +0700

    nuke GATEWAY_VERSION (#2293)

--- util/constants.ts
+++ util/constants.ts
@@ -20,9 +20,6 @@ export const BASE_URL = "https://discord.com/api";
 /** https://discord.com/developers/docs/reference#api-versioning-api-versions */
 export const API_VERSION = 10;
 
-/** https://discord.com/developers/docs/topics/gateway#gateways-gateway-versions */
-export const GATEWAY_VERSION = 10;
-
 // TODO: update this version
 /** https://github.com/discordeno/discordeno/releases */
 export const DISCORDENO_VERSION = "13.0.0-rc45";
