13298a2ca62283be9797a8db50a6471f54dcec19
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-06-16 13:27:27 +0700

    avatarUrl and bannerUrl unit test

---
+++ tests/guilds/members/avatarUrl.test.ts
@@ -0,0 +1,23 @@
+import { loadBot } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+Deno.test("[member] avatar URL test group", async (t) => {
+  const bot = loadBot();
+  await t.step("[member] avatar URL user default avatar", async () => {
+    assertEquals(bot.helpers.avatarURL(379643682984296448n, "2195"), "https://cdn.discordapp.com/embed/avatars/0.png");
+  });
+  await t.step("[member] avatar URL user avatar", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", { avatar: 3804984885868334326724060172849678101349n }),
+      "https://cdn.discordapp.com/avatars/379643682984296448/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
+    );
+  });
+  await t.step("[member] avatar URL guild member avatar", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", {
+        avatar: 3804984885868334326724060172849678101349n,
+        guildId: 785384884197392384n,
+      }),
+      "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/avatars/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
+    );
+  });
+});
---
+++ tests/guilds/members/bannerUrl.test.ts
@@ -0,0 +1,20 @@
+import { loadBot } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+Deno.test("[member] banner URL test group", async (t) => {
+  const bot = loadBot();
+  await t.step("[member] banner URL user avatar", async () => {
+    assertEquals(
+      bot.helpers.bannerURL(379643682984296448n, { banner: 3804984885868334326724060172849678101349n }),
+      "https://cdn.discordapp.com/banners/379643682984296448/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
+    );
+  });
+  await t.step("[member] banner URL guild member avatar", async () => {
+    assertEquals(
+      bot.helpers.bannerURL(379643682984296448n, {
+        banner: 3804984885868334326724060172849678101349n,
+        guildId: 785384884197392384n,
+      }),
+      "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/banners/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
+    );
+  });
+});
