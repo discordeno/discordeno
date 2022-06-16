d3334b6ac0a506d730ac4bf550fe08f11d1aead5
LTS20050703 <87189679+lts20050703@users.noreply.github.com>
2022-06-16 13:38:42 +0700

    more avatarUrl and bannerUrl unit test

--- tests/guilds/members/avatarUrl.test.ts
+++ tests/guilds/members/avatarUrl.test.ts
@@ -20,4 +20,42 @@ Deno.test("[member] avatar URL test group", async (t) => {
       "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/avatars/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
     );
   });
+  await t.step("[member] avatar URL user avatar (format png)", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", {
+        avatar: 3804984885868334326724060172849678101349n,
+        format: "png",
+      }),
+      "https://cdn.discordapp.com/avatars/379643682984296448/2e8d6ed6b19ce257d70fec7c26bfab65.png?size=128",
+    );
+  });
+  await t.step("[member] avatar URL guild member avatar (format png)", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", {
+        avatar: 3804984885868334326724060172849678101349n,
+        guildId: 785384884197392384n,
+        format: "png",
+      }),
+      "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/avatars/2e8d6ed6b19ce257d70fec7c26bfab65.png?size=128",
+    );
+  });
+  await t.step("[member] avatar URL user avatar (size 16)", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", {
+        avatar: 3804984885868334326724060172849678101349n,
+        size: 16,
+      }),
+      "https://cdn.discordapp.com/avatars/379643682984296448/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=16",
+    );
+  });
+  await t.step("[member] avatar URL guild member avatar (size 16)", async () => {
+    assertEquals(
+      bot.helpers.avatarURL(379643682984296448n, "2195", {
+        avatar: 3804984885868334326724060172849678101349n,
+        guildId: 785384884197392384n,
+        size: 16,
+      }),
+      "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/avatars/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=16",
+    );
+  });
 });
--- tests/guilds/members/bannerUrl.test.ts
+++ tests/guilds/members/bannerUrl.test.ts
@@ -17,4 +17,20 @@ Deno.test("[member] banner URL test group", async (t) => {
       "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/banners/2e8d6ed6b19ce257d70fec7c26bfab65.jpg?size=128",
     );
   });
+  await t.step("[member] banner URL user avatar (format png)", async () => {
+    assertEquals(
+      bot.helpers.bannerURL(379643682984296448n, { banner: 3804984885868334326724060172849678101349n, format: "png" }),
+      "https://cdn.discordapp.com/banners/379643682984296448/2e8d6ed6b19ce257d70fec7c26bfab65.png?size=128",
+    );
+  });
+  await t.step("[member] banner URL guild member avatar (format png)", async () => {
+    assertEquals(
+      bot.helpers.bannerURL(379643682984296448n, {
+        banner: 3804984885868334326724060172849678101349n,
+        guildId: 785384884197392384n,
+        format: "png",
+      }),
+      "https://cdn.discordapp.com/guilds/785384884197392384/users/379643682984296448/banners/2e8d6ed6b19ce257d70fec7c26bfab65.png?size=128",
+    );
+  });
 });
