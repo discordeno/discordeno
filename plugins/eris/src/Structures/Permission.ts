9947a855501dff689e4f6c86bbd6ab56f8cfc3cf
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-06-26 17:39:39 +0000

    fix: Permissions

--- plugins/eris/src/Structures/Permission.ts
+++ plugins/eris/src/Structures/Permission.ts
@@ -1,13 +1,54 @@
+import { BitwisePermissionFlags } from "../../deps.ts";
+import { Base } from "../Base.ts";
 import { BigString } from "../Client.ts";
 
 export class Permission {
-    allow: bigint;
-    deny: bigint;
-    
-    constructor(allow: BigString | number, deny: BigString | number= 0) {
-        this.allow = BigInt(allow);
-        this.deny = BigInt(deny);
+  allow: bigint;
+  deny: bigint;
+  _json?: Record<string, boolean>;
+
+  constructor(allow: BigString | number, deny: BigString | number = 0) {
+    this.allow = BigInt(allow);
+    this.deny = BigInt(deny);
+  }
+
+  get json() {
+    if (!this._json) {
+      this._json = {};
+      for (const key of Object.keys(BitwisePermissionFlags)) {
+        if (typeof key === "number") continue;
+
+        const perm = key as keyof typeof BitwisePermissionFlags;
+
+        if (this.allow & BigInt(BitwisePermissionFlags[perm])) {
+          this._json[perm] = true;
+        } else if (this.deny & BigInt(BitwisePermissionFlags[perm])) {
+          this._json[perm] = false;
+        }
+      }
     }
+    return this._json;
+  }
+
+  /** Check if this permission allows a specific permission */
+  has(permission: bigint | keyof typeof BitwisePermissionFlags): boolean {
+    if (typeof permission === "bigint") {
+      return (this.allow & permission) === permission;
+    }
+    return !!(this.allow & BigInt(BitwisePermissionFlags[permission]));
+  }
+
+  toString() {
+    return `[${this.constructor.name} +${this.allow} -${this.deny}]`;
+  }
+
+  toJSON(props: string[] = []) {
+    return Base.prototype.toJSON.call([
+      "allow",
+      "deny",
+      ...props,
+    ]);
+  }
 }
 
 export default Permission;
