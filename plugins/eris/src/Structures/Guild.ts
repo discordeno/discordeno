fb959fb2bf5e2945990960cb33347e5f85050671
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-06-26 17:39:44 +0000

    fix: Role

--- plugins/eris/src/Structures/Guild.ts
+++ plugins/eris/src/Structures/Guild.ts
@@ -7,6 +7,8 @@ import PublicThreadChannel from "./PublicThreadChannel.ts";
 import Role from "./Role.ts";
 
 export class Guild extends Base {
+  client: Client;
+
   roles = new Collection<BigString, Role>();
   // TODO: check type for this
   channels = new Collection<BigString, GuildChannel>();
@@ -16,6 +18,7 @@ export class Guild extends Base {
 
   constructor(data: DiscordGuild, client: Client) {
     super(data.id);
+    this.client = client;
   }
 }
 
