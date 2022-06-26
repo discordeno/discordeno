4ad0b51e466409111593e5fbd7b9d1c7c01fa3b6
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-06-26 22:00:06 +0000

    fix: ExtendedUser

--- plugins/eris/src/Structures/ExtendedUser.ts
+++ plugins/eris/src/Structures/ExtendedUser.ts
@@ -1,10 +1,43 @@
-import { DiscordUser } from "../../deps.ts";
+import { DiscordUser, PremiumTypes } from "../../deps.ts";
 import Client from "../Client.ts";
+import User from "./User.ts";
 
-export class ExtendedUser {
-    constructor(data: DiscordUser, client: Client) {
+export class ExtendedUser extends User {
+  email?: string | null;
+  verified?: boolean;
+  mfaEnabled?: boolean;
+  premiumType?: PremiumTypes;
 
+  constructor(data: DiscordUser, client: Client) {
+    super(data, client);
+    this.update(data);
+  }
+
+  update(data: DiscordUser) {
+    super.update(data);
+    if (data.email !== undefined) {
+      this.email = data.email;
     }
+    if (data.verified !== undefined) {
+      this.verified = data.verified;
+    }
+    if (data.mfa_enabled !== undefined) {
+      this.mfaEnabled = data.mfa_enabled;
+    }
+    if (data.premium_type !== undefined) {
+      this.premiumType = data.premium_type;
+    }
+  }
+
+  toJSON(props: string[] = []) {
+    return super.toJSON([
+      "email",
+      "mfaEnabled",
+      "premium",
+      "verified",
+      ...props,
+    ]);
+  }
 }
 
 export default ExtendedUser;
