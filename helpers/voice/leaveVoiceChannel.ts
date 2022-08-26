e2374f1a49084697afb2b4991a9460a01f69f6d4
vxern <vxern@wordcollector.co.uk>
2022-08-26 22:22:24 +0100

    feat: Add return types to voice helpers.

--- helpers/voice/leaveVoiceChannel.ts
+++ helpers/voice/leaveVoiceChannel.ts
@@ -1,18 +1,18 @@
 import type { Bot } from "../../bot.ts";
-import { AtLeastOne, GatewayOpcodes } from "../../types/shared.ts";
+import { GatewayOpcodes } from "../../types/shared.ts";
 
 /** Connect or join a voice channel inside a guild. By default, the "selfDeaf" option is true. Requires `CONNECT` and `VIEW_CHANNEL` permissions. */
 export async function connectToVoiceChannel(
   bot: Bot,
   guildId: bigint,
-) {
+): Promise<void> {
   const shardId = bot.utils.calculateShardId(bot.gateway, guildId);
   const shard = bot.gateway.manager.shards.get(shardId);
   if (!shard) {
     throw new Error(`Shard (id: ${shardId} not found`);
   }
 
-  shard.send({
+  return shard.send({
     op: GatewayOpcodes.VoiceStateUpdate,
     d: {
       guild_id: guildId.toString(),
