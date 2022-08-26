19720c6defb424f0c513fb274b0a7c956c647f0d
vxern <vxern@wordcollector.co.uk>
2022-08-26 23:43:10 +0100

    fix: Change return collection key type from `string` to `bigint`. Remove redundant code.

--- helpers/integrations/getIntegrations.ts
+++ helpers/integrations/getIntegrations.ts
@@ -4,7 +4,7 @@ import { DiscordIntegration } from "../../types/discord.ts";
 import { Collection } from "../../util/collection.ts";
 
 /** Returns a list of integrations for the guild. Requires the MANAGE_GUILD permission. */
-export async function getIntegrations(bot: Bot, guildId: bigint): Promise<Collection<string, Integration>> {
+export async function getIntegrations(bot: Bot, guildId: bigint): Promise<Collection<bigint, Integration>> {
   const result = await bot.rest.runMethod<DiscordIntegration[]>(
     bot.rest,
     "GET",
@@ -12,26 +12,9 @@ export async function getIntegrations(bot: Bot, guildId: bigint): Promise<Collec
   );
 
   return new Collection(
-    result.map<[string, Integration]>((integration) => [
-      integration.id,
-      bot.transformers.integration(bot, {
-        guild_id: guildId.toString(),
-        id: integration.id,
-        name: integration.name,
-        type: integration.type,
-        enabled: integration.enabled,
-        syncing: integration.syncing,
-        role_id: integration.role_id,
-        enable_emoticons: integration.enable_emoticons,
-        expire_behavior: integration.expire_behavior,
-        expire_grace_period: integration.expire_grace_period,
-        user: integration.user,
-        account: integration.account,
-        synced_at: integration.synced_at,
-        subscriber_count: integration.subscriber_count,
-        revoked: integration.revoked,
-        application: integration.application,
-      }),
-    ]),
+    result.map((result) => {
+      const integration = bot.transformers.integration(bot, { guild_id: guildId.toString(), ...result });
+      return [integration.id, integration];
+    }),
   );
 }
