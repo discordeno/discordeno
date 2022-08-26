108576cf4d95701e728e5a6930822c79c486b389
vxern <vxern@wordcollector.co.uk>
2022-08-26 22:19:27 +0100

    misc!: Consistency: Add 'guild' keyword to name.

--- helpers/templates/getGuildTemplate.ts
+++ helpers/templates/getGuildTemplate.ts
@@ -3,7 +3,7 @@ import { Template } from "../../transformers/template.ts";
 import { DiscordTemplate } from "../../types/discord.ts";
 
 /** Returns the guild template if it exists */
-export async function getTemplate(bot: Bot, templateCode: string): Promise<Template> {
+export async function getGuildTemplate(bot: Bot, templateCode: string): Promise<Template> {
   const result = await bot.rest.runMethod<DiscordTemplate>(
     bot.rest,
     "GET",
