cb6c4b7bdaf050ac777acbf5baf75c9babb9175a
vxern <vxern@wordcollector.co.uk>
2022-08-26 17:26:26 +0100

    misc!: Remove unused `bot` parameter. Capitalise 'URL'.

--- helpers/emojis/emojiUrl.ts
+++ helpers/emojis/emojiUrl.ts
@@ -1,6 +1,4 @@
-import { Bot } from "../../bot.ts";
-
 /** Creates a url to the emoji from the Discord CDN. */
-export function emojiUrl(bot: Bot, id: bigint, animated = false): string {
+export function emojiURL(id: bigint, animated = false): string {
   return `https://cdn.discordapp.com/emojis/${id}.${animated ? "gif" : "png"}`;
 }
