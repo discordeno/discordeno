38e541b428dfa23a4760a4926c927d946f25fc86
vxern <vxern@wordcollector.co.uk>
2022-08-26 21:39:50 +0100

    fix: Use `slice()` to prevent unwanted side effects.

--- helpers/messages/deleteMessages.ts
+++ helpers/messages/deleteMessages.ts
@@ -7,11 +7,11 @@ export async function deleteMessages(bot: Bot, channelId: bigint, ids: bigint[],
   }
 
   if (ids.length > 100) {
-    console.warn(`This endpoint only accepts a maximum of 100 messages. Deleting the first 100 message ids provided.`);
+    console.warn(`This endpoint only accepts a maximum of 100 messages. Using the first 100 message ids provided.`);
   }
 
   return void await bot.rest.runMethod(bot.rest, "POST", bot.constants.routes.CHANNEL_BULK_DELETE(channelId), {
-    messages: ids.splice(0, 100).map((id) => id.toString()),
+    messages: ids.slice(0, 100).map((id) => id.toString()),
     reason,
   });
 }
