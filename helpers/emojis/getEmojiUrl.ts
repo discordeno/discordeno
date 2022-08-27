5beb6f81a544d963db0186baf6ca8396044fe377
vxern <vxern@wordcollector.co.uk>
2022-08-27 01:07:48 +0100

    misc: Reintroduce `bot` as first parameter.

--- helpers/emojis/getEmojiUrl.ts
+++ helpers/emojis/getEmojiUrl.ts
@@ -1,4 +1,6 @@
+import { Bot } from "../../bot.ts";
+
 /** Creates a url to the emoji from the Discord CDN. */
-export function getEmojiURL(id: bigint, animated = false): string {
+export function getEmojiURL(_bot: Bot, id: bigint, animated = false): string {
   return `https://cdn.discordapp.com/emojis/${id}.${animated ? "gif" : "png"}`;
 }
