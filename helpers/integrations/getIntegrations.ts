ab07eb0f22d602ffa25db1fd4cd69359670fb563
vxern <vxern@wordcollector.co.uk>
2022-08-28 10:43:52 +0100

    misc: Write out properties manually instead of using the spread operator.

--- helpers/integrations/getIntegrations.ts
+++ helpers/integrations/getIntegrations.ts
@@ -13,7 +13,24 @@ export async function getIntegrations(bot: Bot, guildId: bigint): Promise<Collec
 
   return new Collection(
     results.map((result) => {
-      const integration = bot.transformers.integration(bot, { guild_id: guildId.toString(), ...result });
+      const integration = bot.transformers.integration(bot, {
+        guild_id: guildId.toString(),
+        id: result.id,
+        name: result.name,
+        type: result.type,
+        enabled: result.enabled,
+        syncing: result.syncing,
+        role_id: result.role_id,
+        enable_emoticons: result.enable_emoticons,
+        expire_behavior: result.expire_behavior,
+        expire_grace_period: result.expire_grace_period,
+        user: result.user,
+        account: result.account,
+        synced_at: result.synced_at,
+        subscriber_count: result.subscriber_count,
+        revoked: result.revoked,
+        application: result.application,
+      });
       return [integration.id, integration];
     }),
   );
