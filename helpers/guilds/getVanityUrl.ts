151fb10bd89aee1156d55c120421680d2f2d3559
Dorian Oszczęda <vxern@wordcollector.co.uk>
2022-08-28 10:29:45 +0100

    misc: Add missing `await` keyword.
    Co-authored-by: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- helpers/guilds/getVanityUrl.ts
+++ helpers/guilds/getVanityUrl.ts
@@ -7,7 +7,7 @@ export type VanityUrl = {
 
 /** Returns the code and uses of the vanity url for this server if it is enabled else `code` will be null. Requires the `MANAGE_GUILD` permission. */
 export async function getVanityUrl(bot: Bot, guildId: bigint): Promise<VanityUrl> {
-  return bot.rest.runMethod<VanityUrl>(
+  return await bot.rest.runMethod<VanityUrl>(
     bot.rest,
     "GET",
     bot.constants.routes.GUILD_VANITY_URL(guildId),
