d7eb8b5b075f93dfa000a0c34fbf24bf44027ce8
TriForMine <quentin.nicolini@hotmail.fr>
2022-09-01 13:32:22 +0200

    update: Update kwik to 1.3.0

--- template/beginner/deps.ts
+++ template/beginner/deps.ts
@@ -6,4 +6,4 @@ export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
 // Get data from .env files
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 // Database, thx Tri!
-export * from "https://deno.land/x/kwik@v1.2.1/mod.ts";
+export * from "https://deno.land/x/kwik@v1.3.0/mod.ts";
