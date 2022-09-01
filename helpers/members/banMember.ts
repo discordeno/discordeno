fd6fbf520c17fa11fc2e17a371b2ecf650b4d9a0
lts20050703 <lts20050703@gmail.com>
2022-09-01 15:37:13 +0700

    add ban delete_message_seconds and remove reason

--- helpers/members/banMember.ts
+++ helpers/members/banMember.ts
@@ -6,19 +6,17 @@ export async function banMember(bot: Bot, guildId: bigint, id: bigint, options?:
     bot.rest,
     "PUT",
     bot.constants.routes.GUILD_BAN(guildId, id),
-    options
-      ? {
-        delete_message_days: options.deleteMessageDays,
-        reason: options.reason,
-      }
-      : {},
+    {
+      delete_message_days: options?.deleteMessageDays,
+      delete_message_seconds: options?.deleteMessageSeconds,
+    },
   );
 }
 
 /** https://discord.com/developers/docs/resources/guild#create-guild-ban */
 export interface CreateGuildBan {
-  /** Number of days to delete messages for (0-7) */
+  /** Number of days to delete messages for (0-7) (deprecated) */
   deleteMessageDays?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-  /** Reason for the ban */
-  reason?: string;
+  /** number of seconds to delete messages for, between 0 and 604800 (7 days) */
+  deleteMessageSeconds?: number;
 }
