fb02ea6d378b22f6a2ca4b8414f639b22eadf452
lts20050703 <lts20050703@gmail.com>
2022-09-01 14:40:33 +0700

    add resume_gateway_url to DiscordReady

--- types/discord.ts
+++ types/discord.ts
@@ -17,7 +17,6 @@ import {
   GuildNsfwLevel,
   IntegrationExpireBehaviors,
   InteractionTypes,
-  Locales,
   Localization,
   MessageActivityTypes,
   MessageComponentTypes,
@@ -2157,6 +2156,8 @@ export interface DiscordReady {
   guilds: DiscordUnavailableGuild[];
   /** Used for resuming connections */
   session_id: string;
+  /** Gateway url for resuming connections */
+  resume_gateway_url: string;
   /** The shard information associated with this session, if sent when identifying */
   shard?: [number, number];
   /** Contains id and flags */
