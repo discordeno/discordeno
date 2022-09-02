03cfb900489df8d2f5e1301fe49a6dce10be15ab
vxern <vxern@wordcollector.co.uk>
2022-09-02 14:18:43 +0100

    refactor: Rename `followChannel` -> `followAnnouncementChannel`.

--- helpers/channels/announcements/followAnnouncementChannel.ts
+++ helpers/channels/announcements/followAnnouncementChannel.ts
@@ -2,7 +2,11 @@ import type { Bot } from "../../../bot.ts";
 import { DiscordFollowedChannel } from "../../../types/discord.ts";
 
 /** Follow a News Channel to send messages to a target channel. Requires the `MANAGE_WEBHOOKS` permission in the target channel. Returns the webhook id. */
-export async function followChannel(bot: Bot, sourceChannelId: bigint, targetChannelId: bigint): Promise<bigint> {
+export async function followAnnouncementChannel(
+  bot: Bot,
+  sourceChannelId: bigint,
+  targetChannelId: bigint,
+): Promise<bigint> {
   const result = await bot.rest.runMethod<DiscordFollowedChannel>(
     bot.rest,
     "POST",
