b6f1040ab841bbbd1069d2efd0f2cc08576fb16f
lts20050703 <lts20050703@gmail.com>
2022-09-02 23:36:01 +0700

    helpers/guilds: editGuuldMfaLevel return void

--- helpers/guilds/editGuildMfaLevel.ts
+++ helpers/guilds/editGuildMfaLevel.ts
@@ -3,10 +3,11 @@ import type { MfaLevels } from "../../types/shared.ts";
 
 /** Modify a guild's MFA level. Requires guild ownership. */
 export async function editGuildMfaLevel(bot: Bot, guildId: bigint, mfaLevel: MfaLevels, reason?: string) {
-  return await bot.rest.runMethod<MfaLevels>(
+  await bot.rest.runMethod<MfaLevels>(
     bot.rest,
     "POST",
     bot.constants.routes.GUILD_MFA_LEVEL(guildId),
     { level: mfaLevel, reason },
   );
+  return;
 }
