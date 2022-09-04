11cbc50602e40aa04e675984f9632a2b066af938
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-09-04 12:01:47 -0500

    Update plugins/permissions/src/guilds/editGuildMfaLevel.ts

--- plugins/permissions/src/guilds/editGuildMfaLevel.ts
+++ plugins/permissions/src/guilds/editGuildMfaLevel.ts
@@ -5,7 +5,7 @@ export default function editGuildMfaLevel(bot: BotWithCache) {
 
   bot.helpers.editGuildMfaLevel = async function (guildId, mfaLevel, reason) {
     const guild = bot.guilds.get(guildId);
-    if (guild && guild.ownerId !== bot.id) throw new Error("The bot is not the owner of the guild");
+    if (guild?.ownerId !== bot.id) throw new Error("The bot is not the owner of the guild");
     return await editGuildMfaLevelOld(guildId, mfaLevel, reason);
   };
 }
