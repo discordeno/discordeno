80632f6fbb9d2cc195d7019f81637c82167c676c
lts20050703 <lts20050703@gmail.com>
2022-09-04 11:29:32 +0700

    helpers/banMember: nuke deleteMessageDays

--- helpers/members/banMember.ts
+++ helpers/members/banMember.ts
@@ -7,7 +7,6 @@ export async function banMember(bot: Bot, guildId: bigint, id: bigint, options?:
     "PUT",
     bot.constants.routes.GUILD_BAN(guildId, id),
     {
-      delete_message_days: options?.deleteMessageDays,
       delete_message_seconds: options?.deleteMessageSeconds,
       reason: options?.reason,
     },
@@ -16,8 +15,6 @@ export async function banMember(bot: Bot, guildId: bigint, id: bigint, options?:
 
 /** https://discord.com/developers/docs/resources/guild#create-guild-ban */
 export interface CreateGuildBan {
-  /** Number of days to delete messages for (0-7) (deprecated) */
-  deleteMessageDays?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
   /** Number of seconds to delete messages for, between 0 and 604800 (7 days) */
   deleteMessageSeconds?: number;
   /** Reason for the ban */
