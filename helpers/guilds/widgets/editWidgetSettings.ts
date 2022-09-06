ae07e1755a4c1c2a3eadbb4afbb2a4ff173a3e0c
vxern <vxern@wordcollector.co.uk>
2022-09-06 11:42:45 +0100

    docs: Document guild widget helpers.

--- helpers/guilds/widgets/editWidgetSettings.ts
+++ helpers/guilds/widgets/editWidgetSettings.ts
@@ -2,7 +2,22 @@ import type { Bot } from "../../../bot.ts";
 import { GuildWidgetSettings } from "../../../transformers/widgetSettings.ts";
 import { DiscordGuildWidgetSettings } from "../../../types/discord.ts";
 
-/** Modify a guild widget object for the guild. Requires the MANAGE_GUILD permission. */
+// TODO: Use `options` instead of `enabled` and `channelId`.
+
+/**
+ * Edits the settings of a guild's widget.
+ *
+ * @param bot - The bot instance to use to make the request.
+ * @param guildId - The ID of the guild to edit the settings of the widget of.
+ * @returns An instance of the edited {@link GuildWidgetSettings}.
+ *
+ * @remarks
+ * Requires the `MANAGE_GUILD` permission.
+ *
+ * Fires a _Guild Update_ event.
+ *
+ * @see {@link https://discord.com/developers/docs/resources/guild#modify-guild-widget}
+ */
 export async function editWidgetSettings(
   bot: Bot,
   guildId: bigint,
--- helpers/guilds/widgets/getWidget.ts
+++ helpers/guilds/widgets/getWidget.ts
@@ -2,7 +2,15 @@ import type { Bot } from "../../../bot.ts";
 import { GuildWidget } from "../../../transformers/widget.ts";
 import { DiscordGuildWidget } from "../../../types/discord.ts";
 
-/** Returns the widget for the guild. */
+/**
+ * Gets the guild widget by guild ID.
+ *
+ * @param bot - The bot instance to use to make the request.
+ * @param guildId - The ID of the guild to get the widget of.
+ * @returns An instance of {@link GuildWidget}.
+ *
+ * @see {@link https://discord.com/developers/docs/resources/guild#get-guild-widget}
+ */
 export async function getWidget(bot: Bot, guildId: bigint): Promise<GuildWidget> {
   const result = await bot.rest.runMethod<DiscordGuildWidget>(
     bot.rest,
