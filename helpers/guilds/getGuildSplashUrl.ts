8b679ad3bf5ddf2ab525ab9620cefdb190248686
vxern <vxern@wordcollector.co.uk>
2022-09-07 00:43:16 +0100

    docs: Fix inconsistency.

--- helpers/guilds/getGuildSplashUrl.ts
+++ helpers/guilds/getGuildSplashUrl.ts
@@ -4,9 +4,10 @@ import { ImageFormat, ImageSize } from "../members/getAvatarUrl.ts";
 /**
  * Builds the URL to a guild splash stored in the Discord CDN.
  *
- * @param bot - The bot instance to use to make the request.
+ * @param bot - The bot instance to use to build the URL.
  * @param guildId - The ID of the guild to get the splash of.
  * @param imageHash - The hash identifying the splash image.
+ * @param options - The parameters for the building of the URL.
  * @returns The link to the resource or `undefined` if the guild does not have a splash image set.
  */
 export function getGuildSplashURL(
