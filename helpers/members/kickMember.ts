15ffc093d64df8c139077dc1f1bbb7011a2f5813
vxern <vxern@wordcollector.co.uk>
2022-09-07 11:26:37 +0100

    docs: Document member helpers.

--- helpers/members/kickMember.ts
+++ helpers/members/kickMember.ts
@@ -1,11 +1,24 @@
 import { Bot } from "../../bot.ts";
 
-/** Kick a member from the server */
-export async function kickMember(bot: Bot, guildId: bigint, memberId: bigint, reason?: string): Promise<void> {
+/**
+ * Kicks a member from a guild.
+ *
+ * @param bot - The bot instance to use to make the request.
+ * @param guildId - The ID of the guild to kick the member from.
+ * @param userId - The user ID of the member to kick from the guild.
+ *
+ * @remarks
+ * Requires the `KICK_MEMBERS` permission.
+ *
+ * Fires a _Guild Member Remove_ gateway event.
+ *
+ * @see {@link https://discord.com/developers/docs/resources/guild#remove-guild-member}
+ */
+export async function kickMember(bot: Bot, guildId: bigint, userId: bigint, reason?: string): Promise<void> {
   return await bot.rest.runMethod<void>(
     bot.rest,
     "DELETE",
-    bot.constants.routes.GUILD_MEMBER(guildId, memberId),
+    bot.constants.routes.GUILD_MEMBER(guildId, userId),
     {
       reason,
     },
