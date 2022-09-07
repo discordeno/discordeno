b995943a3762414eb62ae5f8a0f4e5be5a8b1cca
Sakib Ahamed <you@example.com>
2022-09-07 14:44:29 +0100

    minor fixes

--- template/nodejs/events/ready.js
+++ template/nodejs/events/ready.js
@@ -1,7 +1,6 @@
-const User = require("../Structures/User");
 module.exports = async (client, payload) => {
   client.user = client.users.forge(payload.user);
-  if (payload.shardId + 1 === client.gateway.maxShards) {
+  if (payload.shardId === client.gateway.lastShardId) {
     //All Shards are ready
     console.log("Successfully connected to the gateway as " + client.user.tag);
   }
