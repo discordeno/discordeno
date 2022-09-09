2b988983bd72275b1af0034496549cea76670d25
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-09-09 00:46:58 +0000

    fix: edit bot member returns full member

--- helpers/members/editBotMember.ts
+++ helpers/members/editBotMember.ts
@@ -1,4 +1,5 @@
 import type { Bot } from "../../bot.ts";
+import { DiscordMember, Member } from "../../mod.ts";
 
 /**
  * Edits the nickname of the bot user.
@@ -6,6 +7,7 @@ import type { Bot } from "../../bot.ts";
  * @param bot - The bot instance to use to make the request.
  * @param guildId - The ID of the guild to edit the nickname of the bot user in.
  * @param options - The parameters for the edit of the nickname.
+ * @returns An instance of the edited {@link Member}
  *
  * @remarks
  * Fires a _Guild Member Update_ gateway event.
@@ -16,11 +18,13 @@ export async function editBotMember(
   bot: Bot,
   guildId: bigint,
   options: { nick: string | null; reason?: string },
-): Promise<void> {
-  return await bot.rest.runMethod<void>(
+): Promise<Member> {
+  const result = await bot.rest.runMethod<DiscordMember>(
     bot.rest,
     "PATCH",
     bot.constants.routes.USER_NICK(guildId),
     options,
   );
+
+  return bot.transformers.member(bot, result, guildId, bot.id);
 }
