b0de3f307d428a3dbcd80c7180f80d747305c9da
lts20050703 <lts20050703@gmail.com>
2022-09-10 21:38:27 +0700

    plugins/validations: slash commands max 4000 chars

--- plugins/validations/src/interaction/commands/createGlobalApplicationCommand.ts
+++ plugins/validations/src/interaction/commands/createGlobalApplicationCommand.ts
@@ -4,6 +4,7 @@ import {
   CONTEXT_MENU_COMMANDS_NAME_REGEX,
   SLASH_COMMANDS_NAME_REGEX,
 } from "../../../deps.ts";
+import { validateApplicationCommandLength } from "../../applicationCommandLength.ts";
 import { validateApplicationCommandOptions } from "../../applicationCommandOptions.ts";
 
 export function createGlobalApplicationCommand(bot: Bot) {
@@ -33,9 +34,22 @@ export function createGlobalApplicationCommand(bot: Bot) {
 
         options.options = validateApplicationCommandOptions(bot, options.options);
       }
+
+      validateApplicationCommandLength(bot, options);
     } else {
       if (!CONTEXT_MENU_COMMANDS_NAME_REGEX.test(options.name)) {
         throw new Error("The name of the context menu did not match the required regex.");
+      }
+
+      let length = 0;
+      if (options.nameLocalizations) {
+        length += Math.max(options.name.length, ...Object.values(options.nameLocalizations).map((name) => name.length));
+      } else length += options.name.length;
+
+      if (length > 4000) {
+        throw new Error(
+          "Slash commands can have a maximum of 4000 characters for combined name, description, and value properties for each command, its options (including subcommands and groups), and choices. When localization fields are present, only the longest localization for each field (including the default value) is counted towards the size limit.",
+        );
       }
     }
 
