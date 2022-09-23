872693c98020cf632e6098e4ebb50e97e1ca10a3
lts20050703 <lts20050703@gmail.com>
2022-09-23 10:50:10 +0700

    tests/misc: rateLimit.test promise.all

--- tests/misc/rateLimit.test.ts
+++ tests/misc/rateLimit.test.ts
@@ -4,6 +4,6 @@ import { CACHED_COMMUNITY_GUILD_ID } from "../utils.ts";
 Deno.test("[Misc] Rate Limit Test", async () => {
   const bot = loadBot();
   const channel = await bot.helpers.createChannel(CACHED_COMMUNITY_GUILD_ID);
-  for (let i = 0; i < 15; i += 1) await bot.helpers.sendMessage(channel.id, { content: "Rate Limit Test" });
+  await Promise.all(Array(10).map(() => bot.helpers.sendMessage(channel.id, { content: "Rate Limit Test" })));
   await bot.helpers.deleteChannel(channel.id);
 });
