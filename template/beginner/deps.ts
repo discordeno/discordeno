cd511b2a96986e746a81c7706f6e65641e402e29
Awesome Stickz <awesome@stickz.dev>
2022-10-08 03:07:31 +0530

    fix: kwik's version in beginner template

--- template/beginner/deps.ts
+++ template/beginner/deps.ts
@@ -6,4 +6,4 @@ export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
 // Get data from .env files
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 // Database, thx Tri!
-export { decode as KwikDecode, encode as KwikEncode, Kwik } from "https://deno.land/x/kwik@v1.2.1/mod.ts";
+export { decode as KwikDecode, encode as KwikEncode, Kwik } from "https://deno.land/x/kwik@v1.3.1/mod.ts";
