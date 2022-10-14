ce3060b079ffcc05c27c7c558d2fb0de6f7f626c
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-10-14 15:12:59 +0000

    fix: shard total log

--- template/beginner/src/events/ready.ts
+++ template/beginner/src/events/ready.ts
@@ -2,7 +2,7 @@ import { Bot } from "../../bot.ts";
 import log from "../utils/logger.ts";
 
 Bot.events.ready = (_, payload) => {
-  log.info(`[READY] Shard ID ${payload.shardId} of ${Bot.gateway.lastShardId} shards is ready!`);
+  log.info(`[READY] Shard ID ${payload.shardId} of ${Bot.gateway.lastShardId + 1} shards is ready!`);
 
   if (payload.shardId === Bot.gateway.lastShardId) {
     botFullyReady();
