6a408818fac9256e65b688f3b5b4797bc777c49b
H01001000 <heiheiho000@gmail.com>
2022-10-21 16:18:54 +0800

    chore: remove dev console.log

--- template/bigbot/src/bot/events/messages/create.ts
+++ template/bigbot/src/bot/events/messages/create.ts
@@ -4,7 +4,7 @@ import { processMessageCollectors } from "../../utils/collectors.js";
 export function setMessageCreateEvent() {
   bot.events.messageCreate = async function (_, message) {
     processMessageCollectors(message);
-    console.log(message);
+
     await Promise.allSettled([
       // SETUP-DD-TEMP: Add any functions you want to run on every message here. For example, automoderation filters.
     ]).catch(console.log);
