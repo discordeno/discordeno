0c9087bdbf88054c5840c01ee10b7b372ab01c93
H01001000 <heiheiho000@gmail.com>
2022-10-24 02:09:01 +0800

    fix: tranform

--- performance/tranformOutput.ts
+++ performance/tranformOutput.ts
@@ -9,11 +9,8 @@ for (const line of lines) {
 
   const name = m[1].trim();
   const value = m[2];
-  const range = Math.round(
-    (parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[2]) -
-      parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[0])) * 10,
-  ) / 10;
-  const unit = m[3];
+  const range = Math.round(parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[2]) - parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[0]));
+  const unit = Math.round(parseFloat(m[3]));
 
   ret.push(`test ${name} ... bench: ${value} ${unit} (+/- ${range})`);
 }
