3e29ae0d5a34b85c1ee0d0f49db059879cf210ff
H01001000 <heiheiho000@gmail.com>
2022-10-25 14:04:51 +0800

    refactor: use custom input

--- performance/tranformOutput.ts
+++ performance/tranformOutput.ts
@@ -14,14 +14,16 @@ for (const line of lines) {
   const m = line.match(/^(.+)\s+([0-9.]+) (.s)\/iter\s+\((.+) .s … (.+) .s\)(.+)$/);
   if (m === null) continue;
 
-  const name = m[1].trim();
-  const value = Math.round(parseFloat(m[2]) * unitMultiplier[m[3] as keyof typeof unitMultiplier]);
-  const range = Math.round(
-    (parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[2]) -
-      parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[0])) * unitMultiplier[m[3] as keyof typeof unitMultiplier],
-  );
 
-  ret.push(`test ${name} ... bench: ${value} ns/iter (+/- ${range})`);
+  ret.push({
+    name: m[1].trim(),
+    value: Math.round(parseFloat(m[2]) * unitMultiplier[m[3] as keyof typeof unitMultiplier]),
+    range: Math.round(
+      (parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[2]) -
+        parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[0])) * unitMultiplier[m[3] as keyof typeof unitMultiplier],
+    ),
+    unit: "ns/iter"
+  });
 }
 
-await Deno.writeTextFile("output.txt", ret.join("\n"));
+await Deno.writeTextFile("output.txt", JSON.stringify(ret, undefined, 2));
