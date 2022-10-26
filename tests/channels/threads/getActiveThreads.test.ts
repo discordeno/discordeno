7aded3545b7bd72590d1ab1a0b7ba57e03d53fad
H01001000 <heiheiho000@gmail.com>
2022-10-26 13:12:28 +0800

    style: format Deno.test\("(.+)", async \(t\) => \{\n([\S\s]*?)^\}\);

--- tests/channels/threads/getActiveThreads.test.ts
+++ tests/channels/threads/getActiveThreads.test.ts
@@ -1,23 +1,26 @@
-import { assertEquals, assertExists, assertNotEquals } from "../../deps.ts";
+import { assertEquals, assertExists } from "../../deps.ts";
 import { loadBot } from "../../mod.ts";
 import { CACHED_COMMUNITY_GUILD_ID } from "../../utils.ts";
 
-Deno.test("[thread] Get active threads", async (t) => {
-  const bot = loadBot();
-  const channel = await bot.helpers.createChannel(CACHED_COMMUNITY_GUILD_ID, { name: "threads" });
-  const message = await bot.helpers.sendMessage(channel.id, { content: "thread message" });
-  const thread = await bot.helpers.startThreadWithMessage(channel.id, message.id, {
-    reason: "idk",
-    rateLimitPerUser: 5,
-    name: "tread carefully",
-    autoArchiveDuration: 60,
-  });
+Deno.test({
+  name: "[thread] Get active threads",
+  async fn(t) {
+    const bot = loadBot();
+    const channel = await bot.helpers.createChannel(CACHED_COMMUNITY_GUILD_ID, { name: "threads" });
+    const message = await bot.helpers.sendMessage(channel.id, { content: "thread message" });
+    const thread = await bot.helpers.startThreadWithMessage(channel.id, message.id, {
+      reason: "idk",
+      rateLimitPerUser: 5,
+      name: "tread carefully",
+      autoArchiveDuration: 60,
+    });
 
-  const threadMessage = await bot.helpers.sendMessage(thread.id, { content: "message in a bottle" });
-  assertExists(threadMessage.id);
+    const threadMessage = await bot.helpers.sendMessage(thread.id, { content: "message in a bottle" });
+    assertExists(threadMessage.id);
 
-  const activeThreads = await bot.helpers.getActiveThreads(CACHED_COMMUNITY_GUILD_ID);
-  assertEquals(Boolean(activeThreads.threads.size), true);
+    const activeThreads = await bot.helpers.getActiveThreads(CACHED_COMMUNITY_GUILD_ID);
+    assertEquals(Boolean(activeThreads.threads.size), true);
 
-  await bot.helpers.deleteChannel(channel.id);
+    await bot.helpers.deleteChannel(channel.id);
+  },
 });
