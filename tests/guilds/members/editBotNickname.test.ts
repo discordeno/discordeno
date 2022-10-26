c8078e55d7d57c84c1e8c41cf58c108b7d461f6f
H01001000 <heiheiho000@gmail.com>
2022-10-26 13:13:24 +0800

    style: format async function

--- tests/guilds/members/editBotNickname.test.ts
+++ tests/guilds/members/editBotNickname.test.ts
@@ -2,14 +2,17 @@ import { assertEquals } from "../../deps.ts";
 import { loadBot } from "../../mod.ts";
 import { CACHED_COMMUNITY_GUILD_ID } from "../../utils.ts";
 
-Deno.test("[misc] edit a bot's nickname", async function () {
-  const bot = loadBot();
+Deno.test({
+  name: "[misc] edit a bot's nickname",
+  async fn() {
+    const bot = loadBot();
 
-  const nick = "lts20050703";
-  const member = await bot.helpers.editBotMember(CACHED_COMMUNITY_GUILD_ID, { nick });
-  assertEquals(member.nick, nick);
+    const nick = "lts20050703";
+    const member = await bot.helpers.editBotMember(CACHED_COMMUNITY_GUILD_ID, { nick });
+    assertEquals(member.nick, nick);
 
-  // Change nickname back
-  const member2 = await bot.helpers.editBotMember(CACHED_COMMUNITY_GUILD_ID, { nick: null });
-  assertEquals(member2.nick, undefined);
+    // Change nickname back
+    const member2 = await bot.helpers.editBotMember(CACHED_COMMUNITY_GUILD_ID, { nick: null });
+    assertEquals(member2.nick, undefined);
+  },
 });
