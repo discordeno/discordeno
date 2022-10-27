474bab605bec02f57a3fd2979230110c84b737c8
H01001000 <heiheiho000@gmail.com>
2022-10-28 00:16:51 +0800

    fix: range

--- performance/tranformOutput.ts
+++ performance/tranformOutput.ts
@@ -11,17 +11,13 @@ const unitMultiplier = {
 };
 
 for (const line of lines) {
-  const m = line.match(/^(.+)\s+([0-9.]+) (.s)\/iter\s+\((.+) .s … (.+) .s\)(.+)$/);
+  const m = line.match(/^(.+)\s+([0-9.]+) (.s)\/iter\s+\((.+) (.s) … (.+) (.s)\)(.+)$/);
   if (m === null) continue;
 
   ret.push({
     name: m[1].trim(),
     value: Math.round(parseFloat(m[2]) * unitMultiplier[m[3] as keyof typeof unitMultiplier]),
-    range: Math.round(
-      (parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[2]) -
-        parseFloat(m[6].replace(/\s+/g, " ").trim().split(" ")[0])) *
-        unitMultiplier[m[3] as keyof typeof unitMultiplier],
-    ),
+    range: `${parseFloat(m[4]) * unitMultiplier[m[5] as keyof typeof unitMultiplier]} … ${parseFloat(m[6]) * unitMultiplier[m[7] as keyof typeof unitMultiplier]}`,
     unit: "ns/iter",
   });
 }
