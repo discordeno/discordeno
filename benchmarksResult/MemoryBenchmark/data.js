20528a88b0e27bd97224e8847b358685b5adc012
github-action-benchmark <github@users.noreply.github.com>
2022-10-27 04:08:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b7f88d5ac4ad690aebe83c190c75b2715ddc1ece

--- benchmarksResult/MemoryBenchmark/data.js
+++ benchmarksResult/MemoryBenchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666841178665,
+  "lastUpdate": 1666843702395,
   "repoUrl": "https://github.com/H01001000/discordeno",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 274.66,
             "unit": "MB",
             "range": 3.87
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "heiheiho000@gmail.com",
+            "name": "H01001000",
+            "username": "H01001000"
+          },
+          "committer": {
+            "email": "heiheiho000@gmail.com",
+            "name": "H01001000",
+            "username": "H01001000"
+          },
+          "distinct": true,
+          "id": "b7f88d5ac4ad690aebe83c190c75b2715ddc1ece",
+          "message": "fix: use bot from this commit",
+          "timestamp": "2022-10-27T12:07:16+08:00",
+          "tree_id": "589d5416f09fc7be318318807ff8a3b83f31f100",
+          "url": "https://github.com/H01001000/discordeno/commit/b7f88d5ac4ad690aebe83c190c75b2715ddc1ece"
+        },
+        "date": 1666843699826,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "RSS",
+            "value": 272.56,
+            "unit": "MB",
+            "range": 9.17
+          },
+          {
+            "name": "Heap Used",
+            "value": 260.1,
+            "unit": "MB",
+            "range": 11.02
+          },
+          {
+            "name": "Heap Total",
+            "value": 273.33,
+            "unit": "MB",
+            "range": 5.17
           }
         ]
       }
