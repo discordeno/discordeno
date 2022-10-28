2a36e06e6996272a69a5e65b9bb0d84c6c305835
H01001000 <heiheiho000@gmail.com>
2022-10-28 16:49:40 +0800

    feat: add using cache

--- performance/generateMessage.ts
+++ performance/generateMessage.ts
@@ -1,80 +1,73 @@
 const commitSha = await Deno.readTextFile("./sha");
-const repo = await Deno.readTextFile("./repo");
-const results = JSON.parse(await Deno.readTextFile("./output.txt"));
-
-await import(`https://raw.githubusercontent.com/${repo}/benchies/benchmarksResult/data.js`);
+const results = JSON.parse(await Deno.readTextFile("./data.txt"));
 
 let message = "# Benchmark";
 
-//@ts-ignore
-if (window.BENCHMARK_DATA) {
-  //@ts-ignore
-  const benchmarks = JSON.parse(JSON.stringify(window.BENCHMARK_DATA.entries.Benchmark)).slice(-2) as {
-    commit: {
-      author: { email: string; name: string; username: string };
-      committer: { email: string; name: string; username: string };
-      distinct: boolean;
-      id: string;
-      message: string;
-      timestamp: string;
-      tree_id: string;
-      url: string;
-    };
-    date: number;
-    tool: string;
-    benches: { name: string; value: number; unit: string; range: string }[];
-  }[];
+const benchmarks = results.entries.Benchmark.slice(-2) as {
+  commit: {
+    author: { email: string; name: string; username: string };
+    committer: { email: string; name: string; username: string };
+    distinct: boolean;
+    id: string;
+    message: string;
+    timestamp: string;
+    tree_id: string;
+    url: string;
+  };
+  date: number;
+  tool: string;
+  benches: { name: string; value: number; unit: string; range: string }[];
+}[];
 
-  const compare: {
-    [index: string]: {
-      current: { name: string; value: number; unit: string; range: string } | {
-        name?: string;
-        value?: number;
-        unit?: string;
-        range?: string;
-      };
-      previous: { name: string; value: number; unit: string; range: string } | {
-        name?: string;
-        value?: number;
-        unit?: string;
-        range?: string;
-      };
+const compare: {
+  [index: string]: {
+    current: { name: string; value: number; unit: string; range: string } | {
+      name?: string;
+      value?: number;
+      unit?: string;
+      range?: string;
     };
-  } = {};
+    previous: { name: string; value: number; unit: string; range: string } | {
+      name?: string;
+      value?: number;
+      unit?: string;
+      range?: string;
+    };
+  };
+} = {};
 
-  for (const benchmark of benchmarks[0].benches) {
-    compare[benchmark.name] = {
-      previous: benchmark,
-      current: {},
-    };
-  }
-  for (const benchmark of benchmarks[1].benches) {
-    compare[benchmark.name] = {
-      //@ts-ignore
-      previous: {},
-      ...compare[benchmark.name],
-      current: benchmark,
-    };
-  }
-  message += `\n\n| Benchmark suite | Current: ${benchmarks[1].commit.id} | Previous: ${
-    benchmarks[0].commit.id
-  } | Ratio |\n | -| -| -| -|\n`;
-  for (const field of Object.keys(compare)) {
-    message += `| \`${field}\` | ${compare[field].current.value ? `\`${compare[field].current.value}\`` : ""} ${
-      compare[field].current.unit ?? ""
-    } ${compare[field].current.range ? `(\`${compare[field].current.range ?? ""}\`)` : ""} | ${
-      compare[field].previous.value ? `\`${compare[field].previous.value}\`` : ""
-    } ${compare[field].previous.unit ?? ""} ${
-      compare[field].previous.range ? `(\`${compare[field].previous.range ?? ""}\`)` : ""
-    } | ${
-      compare[field].previous.value && compare[field].current.value
-        ? `\`${
-          //@ts-ignore
-          Math.round((parseFloat(compare[field].previous.value) / parseFloat(compare[field].current.value)) * 100) /
-          100}\``
-        : ""
-    } |\n`;
-  }
+for (const benchmark of benchmarks[0].benches) {
+  compare[benchmark.name] = {
+    previous: benchmark,
+    current: {},
+  };
+}
+for (const benchmark of benchmarks[1].benches) {
+  compare[benchmark.name] = {
+    //@ts-ignore
+    previous: {},
+    ...compare[benchmark.name],
+    current: benchmark,
+  };
+}
+message += `\n\n| Benchmark suite | Current: ${benchmarks[1].commit.id} | Previous: ${
+  benchmarks[0].commit.id
+} | Ratio |\n | -| -| -| -|\n`;
+for (const field of Object.keys(compare)) {
+  message += `| \`${field}\` | ${compare[field].current.value ? `\`${compare[field].current.value}\`` : ""} ${
+    compare[field].current.unit ?? ""
+  } ${compare[field].current.range ? `(\`${compare[field].current.range ?? ""}\`)` : ""} | ${
+    compare[field].previous.value ? `\`${compare[field].previous.value}\`` : ""
+  } ${compare[field].previous.unit ?? ""} ${
+    compare[field].previous.range ? `(\`${compare[field].previous.range ?? ""}\`)` : ""
+  } | ${
+    compare[field].previous.value && compare[field].current.value
+      ? `\`${
+        //@ts-ignore
+        Math.round((parseFloat(compare[field].previous.value) / parseFloat(compare[field].current.value)) * 100) /
+        100}\``
+      : ""
+  } |\n`;
 }
 
 await import(`https://raw.githubusercontent.com/discordeno/discordeno/benchies/benchmarksResult/data.js`);
@@ -132,68 +125,6 @@ if (window.BENCHMARK_DATA) {
   message += `\n\n| Benchmark suite | Current (Head): ${
     commitSha.slice(-1)
   } | Previous (Base): ${benchmarks.commit.id} | Ratio |\n | -| -| -| -|\n`;
-  for (const field of Object.keys(compare)) {
-    message += `| \`${field}\` | ${compare[field].current.value ? `\`${compare[field].current.value}\`` : ""} ${
-      compare[field].current.unit ?? ""
-    } ${compare[field].current.range ? `(\`${compare[field].current.range ?? ""}\`)` : ""} | ${
-      compare[field].previous.value ? `\`${compare[field].previous.value}\`` : ""
-    } ${compare[field].previous.unit ?? ""} ${
-      compare[field].previous.range ? `(\`${compare[field].previous.range ?? ""}\`)` : ""
-    } | ${
-      compare[field].previous.value && compare[field].current.value
-        ? `\`${
-          //@ts-ignore
-          Math.round((parseFloat(compare[field].previous.value) / parseFloat(compare[field].current.value)) * 100) /
-          100}\``
-        : ""
-    } |\n`;
-  }
-}
-
-if (message === "# Benchmark") {
-  //@ts-ignore
-  const benchmarks = JSON.parse(JSON.stringify(window.BENCHMARK_DATA.entries.Benchmark)).slice(-2) as {
-    commit: {
-      author: { email: string; name: string; username: string };
-      committer: { email: string; name: string; username: string };
-      distinct: boolean;
-      id: string;
-      message: string;
-      timestamp: string;
-      tree_id: string;
-      url: string;
-    };
-    date: number;
-    tool: string;
-    benches: { name: string; value: number; unit: string; range: string }[];
-  }[];
-
-  const compare: {
-    [index: string]: {
-      current: { name: string; value: number; unit: string; range: string } | {
-        name?: string;
-        value?: number;
-        unit?: string;
-        range?: string;
-      };
-      previous: { name: string; value: number; unit: string; range: string } | {
-        name?: string;
-        value?: number;
-        unit?: string;
-        range?: string;
-      };
-    };
-  } = {};
-
-  for (const benchmark of results) {
-    compare[benchmark.name] = {
-      //@ts-ignore
-      previous: {},
-      current: benchmark,
-    };
-  }
-
-  message += `\n\n| Benchmark suite | Current: ${commitSha.slice(-1)} | Previous:  | Ratio |\n | -| -| -| -|\n`;
   for (const field of Object.keys(compare)) {
     message += `| \`${field}\` | ${compare[field].current.value ? `\`${compare[field].current.value}\`` : ""} ${
       compare[field].current.unit ?? ""
