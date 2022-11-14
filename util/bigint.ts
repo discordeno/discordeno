1cc01a8fb5547451b823781572e758bb4c0f2ab6
H01001000 <heiheiho000@gmail.com>
2022-11-15 03:10:08 +0800

    add bigString to reverse snowflake

--- util/bigint.ts
+++ util/bigint.ts
@@ -4,6 +4,6 @@ export function snowflakeToBigint(snowflake: BigString) {
   return BigInt(snowflake) | 0n;
 }
 
-export function bigintToSnowflake(snowflake: bigint) {
+export function bigintToSnowflake(snowflake: BigString) {
   return snowflake === 0n ? "" : snowflake.toString();
 }
