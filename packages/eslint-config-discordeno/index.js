aaebf36eb3c5b9d8d4f3cddd38f5a1437ff5c05e
H01001000 <heiheiho000@gmail.com>
2022-12-01 01:58:02 +0800

    chore: setup turborepo

---
+++ packages/eslint-config-discordeno/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  rules: {
+  }
+}
