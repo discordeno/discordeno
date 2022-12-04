d8df68d0643f58ee4d7768b496dc0a78c64d8635
H01001000 <heiheiho000@gmail.com>
2022-12-04 14:36:31 +0800

    refactor: dump preset

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -23,10 +23,7 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/discordeno/discordeno/tree/main/site/",
-        },
+        docs: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -49,12 +46,14 @@ const config = {
             sidebarId: "docs",
             position: "left",
             label: "Docs",
+            docsPluginId: "docs",
           },
           {
             type: "docSidebar",
             sidebarId: "tutorial",
             position: "left",
             label: "Tutorial",
+            docsPluginId: "tutorial",
           },
           {
             href: "https://github.com/discordeno/discordeno",
@@ -71,19 +70,19 @@ const config = {
             items: [
               {
                 label: "Introduction",
-                to: "/docs/docs/intro",
+                to: "/docs/intro",
               },
               {
                 label: "Getting Started",
-                to: "/docs/docs/getting-started",
+                to: "/docs/getting-started",
               },
               {
                 label: "FAQ",
-                to: "/docs/docs/frequently-asked-questions",
+                to: "/docs/frequently-asked-questions",
               },
               {
                 label: "Benchmark",
-                to: "/docs/docs/benchmark",
+                to: "/docs/benchmark",
               },
             ],
           },
@@ -92,15 +91,15 @@ const config = {
             items: [
               {
                 label: "Big Bot",
-                to: "/docs/tutorial/big-bot-guide/step-by-step",
+                to: "/tutorial/big-bot-guide/step-by-step",
               },
               {
                 label: "Node.js",
-                to: "/docs/tutorial/nodejs/getting-started",
+                to: "/tutorial/nodejs/getting-started",
               },
               {
                 label: "Amethyst",
-                to: "/docs/tutorial/amethyst/intro",
+                to: "/tutorial/amethyst/intro",
               },
             ],
           },
@@ -132,16 +131,37 @@ const config = {
     }),
 
   plugins: [
-    // ... Your other plugins.
+    [
+      "@docusaurus/plugin-content-docs",
+      /** @type {import('@docusaurus/plugin-content-docs').Options} */
+      {
+        id: "docs",
+        path: "docs",
+        routeBasePath: "docs",
+        sidebarPath: require.resolve("./docsSidebars.js"),
+        editUrl: "https://github.com/discordeno/discordeno/tree/main/site/",
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      /** @type {import('@docusaurus/plugin-content-docs').Options} */
+      {
+        id: "tutorial",
+        path: "tutorial",
+        routeBasePath: "tutorial",
+        sidebarPath: require.resolve("./tutorialSidebars.js"),
+        editUrl: "https://github.com/discordeno/discordeno/tree/main/site/",
+      },
+    ],
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
       {
         indexDocs: true,
         indexPages: true,
-        docsRouteBasePath: ["/docs"],
+        docsRouteBasePath: ["/docs", "/tutorial"],
         language: ["en"],
         hashed: true,
-        docsDir: ["docs"],
+        docsDir: ["docs", "tutorial"],
         blogDir: [],
         removeDefaultStopWordFilter: true,
         highlightSearchTermsOnTargetPage: true,
@@ -155,11 +175,11 @@ const config = {
       {
         createRedirects(existingPath) {
           if (
-            existingPath.includes("/docs/tutorial/big-bot-guide/") || existingPath.includes("/docs/tutorial/nodejs/") ||
-            existingPath.includes("/docs/tutorial/amethyst/")
+            existingPath.includes("/tutorial/big-bot-guide/") || existingPath.includes("/tutorial/nodejs/") ||
+            existingPath.includes("/tutorial/amethyst/")
           ) {
             return [
-              existingPath.replace("/docs/tutorial/", "/docs/"),
+              existingPath.replace("/tutorial/", "/docs/"),
             ];
           }
           if (
@@ -167,12 +187,7 @@ const config = {
             existingPath.includes("/docs/docs/getting-started") || existingPath.includes("/docs/docs/migrating")
           ) {
             return [
-              existingPath.replace("/docs/docs/", "/docs/general/"),
-            ];
-          }
-          if (existingPath.includes("/docs/docs/intro")) {
-            return [
-              existingPath.replace("/docs/docs/", "/docs/"),
+              existingPath.replace("/docs/", "/docs/general/"),
             ];
           }
           return undefined; // Return a falsy value: no redirect created
