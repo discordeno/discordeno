869554167fba7c3fda0fa873d17f9f99f09d6392
H01001000 <heiheiho000@gmail.com>
2022-12-05 05:41:02 +0800

    test: move bot utils test

--- packages/utils/tests/utils.spec.ts
+++ packages/utils/tests/utils.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import sinon from 'sinon'
-import { delay } from '../src/utils.js'
+import { delay, formatImageURL, hasProperty } from '../src/utils.js'
 
 describe('utils.ts', () => {
   let clock: sinon.SinonFakeTimers
@@ -25,4 +25,29 @@ describe('utils.ts', () => {
     await clock.tickAsync(31)
     expect(delayEnded).to.be.true
   })
+})
+
+const obj = { prop: 'lts372005' }
+
+it('[utils] hasProperty does HAVE property', () => {
+  expect(hasProperty(obj, 'prop')).to.be.equal(true)
+})
+
+it('[utils] hasProperty does NOT HAVE property', () => {
+  expect(hasProperty(obj, 'lts372005')).to.be.equal(false)
+})
+
+it('[utils] format image url', () => {
+  expect(formatImageURL('https://skillz.is.pro')).to.be.equal(
+    'https://skillz.is.pro.jpg?size=128'
+  )
+  expect(formatImageURL('https://skillz.is.pro', 1024)).to.be.equal(
+    'https://skillz.is.pro.jpg?size=1024'
+  )
+  expect(formatImageURL('https://skillz.is.pro', 1024, 'gif')).to.be.equal(
+    'https://skillz.is.pro.gif?size=1024'
+  )
+  expect(formatImageURL('https://skillz.is.pro', undefined, 'gif')).to.be.equal(
+    'https://skillz.is.pro.gif?size=128'
+  )
 })
