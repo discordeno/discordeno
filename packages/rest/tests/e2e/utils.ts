47059b96be57e316ab0f176a4b5cbc10d0a9c274
H01001000 <heiheiho000@gmail.com>
2022-12-21 13:10:27 +0800

    ci: add discord token

--- packages/rest/tests/e2e/utils.ts
+++ packages/rest/tests/e2e/utils.ts
@@ -5,7 +5,7 @@ dotenv.config()
 if (!process.env.DISCORD_TOKEN) throw new Error('Token was not provided.')
 export const token = process.env.DISCORD_TOKEN
 
-const UNIT_TEST_GUILD_ID = process.env.UNIT_TEST_GUILD_ID
-export const CACHED_COMMUNITY_GUILD_ID = UNIT_TEST_GUILD_ID
-  ? BigInt(UNIT_TEST_GUILD_ID)
+const E2E_TEST_GUILD_ID = process.env.E2E_TEST_GUILD_ID
+export const CACHED_COMMUNITY_GUILD_ID = E2E_TEST_GUILD_ID
+  ? BigInt(E2E_TEST_GUILD_ID)
   : 907350958810480671n
