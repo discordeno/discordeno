a03f191358895dc5d471b48490d425a4d6c2540d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2023-01-03 18:50:48 +0000

    fix: add gateway package base

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -1818,27 +1818,27 @@ export interface DiscordMessage {
 //   location?: string
 // }
 
-// /** https://discord.com/developers/docs/topics/gateway#get-gateway-bot */
-// export interface DiscordGetGatewayBot {
-//   /** The WSS URL that can be used for connecting to the gateway */
-//   url: string
-//   /** The recommended number of shards to use when connecting */
-//   shards: number
-//   /** Information on the current session start limit */
-//   session_start_limit: DiscordSessionStartLimit
-// }
+/** https://discord.com/developers/docs/topics/gateway#get-gateway-bot */
+export interface DiscordGetGatewayBot {
+  /** The WSS URL that can be used for connecting to the gateway */
+  url: string
+  /** The recommended number of shards to use when connecting */
+  shards: number
+  /** Information on the current session start limit */
+  session_start_limit: DiscordSessionStartLimit
+}
 
-// /** https://discord.com/developers/docs/topics/gateway#session-start-limit-object */
-// export interface DiscordSessionStartLimit {
-//   /** The total number of session starts the current user is allowed */
-//   total: number
-//   /** The remaining number of session starts the current user is allowed */
-//   remaining: number
-//   /** The number of milliseconds after which the limit resets */
-//   reset_after: number
-//   /** The number of identify requests allowed per 5 seconds */
-//   max_concurrency: number
-// }
+/** https://discord.com/developers/docs/topics/gateway#session-start-limit-object */
+export interface DiscordSessionStartLimit {
+  /** The total number of session starts the current user is allowed */
+  total: number
+  /** The remaining number of session starts the current user is allowed */
+  remaining: number
+  /** The number of milliseconds after which the limit resets */
+  reset_after: number
+  /** The number of identify requests allowed per 5 seconds */
+  max_concurrency: number
+}
 
 // /** https://discord.com/developers/docs/resources/invite#invite-metadata-object */
 // export interface DiscordInviteMetadata extends DiscordInvite {
