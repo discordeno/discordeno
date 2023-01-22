ae1d0d5b8a813531eb2a9b084bbebd8802a82bd2
H01001000 <heiheiho000@gmail.com>
2023-01-21 19:00:40 -0800

    test(utils): fix missing import buffer

--- packages/utils/tests/token.spec.ts
+++ packages/utils/tests/token.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
+import { Buffer } from 'node:buffer'
 import { getBotIdFromToken, removeTokenPrefix } from '../src/token.js'
 
 describe('token.ts', () => {
