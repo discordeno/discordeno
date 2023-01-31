bec0f8facd3442eef97a3b03016264b0e3b96c04
Skillz4Killz <drzishan12@gmail.com>
2023-01-31 16:40:02 +0000

    fox: remove more logs

--- packages/rest/tests/e2e/utils.ts
+++ packages/rest/tests/e2e/utils.ts
@@ -1,18 +1,16 @@
-import { logger, LogLevels } from '@discordeno/utils'
+import { LogDepth, logger, LogLevels } from '@discordeno/utils'
 
 import { createRestManager } from '../../src/manager.js'
 import { token } from './constants.js'
 // For debugging purposes
-logger.setLevel(LogLevels.Debug)
+// logger.setLevel(LogLevels.Debug)
+// logger.setDepth(LogDepth.Full)
 
 export const rest = createRestManager({
   token,
 })
 rest.deleteQueueDelay = 10000
 
-console.log('CREATING GUILD')
 export const e2ecache = {
   guild: await rest.createGuild({ name: 'ddenotester' }),
 }
-
-console.log('CACHED check', e2ecache.guild)
