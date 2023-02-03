0b1c3184a87021b5d8e24490f38fc09d70b00154
H01001000 <heiheiho000@gmail.com>
2023-02-03 14:54:25 -0800

    test(utils): add color test

---
+++ packages/utils/tests/color.spec.ts
@@ -0,0 +1,51 @@
+import { expect } from 'chai'
+import * as colors from '../src/colors.js'
+
+const arrayOfColors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+
+describe('color.ts', () => {
+  arrayOfColors.forEach((color, index) => {
+    describe(`${color} function`, () => {
+      it('will return colored word', () => {
+        expect(colors[color]('testWord')).to.equal(`\x1B[${30 + index}mtestWord\x1B[39m`)
+      })
+    })
+  })
+
+  describe(`grey function`, () => {
+    it('will return colored word', () => {
+      expect(colors.gray('testWord')).to.equal(`\x1B[${90}mtestWord\x1B[39m`)
+    })
+  })
+  arrayOfColors
+    .map((color) => `bright${color.slice(0, 1).toUpperCase()}${color.slice(1)}`)
+    .forEach((color, index) => {
+      describe(`${color} function`, () => {
+        it('will return colored word', () => {
+          expect(colors[color]('testWord')).to.equal(`\x1B[${90 + index}mtestWord\x1B[39m`)
+        })
+      })
+    })
+
+  arrayOfColors
+    .map((color) => `bg${color.slice(0, 1).toUpperCase()}${color.slice(1)}`)
+    .forEach((color, index) => {
+      describe(`${color} function`, () => {
+        it('will return colored word', () => {
+          console.dir(colors[color]('testWord'))
+          expect(colors[color]('testWord')).to.equal(`\x1B[${40 + index}mtestWord\x1B[49m`)
+        })
+      })
+    })
+
+  arrayOfColors
+    .map((color) => `bgBright${color.slice(0, 1).toUpperCase()}${color.slice(1)}`)
+    .forEach((color, index) => {
+      describe(`${color} function`, () => {
+        it('will return colored word', () => {
+          console.dir(colors[color]('testWord'))
+          expect(colors[color]('testWord')).to.equal(`\x1B[${100 + index}mtestWord\x1B[49m`)
+        })
+      })
+    })
+})
