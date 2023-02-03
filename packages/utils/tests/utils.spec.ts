d678f0a27930db0d2eed4684c4a7193a3e9a9101
H01001000 <heiheiho000@gmail.com>
2023-02-03 14:38:16 -0800

    Revert "test(utils): remove old test"
    This reverts commit 04fb6dd4b57b153c7ae418ce0798eb93e687857a.

--- packages/utils/tests/utils.spec.ts
+++ packages/utils/tests/utils.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import { afterEach, beforeEach, describe, it } from 'mocha'
 import sinon from 'sinon'
 import { delay, formatImageURL } from '../src/utils.js'
+let hasProperty: any
 
 describe('utils.ts', () => {
   let clock: sinon.SinonFakeTimers
@@ -26,6 +27,16 @@ describe('utils.ts', () => {
     await clock.tickAsync(31)
     expect(delayEnded).to.be.true
   })
+})
+
+const obj = { prop: 'lts372005' }
+
+it.skip('[utils] hasProperty does HAVE property', () => {
+  expect(hasProperty(obj, 'prop')).to.be.equal(true)
+})
+
+it.skip('[utils] hasProperty does NOT HAVE property', () => {
+  expect(hasProperty(obj, 'lts372005')).to.be.equal(false)
 })
 
 it('[utils] format image url', () => {
