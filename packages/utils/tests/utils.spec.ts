04fb6dd4b57b153c7ae418ce0798eb93e687857a
H01001000 <heiheiho000@gmail.com>
2023-02-03 14:33:33 -0800

    test(utils): remove old test

--- packages/utils/tests/utils.spec.ts
+++ packages/utils/tests/utils.spec.ts
@@ -2,7 +2,6 @@ import { expect } from 'chai'
 import { afterEach, beforeEach, describe, it } from 'mocha'
 import sinon from 'sinon'
 import { delay, formatImageURL } from '../src/utils.js'
-let hasProperty: any
 
 describe('utils.ts', () => {
   let clock: sinon.SinonFakeTimers
@@ -27,16 +26,6 @@ describe('utils.ts', () => {
     await clock.tickAsync(31)
     expect(delayEnded).to.be.true
   })
-})
-
-const obj = { prop: 'lts372005' }
-
-it.skip('[utils] hasProperty does HAVE property', () => {
-  expect(hasProperty(obj, 'prop')).to.be.equal(true)
-})
-
-it.skip('[utils] hasProperty does NOT HAVE property', () => {
-  expect(hasProperty(obj, 'lts372005')).to.be.equal(false)
 })
 
 it('[utils] format image url', () => {
