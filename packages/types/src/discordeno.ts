2dafa8f5f81daa25e62da285faacec42c8073df4
Skillz <gankstarsleague@gmail.com>
2023-02-13 13:54:41 -0600

    fix: remove aliases and add createGuildFromTemplate

--- packages/types/src/discordeno.ts
+++ packages/types/src/discordeno.ts
@@ -1059,3 +1059,11 @@ export interface EditGuildStickerOptions extends WithReason {
   /** Autocomplete/suggestion tags for the sticker (max 200 characters) */
   tags?: string
 }
+
+/** https://discord.com/developers/docs/resources/template#create-guild-from-template-json-params */
+export interface CreateGuildFromTemplate {
+  /** Name of the guild (2-100 characters) */
+  name: string
+  /** base64 128x128 image for the guild icon */
+  icon?: string
+}
