5f6a1114927e83e88ac513038c9bd07999ebc7fc
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2023-02-20 21:14:07 +0000

    fix: remove old files

--- handlers/guilds/GUILD_AUDIT_LOG_ENTRY_CREATE.ts
+++
@@ -1,8 +0,0 @@
-import type { Bot } from "../../bot.ts";
-import { DiscordGatewayPayload, DiscordAuditLogEntry } from "../../types/discord.ts";
-
-export async function handleGuildAuditLogEntryCreate(bot: Bot, data: DiscordGatewayPayload) {
-  // TODO: better type here
-  const payload = data.d as DiscordAuditLogEntry & { guild_id: string };
-  bot.events.auditLogEntryCreate(bot, bot.transformers.auditLogEntry(bot, payload), bot.transformers.snowflake(payload.guild_id));
-}
