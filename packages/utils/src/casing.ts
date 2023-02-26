77c6f50aa2947d883a0a6d5ab3f5479b7a46840e
H01001000 <heiheiho000@gmail.com>
2023-02-26 09:08:27 -0800

    close test(utils): more test for casing.ts #2797

--- packages/utils/src/casing.ts
+++ packages/utils/src/casing.ts
@@ -9,7 +9,7 @@ export const camelize = <T>(object: T): Camelize<T> => {
     const obj = {} as Camelize<T>
     ;(Object.keys(object) as Array<keyof T>).forEach((key) => {
       // @ts-expect-error js hack
-      ;(obj[typeof key === 'string' ? snakeToCamelCase(key) : key] as Camelize<(T & object)[keyof T]>) = camelize(object[key])
+      ;(obj[snakeToCamelCase(key)] as Camelize<(T & object)[keyof T]>) = camelize(object[key])
     })
     return obj
   }
@@ -24,9 +24,9 @@ export const snakelize = <T>(object: T): Snakelize<T> => {
   if (typeof object === 'object' && object !== null) {
     const obj = {} as Snakelize<T>
     ;(Object.keys(object) as Array<keyof T>).forEach((key) => {
+      console.log(Object.keys(object))
       // @ts-expect-error js hack
-
-      ;(obj[typeof key === 'string' ? camelToSnakeCase(key) : key] as Snakelize<(T & object)[keyof T]>) = snakelize(object[key])
+      ;(obj[camelToSnakeCase(key)] as Snakelize<(T & object)[keyof T]>) = snakelize(object[key])
     })
     return obj
   }
