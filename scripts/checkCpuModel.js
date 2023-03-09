456a792494b9ab70023bd27b94953c95b07d9d98
H01001000 <heiheiho000@gmail.com>
2023-03-08 19:06:21 -0800

    ci: fix script

--- scripts/checkCpuModel.js
+++ scripts/checkCpuModel.js
@@ -1,7 +1,9 @@
 import os from 'node:os'
 
 const hostCpu = os.cpus()[0].model.trim()
-const targetCpu = await fetch('https://raw.githubusercontent.com/discordeno/discordeno/benchies/cpu').then((res) => res.text())
+const targetCpu = await fetch('https://raw.githubusercontent.com/discordeno/discordeno/benchies/cpu')
+  .then((res) => res.text())
+  .then((text) => text.slice(0, -1))
 console.dir(`host cpu: ${hostCpu} target cpu: ${targetCpu}`)
 
 if (hostCpu !== targetCpu) process.exit(1)
