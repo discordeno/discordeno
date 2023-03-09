3112007ed91d9dbadb7d46c3983f2a1690bbbd2d
H01001000 <heiheiho000@gmail.com>
2023-03-08 19:52:33 -0800

    fix: read file with dir

--- packages/benchmark/src/index.ts
+++ packages/benchmark/src/index.ts
@@ -9,7 +9,7 @@ const files = await fs.readdir('db/events')
 
 await Promise.all(
   files.map(async (file) => {
-    const eventsInFile = Object.values(await fs.readFile(file, 'utf8').then((text) => JSON.parse(text)))
+    const eventsInFile = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
     eventsInFile.forEach((eventInFile) => {
       if (typeof eventInFile === 'string') return
       events.push(eventInFile)
