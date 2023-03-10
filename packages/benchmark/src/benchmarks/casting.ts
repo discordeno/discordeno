948d57f3678f51e401b68c4028ebccf3972ffe2d
H01001000 <heiheiho000@gmail.com>
2023-03-10 09:53:52 -0800

    chore: clean up dir

---
+++ packages/benchmark/src/benchmarks/casting.ts
@@ -0,0 +1,37 @@
+import { camelize, snakelize } from '@discordeno/utils'
+import fs from 'node:fs/promises'
+import { suite } from '../benchmarkSuite.js'
+
+const events: any[] = []
+const camelizedEvents: any[] = []
+
+const files = await fs.readdir('db/events')
+
+console.log('Start reading events')
+console.time('read db')
+await Promise.all(
+  files.map(async (file) => {
+    const eventsInFile = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
+    eventsInFile.forEach((eventInFile) => {
+      if (typeof eventInFile === 'string') return
+      events.push(eventInFile)
+    })
+  }),
+)
+console.timeEnd('read db')
+
+events.forEach((event) => {
+  camelizedEvents.push(camelize(event))
+})
+
+suite.add(`Camelize ${events.length} events`, () => {
+  events.forEach((event) => {
+    camelize(event)
+  })
+})
+
+suite.add(`Snakelize ${events.length} events`, () => {
+  camelizedEvents.forEach((event) => {
+    snakelize(event)
+  })
+})
