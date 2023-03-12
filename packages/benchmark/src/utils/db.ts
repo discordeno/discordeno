2876ae7c4ceac88c70cdde6a7297d230b66bc2cc
H01001000 <heiheiho000@gmail.com>
2023-03-11 20:22:00 -0800

    ci: read file in sync

--- packages/benchmark/src/utils/db.ts
+++ packages/benchmark/src/utils/db.ts
@@ -4,12 +4,10 @@ export const events: any[] = []
 
 const files = await fs.readdir('db/events')
 
-await Promise.all(
-  files.map(async (file) => {
-    const eventsInFile = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
-    eventsInFile.forEach((eventInFile) => {
-      if (typeof eventInFile === 'string') return
-      events.push(eventInFile)
-    })
-  }),
-)
+for await (const file of files) {
+  const eventsInFile = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
+  eventsInFile.forEach((eventInFile) => {
+    if (typeof eventInFile === 'string') return
+    events.push(eventInFile)
+  })
+}
