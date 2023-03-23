cf7e5d22b297b04acbec3a9ba0682962a5f2f817
H01001000 <heiheiho000@gmail.com>
2023-03-22 23:58:58 -0700

    test(utils): add some bucket test

--- packages/utils/tests/bucket.spec.ts
+++ packages/utils/tests/bucket.spec.ts
@@ -3,6 +3,14 @@ import { afterEach, beforeEach, describe, it } from 'mocha'
 import sinon from 'sinon'
 import { LeakyBucket } from '../src/bucket.js'
 
+const promiseState = async (p: Promise<any>): Promise<string> => {
+  const t = {}
+  return await Promise.race([p, t]).then(
+    (v) => (v === t ? 'pending' : 'fulfilled'),
+    () => 'rejected',
+  )
+}
+
 describe('bucket.ts', () => {
   let clock: sinon.SinonFakeTimers
 
@@ -15,13 +23,12 @@ describe('bucket.ts', () => {
     clock.restore()
   })
 
-  describe('LeakyBucket function', () => {
+  describe('LeakyBucket class', () => {
     it('will return bucket with given options', () => {
       const options = {
         max: 6006,
         refillInterval: 2002,
         refillAmount: 3003,
-        tokens: 4004,
         someThingElse: {
           thing: 'else',
         },
@@ -37,7 +44,6 @@ describe('bucket.ts', () => {
         max: 111,
         refillInterval: 2002,
         refillAmount: 3003,
-        tokens: 4004,
       }
       const bucket = new LeakyBucket(options)
       expect(bucket.refillAmount).to.equal(options.max)
@@ -48,7 +54,6 @@ describe('bucket.ts', () => {
         max: 111,
         refillInterval: 2002,
         refillAmount: 3003,
-        tokens: 4004,
       }
       const bucket = new LeakyBucket(options)
       expect(bucket.refillAmount).to.equal(options.max)
@@ -61,6 +66,119 @@ describe('bucket.ts', () => {
         refillAmount: 3003,
       })
       expect(bucket.queue).to.deep.equal([])
+    })
+
+    describe('remaining getter', () => {
+      it('will return the remaining amount', async () => {
+        const options = {
+          max: 10,
+          refillInterval: 2002,
+          refillAmount: 1,
+        }
+        const bucket = new LeakyBucket(options)
+        expect(bucket.remaining).to.equal(options.max)
+        bucket.used = 1
+        expect(bucket.remaining).to.equal(options.max - 1)
+      })
+
+      it('will return 0 if no remining', async () => {
+        const options = {
+          max: 10,
+          refillInterval: 2002,
+          refillAmount: 1,
+        }
+        const bucket = new LeakyBucket(options)
+        bucket.used = 20
+        expect(bucket.remaining).to.equal(0)
+      })
+    })
+
+    describe('.acquire() method', () => {
+      it('will put acquire in queue', async () => {
+        const bucket = new LeakyBucket({
+          max: 1,
+          refillInterval: 1,
+          refillAmount: 1,
+        })
+        sinon.stub(bucket, 'processQueue')
+
+        const acquired = bucket.acquire()
+        expect(await promiseState(acquired)).to.equal('pending')
+        expect(bucket.queue.length).equal(1)
+        bucket.queue.shift()?.()
+        await (async () => {})()
+        expect(await promiseState(acquired)).to.equal('fulfilled')
+      })
+
+      it('will put high priority acquire in front of queue', async () => {
+        const bucket = new LeakyBucket({
+          max: 1,
+          refillInterval: 1,
+          refillAmount: 1,
+        })
+        sinon.stub(bucket, 'processQueue')
+
+        const dummyFunction = (): void => {}
+        bucket.queue.push(dummyFunction)
+        expect(bucket.queue[0]).equal(dummyFunction)
+
+        bucket.acquire(true)
+        expect(bucket.queue[1]).equal(dummyFunction)
+      })
+
+      it('will put acquire in queue', async () => {
+        const bucket = new LeakyBucket({
+          max: 1,
+          refillInterval: 1,
+          refillAmount: 1,
+        })
+        const processQueueStub = sinon.stub(bucket, 'processQueue')
+
+        bucket.acquire()
+        expect(processQueueStub.called).to.equal(true)
+      })
+    })
+  })
+
+  describe('.processQueue() method', async () => {
+    it('will resolve queue', async () => {
+      const bucket = new LeakyBucket({
+        max: 2,
+        refillInterval: 1,
+        refillAmount: 2,
+      })
+
+      const processQueueStub = sinon.stub(bucket, 'processQueue')
+      const acquired1 = bucket.acquire()
+      const acquired2 = bucket.acquire()
+      processQueueStub.restore()
+
+      await bucket.processQueue()
+
+      expect(await promiseState(acquired1)).to.equal('fulfilled')
+      expect(await promiseState(acquired2)).to.equal('fulfilled')
+      expect(bucket.used).equal(2)
+    })
+
+    it('will refill bucket', async () => {
+      const bucket = new LeakyBucket({
+        max: 2,
+        refillInterval: 1,
+        refillAmount: 2,
+      })
+
+      const processQueueStub = sinon.stub(bucket, 'processQueue')
+      const acquired = bucket.acquire()
+      processQueueStub.restore()
+
+      await bucket.processQueue()
+
+      expect(await promiseState(acquired)).to.equal('fulfilled')
+      expect(bucket.remaining).equal(1)
+
+      await clock.tickAsync(1)
+
+      expect(bucket.remaining).equal(2)
     })
   })
 })
