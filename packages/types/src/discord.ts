86268295905ca94449b7465e8e1a90ac8b3f5cb5
ITOH <to@itoh.at>
2023-03-28 23:52:39 +0200

    feat(bot,types): add `customMessage` to `AutoModerationActionMetadata`
    Closes: https://github.com/discordeno/discordeno/issues/2881

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -1443,6 +1443,8 @@ export enum AutoModerationActionType {
 export interface DiscordAutoModerationActionMetadata {
   /** The id of channel to which user content should be logged. Only in ActionType.SendAlertMessage */
   channel_id?: string
+  /** Additional explanation that will be shown to members whenever their message is blocked. Maximum of 150 characters. */
+  custom_message?: string
   /** Timeout duration in seconds maximum of 2419200 seconds (4 weeks). Only supported for TriggerType.Keyword && Only in ActionType.Timeout */
   duration_seconds?: number
 }
