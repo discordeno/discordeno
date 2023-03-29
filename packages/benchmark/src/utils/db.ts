6559f6cabe028e09ad10d9ef3b063b54a13a4ca5
H01001000 <heiheiho000@gmail.com>
2023-03-29 15:41:17 -0700

    bench: add type to db event

--- packages/benchmark/src/utils/db.ts
+++ packages/benchmark/src/utils/db.ts
@@ -1,11 +1,21 @@
+import type { DiscordGatewayPayload } from '@discordeno/types'
 import fs from 'node:fs/promises'
 
-export const events: any[] = []
+export const events: Array<{
+  shardId: number
+  payload: DiscordGatewayPayload
+}> = []
 
 const files = await fs.readdir('db/events')
 
 for await (const file of files) {
-  const eventsInFile = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
+  const eventsInFile: Array<
+    | {
+        shardId: number
+        payload: DiscordGatewayPayload
+      }
+    | string
+  > = Object.values(await fs.readFile(`db/events/${file}`, 'utf8').then((text) => JSON.parse(text)))
   eventsInFile.forEach((eventInFile) => {
     if (typeof eventInFile === 'string') return
     events.push(eventInFile)
