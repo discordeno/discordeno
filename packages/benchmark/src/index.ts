ece13ef402a040e62785a37904210687a04ef0fe
H01001000 <heiheiho000@gmail.com>
2023-03-29 15:40:09 -0700

    refactor(bench): use loader

--- packages/benchmark/src/index.ts
+++ packages/benchmark/src/index.ts
@@ -1,7 +1,9 @@
+import fs from 'node:fs/promises'
 import { suite } from './benchmarkSuite.js'
 
-import './benchmarks/casting.js'
-import './benchmarks/memory.js'
+const benchmarks = await fs.readdir('packages/benchmark/dist/benchmarks').then((files) => files.filter((file) => file.endsWith('.js')))
+
+await Promise.all(benchmarks.map(async (file) => await import(`./benchmarks/${file}`)))
 
 suite
   .on('cycle', function (event: any) {
