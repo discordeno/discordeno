4c682e1a2eb1051a2616e862be973268392104c8
ITOH <to@itoh.at>
2023-04-01 16:50:50 +0200

    fix export

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export * from './base64.js'
 export * from './bucket.js'
-export * from './builders.js'
+export * from './builders'
 export * from './casing.js'
 export * from './Collection.js'
 export * from './colors.js'
