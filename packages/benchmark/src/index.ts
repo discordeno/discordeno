a29e45218d7130258fbf35d5ba282588848da067
Jonathan Ho <heiheiho000@gmail.com>
2023-03-31 21:53:35 -0700

    feat(bench): commit benchmark benchies    (#2918)
    * refactor(bench): better readdir
    
    * Update retryBenchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * Update benchmark.yml
    
    * ci: comment the result
    
    * ci: fix missing data

--- packages/benchmark/src/index.ts
+++ packages/benchmark/src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 import { suite } from './benchmarkSuite.js'
 
-const benchmarks = await fs.readdir('packages/benchmark/dist/benchmarks').then((files) => files.filter((file) => file.endsWith('.js')))
+const benchmarks = await fs.readdir(new URL('./benchmarks', import.meta.url)).then((files) => files.filter((file) => file.endsWith('.js')))
 
 await Promise.all(benchmarks.map(async (file) => await import(`./benchmarks/${file}`)))
 
