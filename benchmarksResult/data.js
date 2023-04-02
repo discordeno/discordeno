ea0db6ce75a45abfb271401a58ae6dce4eac3fa1
Jonathan Ho <heiheiho000@gmail.com>
2023-04-02 12:06:15 +0800

    remove old data

--- benchmarksResult/data.js
+++ benchmarksResult/data.js
@@ -6,48 +6,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "62352949+6km@users.noreply.github.com",
-            "name": "Mohammed Taha",
-            "username": "6km"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "1a717ddc7d97e31d3775747df9df153729d2925d",
-          "message": "[📜 Docs] Fix grammar issues (#2779)\n\n* Update getting-started.md\r\n\r\n* Update frequently-asked-questions.md\r\n\r\n* Update getting-started.md",
-          "timestamp": "2023-02-13T12:22:05-06:00",
-          "tree_id": "037e5cc7815b967d7c0057bec6becabc648678cd",
-          "url": "https://github.com/discordeno/discordeno/commit/1a717ddc7d97e31d3775747df9df153729d2925d"
-        },
-        "date": 1676312605677,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "[Cache Plugin] RSS",
-            "value": 284.2,
-            "unit": "MB",
-            "range": "280.36 … 288.51"
-          },
-          {
-            "name": "[Cache Plugin] Heap Used",
-            "value": 268.13,
-            "unit": "MB",
-            "range": "266.4 … 270.12"
-          },
-          {
-            "name": "[Cache Plugin] Heap Total",
-            "value": 277.12,
-            "unit": "MB",
-            "range": "275.51 … 277.98"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "heiheiho000@gmail.com",
             "name": "Jonathan Ho",
             "username": "H01001000"
