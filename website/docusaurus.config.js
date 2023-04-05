cd9092836cc3c5ae79dcc00c9972c23cba3ad7cb
H01001000 <heiheiho000@gmail.com>
2023-04-04 20:51:16 -0700

    fix(site): remove default image

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -61,8 +61,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Discordeno',
         logo: {
