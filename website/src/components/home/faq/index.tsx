c2899873686a3dd63f70d0497f9f4c085f393962
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2023-04-16 05:51:08 +0000

    fix: faq

--- website/src/components/home/faq/index.tsx
+++ website/src/components/home/faq/index.tsx
@@ -44,22 +44,24 @@ const Faq = ({
 
 const questions = [
   {
-    question: 'Question 1',
-    answer: 'Answer 1',
+    question: 'Does Discordeno work on all JavaScript runtimes like Deno, Node.js, and Bun?',
+    answer: "Yes! Discordeno is designed to work on any JavaScript runtime, including Deno, Node.js, and even in the Bun environment. This means that you can use Discordeno in your project no matter where you're running your code.",
     defaultExpanded: true,
   },
   {
-    question: 'Question 2',
-    answer: 'Answer 2',
-    defaultExpanded: true,
+    question: 'Does Discordeno handle sharding?',
+    answer: "Yes! Discordeno helps run some of the largest bot's on discord. Discordeno has built-in support for sharding, which allows you to distribute your bot across multiple processes or servers. Sharding can help improve the scalability and reliability of your bot, especially if you're working with a large number of guilds or users. Discordeno's sharding system is designed to be easy to use and configure, with support for automatic sharding and custom sharding strategies. Make sure to check the Discordeno documentation for more information on how to use sharding in your bot.",
+    defaultExpanded: false,
   },
   {
-    question: 'Question 3',
-    answer: 'Answer 3',
+    question: "Why doesn't Discordeno use Classes like other libraries?",
+    answer: "Yes and No. Generally, Discordeno does not use classes. However, in certain cases where it makes sense we do use them. Instead, Discordeno uses plain JavaScript objects to define commands, events, and other components. This design choice was made to keep the library lightweight and simple, and to allow for more flexible and dynamic code, as components can be easily modified and extended at runtime. While classes can be useful in some cases, Discordeno has chosen to use a more functional approach.",
+    defaultExpanded: false,
   },
   {
-    question: 'Question 4',
-    answer: 'Answer 4',
+    question: 'Can I still use classes in my bot with Discordeno?',
+    answer: "Yes! While Discordeno itself does not use classes, you can still use classes in your own code by using one of the many libraries or frameworks that provide class-based abstractions on top of Discordeno. Some examples of such libraries include the Discordeno.js which provides a very similar framework and API to Discord.js or the Sinf library that provides a similar API to Eris library. These libraries provide classes and other abstractions that can help simplify the development of your bot, while still leveraging the power and flexibility of Discordeno's underlying object-based API. Make sure to check the documentation of these libraries for more information on how to use them in your bot.",
+    defaultExpanded: false,
   },
 ]
 
