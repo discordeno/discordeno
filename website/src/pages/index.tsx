b9561d313d6353cc3e7e40febd1267a92d9e636f
Peter Hanania <peter@pogy.xyz>
2023-04-16 17:35:30 -0400

    Mobile responsiveness + add loading

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -10,7 +10,59 @@ import DiscordenoReviews from '../components/home/reviews'
 import DiscordenoFAQ from '../components/home/faq'
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
+  // Use loading to give time to JS to load
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+
+      // give it a small timeout to make sure the loading screen is shown
+    }, 1000)
+  }, [])
+
+  if (loading)
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          width: '100vw',
+          background: 'var(--ifm-navbar-background-color)',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '10px',
+          }}
+        >
+          <img
+            src="/img/logo.png"
+            alt="Discordeno Logo"
+            style={{
+              width: '100px',
+              height: '100px',
+            }}
+          />
+          <span
+            style={{
+              color: 'var(--ifm-navbar-color)',
+              fontSize: '20px',
+              fontWeight: 'bold',
+            }}
+          >
+            Loading...
+          </span>
+        </div>
+      </div>
+    )
+
   return (
     <Layout
       title={`Discordeno Documentation`}
