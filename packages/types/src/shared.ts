a98665e39878a4134fa72953daa6dae8d65f7521
Awesome Stickz <awesome@stickz.dev>
2023-04-18 16:44:34 +0530

    fix: docs links for typings

--- packages/types/src/shared.ts
+++ packages/types/src/shared.ts
@@ -650,13 +650,13 @@ export enum GatewayCloseEventCodes {
   UnknownOpcode,
   /** You sent an invalid [payload](https://discord.com/developers/docs/topics/gateway#sending-payloads) to us. Don't do that! */
   DecodeError,
-  /** You sent us a payload prior to [identifying](https://discord.com/developers/docs/topics/gateway#identify). */
+  /** You sent us a payload prior to [identifying](https://discord.com/developers/docs/topics/gateway-events#identify). */
   NotAuthenticated,
-  /** The account token sent with your [identify payload](https://discord.com/developers/docs/topics/gateway#identify) is incorrect. */
+  /** The account token sent with your [identify payload](https://discord.com/developers/docs/topics/gateway-events#identify) is incorrect. */
   AuthenticationFailed,
   /** You sent more than one identify payload. Don't do that! */
   AlreadyAuthenticated,
-  /** The sequence sent when [resuming](https://discord.com/developers/docs/topics/gateway#resume) the session was invalid. Reconnect and start a new session. */
+  /** The sequence sent when [resuming](https://discord.com/developers/docs/topics/gateway-events#resume) the session was invalid. Reconnect and start a new session. */
   InvalidSeq = 4007,
   /** Woah nelly! You're sending payloads to us too quickly. Slow it down! You will be disconnected on receiving this. */
   RateLimited,
