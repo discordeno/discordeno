8cc748330686b57f285fba39578eeb920b9d2702
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2023-06-04 21:15:31 -0500

    Update packages/bot/src/handlers/guilds/GUILD_DELETE.ts

--- packages/bot/src/handlers/guilds/GUILD_DELETE.ts
+++ packages/bot/src/handlers/guilds/GUILD_DELETE.ts
@@ -4,7 +4,7 @@ import type { Bot } from '../../bot.js'
 export async function handleGuildDelete(bot: Bot, data: DiscordGatewayPayload, shardId: number): Promise<void> {
   const payload = data.d as DiscordUnavailableGuild
 
-  if (payload.unavailable) return bot.events.guildUnavailable?(bot.transformers.snowflake(payload.id), shardId);
+  if (payload.unavailable) return bot.events.guildUnavailable?.(bot.transformers.snowflake(payload.id), shardId);
 
   bot.events.guildDelete?.(bot.transformers.snowflake(payload.id), shardId)
 }
