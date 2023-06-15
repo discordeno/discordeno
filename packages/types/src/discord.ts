53bfbe3e6be1afa2309b4af53e1562133b37dac0
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2023-06-15 16:58:45 +0200

    fix(types,bot): fix global_name typing

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -267,7 +267,7 @@ export interface DiscordTeamMember {
   /** The id of the parent team of which they are a member */
   team_id: string
   /** The avatar, discriminator, id, and username of the user */
-  user: Partial<DiscordUser> & Pick<DiscordUser, 'avatar' | 'discriminator' | 'id' | 'username'>
+  user: Partial<DiscordUser> & Pick<DiscordUser, 'avatar' | 'discriminator' | 'id' | 'username' | 'global_name'>
 }
 
 /** https://discord.com/developers/docs/topics/gateway#webhooks-update-webhook-update-event-fields */
