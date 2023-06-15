238d15a172803bf4578336a737a52e5c6cc1d172
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2023-06-15 17:03:44 +0200

    fix(types): include global_name in the comment

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -266,7 +266,7 @@ export interface DiscordTeamMember {
   permissions: Array<'*'>
   /** The id of the parent team of which they are a member */
   team_id: string
-  /** The avatar, discriminator, id, and username of the user */
+  /** The avatar, discriminator, id, username, and global_name of the user */
   user: Partial<DiscordUser> & Pick<DiscordUser, 'avatar' | 'discriminator' | 'id' | 'username' | 'global_name'>
 }
 
