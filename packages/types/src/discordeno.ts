8e0b071c445d6a21d391a826ed529f2f005b2a83
Awesome Stickz <awesome@stickz.dev>
2023-07-04 21:54:33 +0530

    fix(docs): set ListGuildMembers options.limit default to 1

--- packages/types/src/discordeno.ts
+++ packages/types/src/discordeno.ts
@@ -348,7 +348,7 @@ export interface GetBans {
 
 /** https://discord.com/developers/docs/resources/guild#list-guild-members */
 export interface ListGuildMembers {
-  /** Max number of members to return (1-1000). Default: 1000 */
+  /** Max number of members to return (1-1000). Default: 1 */
   limit?: number
   /** The highest user id in the previous page. Default: 0 */
   after?: string
