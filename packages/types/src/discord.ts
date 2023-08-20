db8c464081ff702d242e225fcf431d26aed25492
Skillz <gankstarsleague@gmail.com>
2023-08-20 11:16:17 -0500

    fix: docs update Closes #3054

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -982,7 +982,7 @@ export interface DiscordMessage {
   mention_roles?: string[]
   /**
    * Channels specifically mentioned in this message
-   * Note: Not all channel mentions in a message will appear in `mention_channels`. Only textual channels that are visible to everyone in a lurkable guild will ever be included. Only crossposted messages (via Channel Following) currently include `mention_channels` at all. If no mentions in the message meet these requirements, this field will not be sent.
+   * Note: Not all channel mentions in a message will appear in `mention_channels`. Only textual channels that are visible to everyone in a discoverable guild will ever be included. Only crossposted messages (via Channel Following) currently include `mention_channels` at all. If no mentions in the message meet these requirements, this field will not be sent.
    */
   mention_channels?: DiscordChannelMention[]
   /** Any attached files */
