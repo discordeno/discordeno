1e8edb99c6e2af666b6ea84b67a5c2291894b9d9
Fleny <Fleny113@outlook.com>
2023-08-24 19:21:17 +0200

    bug(rest) Invert unauthorized check (#3104)

--- packages/rest/src/manager.ts
+++ packages/rest/src/manager.ts
@@ -156,7 +156,7 @@ export function createRestManager(options: CreateRestManagerOptions): RestManage
         'user-agent': `DiscordBot (https://github.com/discordeno/discordeno, v${version})`,
       }
 
-      if (options?.unauthorized !== false) headers.authorization = `Bot ${rest.token}`
+      if (options?.unauthorized !== true) headers.authorization = `Bot ${rest.token}`
 
       // IF A REASON IS PROVIDED ENCODE IT IN HEADERS
       if (options?.reason !== undefined) {
