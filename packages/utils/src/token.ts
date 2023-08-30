c80950f034b9a699fb489efbf41e456a59ddac42
Fleny <fleny113@outlook.com>
2023-08-30 19:25:35 +0200

    Fix removeTokenPrefix to support Bearer tokens

--- packages/utils/src/token.ts
+++ packages/utils/src/token.ts
@@ -6,10 +6,13 @@ export function removeTokenPrefix(token?: string, type: 'GATEWAY' | 'REST' = 'RE
   if (token === undefined) {
     throw new Error(`The ${type} was not given a token. Please provide a token and try again.`)
   }
+
+  const splittedToken = token.split(' ')
+
   // If the token does not have a prefix just return token
-  if (!token.startsWith('Bot ')) return token
+  if (splittedToken.length < 2) return token
   // Remove the prefix and return only the token.
-  return token.substring(token.indexOf(' ') + 1)
+  return splittedToken[1]
 }
 
 /** Get the bot id from the bot token. WARNING: Discord staff has mentioned this may not be stable forever. Use at your own risk. However, note for over 5 years this has never broken. */
