08317f36ebcc00acc208b6a5e1bad0fc12fd7467
Fleny <fleny113@outlook.com>
2023-09-09 12:30:14 +0200

    add Bearer prefixed url to the rest of the logs

--- packages/rest/src/queue.ts
+++ packages/rest/src/queue.ts
@@ -71,7 +71,7 @@ export class Queue {
     this.processing = true
 
     while (this.waiting.length > 0) {
-      logger.debug(`[Queue] ${this.url} process waiting while loop ran.`)
+      logger.debug(`[Queue] ${this.isOauth2Queue() ? '' : 'Bearer '}${this.url} process waiting while loop ran.`)
       if (this.isRequestAllowed()) {
         // Resolve the next item in the queue
         this.waiting.shift()?.()
@@ -93,7 +93,7 @@ export class Queue {
     this.processingPending = true
 
     while (this.pending.length > 0) {
-      logger.debug(`Queue ${this.url} process pending while loop ran with ${this.pending.length}.`)
+      logger.debug(`Queue ${this.isOauth2Queue() ? '' : 'Bearer '}${this.url} process pending while loop ran with ${this.pending.length}.`)
       if (!this.firstRequest && !this.isRequestAllowed()) {
         const now = Date.now()
         const future = this.frozenAt + this.interval
@@ -135,7 +135,7 @@ export class Queue {
       }
     }
 
-    logger.debug(`Queue ${this.url} process pending while loop exited with ${this.pending.length}.`)
+    logger.debug(`Queue ${this.isOauth2Queue() ? '' : 'Bearer '}${this.url} process pending while loop exited with ${this.pending.length}.`)
 
     // Mark as false so next pending request can be triggered by new loop.
     this.processingPending = false
@@ -174,11 +174,11 @@ export class Queue {
       return
     }
 
-    logger.debug(`[Queue] ${this.url}. Delaying delete for ${this.deleteQueueDelay}ms`)
+    logger.debug(`[Queue] ${this.isOauth2Queue() ? '' : 'Bearer '}${this.url}. Delaying delete for ${this.deleteQueueDelay}ms`)
     // Delete in a minute giving a bit of time to allow new requests that may reuse this queue
     setTimeout(async () => {
       if (!this.isQueueClearable()) {
-        logger.debug(`[Queue] ${this.url}. is not clearable. Restarting processing of queue.`)
+        logger.debug(`[Queue] ${this.isOauth2Queue() ? '' : 'Bearer '}${this.url}. is not clearable. Restarting processing of queue.`)
         this.processPending()
         return
       }
@@ -189,7 +189,7 @@ export class Queue {
       this.rest.queues.delete(`${this.authentication}${this.url}`)
       logger.debug(
         `[Queue] ${this.url}. Deleted! Remaining: (${this.rest.queues.size})`,
-        [...this.rest.queues.values()].map((queue) => `${queue.authentication === '' ? '' : 'Bearer '}${queue.url}`),
+        [...this.rest.queues.values()].map((queue) => `${queue.isOauth2Queue() ? '' : 'Bearer '}${queue.url}`),
       )
       if (this.rest.queues.size) this.processPending()
     }, this.deleteQueueDelay)
@@ -205,6 +205,10 @@ export class Queue {
     if (this.processingPending) return false
 
     return true
+  }
+
+  isOauth2Queue(): boolean {
+    return this.authentication === ''
   }
 }
 
