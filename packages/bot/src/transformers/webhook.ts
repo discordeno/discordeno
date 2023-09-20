c1cfadbb144150d55e648b6c9cf4d73224dbc228
Fleny <fleny113@outlook.com>
2023-09-20 18:49:22 +0200

    add checks for payload

--- packages/bot/src/transformers/webhook.ts
+++ packages/bot/src/transformers/webhook.ts
@@ -6,14 +6,14 @@ export function transformWebhook(bot: Bot, payload: DiscordWebhook) {
   const props = bot.transformers.desiredProperties.webhook
   const webhook = {} as Webhook
 
-  if (props.id) webhook.id = bot.transformers.snowflake(payload.id)
-  if (props.type) webhook.type = payload.type
+  if (props.id && payload.id) webhook.id = bot.transformers.snowflake(payload.id)
+  if (props.type && payload.type) webhook.type = payload.type
   if (props.guildId && payload.guild_id) webhook.guildId = bot.transformers.snowflake(payload.guild_id)
   if (props.channelId && payload.channel_id) webhook.channelId = bot.transformers.snowflake(payload.channel_id)
   if (props.user && payload.user) webhook.user = bot.transformers.user(bot, payload.user)
-  if (props.name) webhook.name = payload.name ?? ''
+  if (props.name && payload.name) webhook.name = payload.name
   if (props.avatar && payload.avatar) webhook.avatar = iconHashToBigInt(payload.avatar)
-  if (props.token) webhook.token = payload.token
+  if (props.token && payload.token) webhook.token = payload.token
   if (props.applicationId && payload.application_id) webhook.applicationId = bot.transformers.snowflake(payload.application_id)
   if (props.sourceGuild && payload.source_guild)
     webhook.sourceGuild = {
@@ -26,7 +26,7 @@ export function transformWebhook(bot: Bot, payload: DiscordWebhook) {
       id: bot.transformers.snowflake(payload.source_channel.id!),
       name: payload.source_channel.name ?? '',
     }
-  if (props.url) webhook.url = payload.url
+  if (props.url && payload.url) webhook.url = payload.url
 
   return webhook
 }
