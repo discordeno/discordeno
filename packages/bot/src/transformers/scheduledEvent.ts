d701b120cfcab284a12601e7d7767300aa0dd434
Fleny <fleny113@outlook.com>
2023-09-20 18:52:41 +0200

    add checks for payload

--- packages/bot/src/transformers/scheduledEvent.ts
+++ packages/bot/src/transformers/scheduledEvent.ts
@@ -12,20 +12,20 @@ export function transformScheduledEvent(bot: Bot, payload: DiscordScheduledEvent
   const props = bot.transformers.desiredProperties.scheduledEvent
   const scheduledEvent = {} as ScheduledEvent
 
-  if (props.id) scheduledEvent.id = bot.transformers.snowflake(payload.id)
-  if (props.guildId) scheduledEvent.guildId = bot.transformers.snowflake(payload.guild_id)
+  if (props.id && payload.id) scheduledEvent.id = bot.transformers.snowflake(payload.id)
+  if (props.guildId && payload.guild_id) scheduledEvent.guildId = bot.transformers.snowflake(payload.guild_id)
   if (props.channelId && payload.channel_id) scheduledEvent.channelId = bot.transformers.snowflake(payload.channel_id)
   if (props.creatorId && payload.creator_id) scheduledEvent.creatorId = bot.transformers.snowflake(payload.creator_id)
-  if (props.scheduledStartTime) scheduledEvent.scheduledStartTime = Date.parse(payload.scheduled_start_time)
+  if (props.scheduledStartTime && payload.scheduled_start_time) scheduledEvent.scheduledStartTime = Date.parse(payload.scheduled_start_time)
   if (props.scheduledEndTime && payload.scheduled_end_time) scheduledEvent.scheduledEndTime = Date.parse(payload.scheduled_end_time)
   if (props.entityId && payload.entity_id) scheduledEvent.entityId = bot.transformers.snowflake(payload.entity_id)
   if (props.creator && payload.creator) scheduledEvent.creator = bot.transformers.user(bot, payload.creator)
-  if (props.name) scheduledEvent.name = payload.name
-  if (props.description) scheduledEvent.description = payload.description
-  if (props.privacyLevel) scheduledEvent.privacyLevel = payload.privacy_level
-  if (props.status) scheduledEvent.status = payload.status
-  if (props.entityType) scheduledEvent.entityType = payload.entity_type
-  if (props.userCount) scheduledEvent.userCount = payload.user_count ?? 0
+  if (props.name && payload.name) scheduledEvent.name = payload.name
+  if (props.description && payload.description) scheduledEvent.description = payload.description
+  if (props.privacyLevel && payload.privacy_level) scheduledEvent.privacyLevel = payload.privacy_level
+  if (props.status && payload.status) scheduledEvent.status = payload.status
+  if (props.entityType && payload.entity_type) scheduledEvent.entityType = payload.entity_type
+  if (props.userCount && payload.user_count) scheduledEvent.userCount = payload.user_count ?? 0
   if (props.location && payload.entity_metadata?.location) scheduledEvent.location = payload.entity_metadata.location
   if (props.image && payload.image) scheduledEvent.image = iconHashToBigInt(payload.image)
 
