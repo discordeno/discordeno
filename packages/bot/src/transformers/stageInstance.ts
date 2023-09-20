358ab38caf06962414bb960050e92c50cb0fb648
Fleny <fleny113@outlook.com>
2023-09-20 18:51:29 +0200

    add checks for payload

--- packages/bot/src/transformers/stageInstance.ts
+++ packages/bot/src/transformers/stageInstance.ts
@@ -6,10 +6,10 @@ export function transformStageInstance(bot: Bot, payload: DiscordStageInstance) 
   const props = bot.transformers.desiredProperties.stageInstance
   const stageInstance = {} as StageInstance
 
-  if (props.id) stageInstance.id = bot.transformers.snowflake(payload.id)
-  if (props.guildId) stageInstance.guildId = bot.transformers.snowflake(payload.guild_id)
-  if (props.channelId) stageInstance.channelId = bot.transformers.snowflake(payload.channel_id)
-  if (props.topic) stageInstance.topic = payload.topic
+  if (props.id && payload.id) stageInstance.id = bot.transformers.snowflake(payload.id)
+  if (props.guildId && payload.guild_id) stageInstance.guildId = bot.transformers.snowflake(payload.guild_id)
+  if (props.channelId && payload.channel_id) stageInstance.channelId = bot.transformers.snowflake(payload.channel_id)
+  if (props.topic && payload.topic) stageInstance.topic = payload.topic
   if (props.guildScheduledEventId && payload.guild_scheduled_event_id)
     stageInstance.guildScheduledEventId = bot.transformers.snowflake(payload.guild_scheduled_event_id)
 
