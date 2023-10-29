c3fa18e018fa9ea1ab8d4a7984b08ff48e4a8ed5
Matt Hatcher <3768988+MatthewSH@users.noreply.github.com>
2023-10-29 01:54:27 -0500

    fix: edit url now links to DD repo
    Closes #3177

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -44,14 +44,10 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/discordeno/discordeno/tree/main/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/styling/index.css'),
