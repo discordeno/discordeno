9a1ce36c8a7afb41c95435cf8fbb0d50d415ff3a
tavi <107622047+tavithedev@users.noreply.github.com>
2023-12-01 00:38:20 +0200

    Fix error

--- packages/bot/src/transformers/attachment.ts
+++ packages/bot/src/transformers/attachment.ts
@@ -5,16 +5,16 @@ export function transformAttachment(bot: Bot, payload: DiscordAttachment): Attac
   const props = bot.transformers.desiredProperties.attachment
   const attachment = {} as Attachment
 
-  if (props.id && payload.id) attachment.id = bot.transformers.snowflake(payload.id)
-  if (props.filename && payload.filename) attachment.filename = payload.filename
-  if (props.contentType && payload.content_type) attachment.contentType = payload.content_type
-  if (props.size && payload.size) attachment.size = payload.size
-  if (props.url && payload.url) attachment.url = payload.url
-  if (props.proxyUrl && payload.proxy_url) attachment.proxyUrl = payload.proxy_url
-  if (props.height && payload.height) attachment.height = payload.height
-  if (props.width && payload.width) attachment.width = payload.width
-  if (props.ephemeral && payload.ephemeral) attachment.ephemeral = payload.ephemeral
-  if (props.description && payload.description) attachment.description = payload.description
+  if (props?.id && payload.id) attachment.id = bot.transformers.snowflake(payload.id)
+  if (props?.filename && payload.filename) attachment.filename = payload.filename
+  if (props?.contentType && payload.content_type) attachment.contentType = payload.content_type
+  if (props?.size && payload.size) attachment.size = payload.size
+  if (props?.url && payload.url) attachment.url = payload.url
+  if (props?.proxyUrl && payload.proxy_url) attachment.proxyUrl = payload.proxy_url
+  if (props?.height && payload.height) attachment.height = payload.height
+  if (props?.width && payload.width) attachment.width = payload.width
+  if (props?.ephemeral && payload.ephemeral) attachment.ephemeral = payload.ephemeral
+  if (props?.description && payload.description) attachment.description = payload.description
 
   return bot.transformers.customizers.attachment(bot, payload, attachment)
 }
@@ -45,3 +45,4 @@ export interface Attachment {
    */
   ephemeral?: boolean
 }
+
