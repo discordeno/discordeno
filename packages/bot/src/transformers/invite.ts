d1ec2c0719dff076c818f24eef62382f2b8323b8
tavi <107622047+tavithedev@users.noreply.github.com>
2023-12-01 00:41:08 +0200

    Fix error

--- packages/bot/src/transformers/invite.ts
+++ packages/bot/src/transformers/invite.ts
@@ -7,27 +7,27 @@ export function transformInvite(bot: Bot, payload: DiscordInviteCreate | Discord
   const invite = {} as Invite
   const hasMetadata = isInviteWithMetadata(payload)
 
-  if (props.code && payload.code) invite.code = payload.code
-  if (props.createdAt && payload.created_at) invite.createdAt = Date.parse(payload.created_at)
-  if (props.inviter && payload.inviter) invite.inviter = bot.transformers.user(bot, payload.inviter)
-  if (props.maxAge && payload.max_age) invite.maxAge = payload.max_age
-  if (props.maxUses && payload.max_uses) invite.maxUses = payload.max_uses
-  if (props.targetType && payload.target_type) invite.targetType = payload.target_type
-  if (props.targetUser && payload.target_user) invite.targetUser = bot.transformers.user(bot, payload.target_user)
-  if (props.targetApplication && payload.target_application)
+  if (props?.code && payload.code) invite.code = payload.code
+  if (props?.createdAt && payload.created_at) invite.createdAt = Date.parse(payload.created_at)
+  if (props?.inviter && payload.inviter) invite.inviter = bot.transformers.user(bot, payload.inviter)
+  if (props?.maxAge && payload.max_age) invite.maxAge = payload.max_age
+  if (props?.maxUses && payload.max_uses) invite.maxUses = payload.max_uses
+  if (props?.targetType && payload.target_type) invite.targetType = payload.target_type
+  if (props?.targetUser && payload.target_user) invite.targetUser = bot.transformers.user(bot, payload.target_user)
+  if (props?.targetApplication && payload.target_application)
     invite.targetApplication = bot.transformers.application(bot, payload.target_application as DiscordApplication)
-  if (props.temporary && payload.temporary) invite.temporary = payload.temporary
-  if (props.uses && payload.uses) invite.uses = payload.uses
+  if (props?.temporary && payload.temporary) invite.temporary = payload.temporary
+  if (props?.uses && payload.uses) invite.uses = payload.uses
 
   if (hasMetadata) {
-    if (props.channelId && payload.channel?.id) invite.channelId = bot.transformers.snowflake(payload.channel.id)
-    if (props.guildId && payload.guild?.id) invite.guildId = bot.transformers.snowflake(payload.guild.id)
-    if (props.approximateMemberCount && payload.approximate_member_count) invite.approximateMemberCount = payload.approximate_member_count
-    if (props.approximatePresenceCount && payload.approximate_presence_count) invite.approximatePresenceCount = payload.approximate_presence_count
-    if (props.guildScheduledEvent && payload.guild_scheduled_event) {
+    if (props?.channelId && payload.channel?.id) invite.channelId = bot.transformers.snowflake(payload.channel.id)
+    if (props?.guildId && payload.guild?.id) invite.guildId = bot.transformers.snowflake(payload.guild.id)
+    if (props?.approximateMemberCount && payload.approximate_member_count) invite.approximateMemberCount = payload.approximate_member_count
+    if (props?.approximatePresenceCount && payload.approximate_presence_count) invite.approximatePresenceCount = payload.approximate_presence_count
+    if (props?.guildScheduledEvent && payload.guild_scheduled_event) {
       invite.guildScheduledEvent = payload.guild_scheduled_event ? bot.transformers.scheduledEvent(bot, payload.guild_scheduled_event) : undefined
     }
-    if (props.stageInstance && invite.guildId && payload.stage_instance) {
+    if (props?.stageInstance && invite.guildId && payload.stage_instance) {
       invite.stageInstance = payload.stage_instance
         ? bot.transformers.inviteStageInstance(bot, {
             ...payload.stage_instance,
@@ -35,12 +35,12 @@ export function transformInvite(bot: Bot, payload: DiscordInviteCreate | Discord
           })
         : undefined
     }
-    if (props.expiresAt && payload.expires_at) {
+    if (props?.expiresAt && payload.expires_at) {
       invite.expiresAt = Date.parse(payload.expires_at)
     }
   } else {
-    if (props.channelId && payload.channel_id) invite.channelId = bot.transformers.snowflake(payload.channel_id)
-    if (props.guildId && payload.guild_id) invite.guildId = bot.transformers.snowflake(payload.guild_id)
+    if (props?.channelId && payload.channel_id) invite.channelId = bot.transformers.snowflake(payload.channel_id)
+    if (props?.guildId && payload.guild_id) invite.guildId = bot.transformers.snowflake(payload.guild_id)
   }
 
   return bot.transformers.customizers.invite(bot, payload, invite)
