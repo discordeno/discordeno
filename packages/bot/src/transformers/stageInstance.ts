44225d0632a0b9843e744e4a80929c07499d8e2d
tavi <107622047+tavithedev@users.noreply.github.com>
2023-12-01 00:42:30 +0200

    Fix error

--- packages/bot/src/transformers/stageInstance.ts
+++ packages/bot/src/transformers/stageInstance.ts
@@ -5,11 +5,11 @@ export function transformStageInstance(bot: Bot, payload: DiscordStageInstance):
   const props = bot.transformers.desiredProperties.stageInstance
   const stageInstance = {} as StageInstance
 
-  if (props.id && payload.id) stageInstance.id = bot.transformers.snowflake(payload.id)
-  if (props.guildId && payload.guild_id) stageInstance.guildId = bot.transformers.snowflake(payload.guild_id)
-  if (props.channelId && payload.channel_id) stageInstance.channelId = bot.transformers.snowflake(payload.channel_id)
-  if (props.topic && payload.topic) stageInstance.topic = payload.topic
-  if (props.guildScheduledEventId && payload.guild_scheduled_event_id)
+  if (props?.id && payload.id) stageInstance.id = bot.transformers.snowflake(payload.id)
+  if (props?.guildId && payload.guild_id) stageInstance.guildId = bot.transformers.snowflake(payload.guild_id)
+  if (props?.channelId && payload.channel_id) stageInstance.channelId = bot.transformers.snowflake(payload.channel_id)
+  if (props?.topic && payload.topic) stageInstance.topic = payload.topic
+  if (props?.guildScheduledEventId && payload.guild_scheduled_event_id)
     stageInstance.guildScheduledEventId = bot.transformers.snowflake(payload.guild_scheduled_event_id)
 
   return bot.transformers.customizers.stageInstance(bot, payload, stageInstance)
