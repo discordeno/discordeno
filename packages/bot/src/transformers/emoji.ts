660c472071b465d2ddded6f753946bab81b55516
tavi <107622047+tavithedev@users.noreply.github.com>
2023-12-01 00:39:06 +0200

    Fix error

--- packages/bot/src/transformers/emoji.ts
+++ packages/bot/src/transformers/emoji.ts
@@ -6,10 +6,10 @@ export function transformEmoji(bot: Bot, payload: DiscordEmoji): Emoji {
   const props = bot.transformers.desiredProperties.emoji
   const emoji = {} as Emoji
 
-  if (props.id && payload.id) emoji.id = bot.transformers.snowflake(payload.id)
-  if (props.name && payload.name) emoji.name = payload.name
-  if (props.roles && payload.roles) emoji.roles = payload.roles.map((id) => bot.transformers.snowflake(id))
-  if (props.user && payload.user) emoji.user = bot.transformers.user(bot, payload.user)
+  if (props?.id && payload.id) emoji.id = bot.transformers.snowflake(payload.id)
+  if (props?.name && payload.name) emoji.name = payload.name
+  if (props?.roles && payload.roles) emoji.roles = payload.roles.map((id) => bot.transformers.snowflake(id))
+  if (props?.user && payload.user) emoji.user = bot.transformers.user(bot, payload.user)
 
   emoji.toggles = new EmojiToggles(payload)
 
@@ -35,3 +35,4 @@ export interface Emoji {
   available?: boolean
   toggles: EmojiToggles
 }
+
