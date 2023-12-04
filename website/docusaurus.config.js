3d493db0ff962733450945b6ab246cc640ccc587
H01001000 <heiheiho000@gmail.com>
2023-12-04 14:48:39 -0800

    refactor: try swc-loader time

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,11 +25,18 @@ const config = {
 
   webpack: {
     jsLoader: isServer => ({
-      loader: require.resolve('esbuild-loader'),
+      loader: require.resolve('swc-loader'),
       options: {
-        loader: 'tsx',
-        format: isServer ? 'cjs' : undefined,
-        target: isServer ? 'node12' : 'es2017',
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
       },
     }),
   },
