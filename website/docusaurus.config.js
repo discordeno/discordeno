2138da531e1c91972d651aaba5359925e5d1029b
Jonathan Ho <heiheiho000@gmail.com>
2023-12-04 18:09:58 -0800

    chore(deps): update docusaurus to v3 (#3264)
    * chore(deps): update docusaurus to v3
    
    * chore(deps): update all deps in site

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const { themes } = require('prism-react-renderer')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -125,8 +124,8 @@ const config = {
         copyright: `Copyright © 2021-${new Date().getFullYear()}, Discordeno.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.github,
+        darkTheme: themes.dracula,
       },
     }),
 
