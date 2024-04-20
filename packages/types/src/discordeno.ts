bd97b45d19adf3191f4e61a65f29b3e224f1141b
Fleny <Fleny113@outlook.com>
2024-04-20 16:54:54 +0200

    Add default for limit on GetPollAnswerVotes

--- packages/types/src/discordeno.ts
+++ packages/types/src/discordeno.ts
@@ -1323,6 +1323,10 @@ export interface CreatePoll {
 export interface GetPollAnswerVotes {
   /** Get users after this user ID */
   after?: BigString
-  /** Max number of users to return (1-100) */
+  /**
+   * Max number of users to return (1-100)
+   *
+   * @default 25
+   */
   limit?: number
 }
