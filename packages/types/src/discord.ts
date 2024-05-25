e64a14d6a5d3cb859353cec63a9e111ef72c72a8
Fleny <fleny113@outlook.com>
2024-05-25 19:39:43 +0200

    Add invite type

--- packages/types/src/discord.ts
+++ packages/types/src/discord.ts
@@ -2261,6 +2261,8 @@ export interface DiscordInviteMetadata extends DiscordInvite {
 
 /** https://discord.com/developers/docs/resources/invite#invite-object */
 export interface DiscordInvite {
+  /** The type of invite */
+  type: DiscordInviteType
   /** The invite code (unique Id) */
   code: string
   /** The guild this invite is for */
@@ -2285,6 +2287,12 @@ export interface DiscordInvite {
   stage_instance?: DiscordInviteStageInstance
   /** guild scheduled event data */
   guild_scheduled_event?: DiscordScheduledEvent
+}
+
+export enum DiscordInviteType {
+  Guild,
+  GroupDm,
+  Friend,
 }
 
 export interface DiscordInviteStageInstance {
