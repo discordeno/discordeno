8b8733ce8b61732596beb675b5b98894af942ff1
Fleny <fleny113@outlook.com>
2024-07-06 21:47:44 +0200

    Use a getter on the toggles for features

--- packages/bot/src/transformers/guild.ts
+++ packages/bot/src/transformers/guild.ts
@@ -4,7 +4,6 @@ import {
   type DiscordGuild,
   type DiscordPresenceUpdate,
   type ExplicitContentFilterLevels,
-  type GuildFeatures,
   type GuildNsfwLevel,
   type MfaLevels,
   type PremiumTiers,
@@ -12,19 +11,22 @@ import {
   type VerificationLevels,
 } from '@discordeno/types'
 import { Collection, iconHashToBigInt } from '@discordeno/utils'
-import type { Bot, Channel, Member, PresenceUpdate, Role, StageInstance, Sticker, VoiceState, WelcomeScreen } from '../index.js'
+import type { Bot, Channel, GuildFeatureKeys, Member, PresenceUpdate, Role, StageInstance, Sticker, VoiceState, WelcomeScreen } from '../index.js'
 import type { Emoji } from '../transformers/emoji.js'
 import { GuildToggles } from './toggles/guild.js'
 
 const baseGuild = {
   get threads() {
-    if (!this.channels) return
+    if (!this.channels) return new Collection<bigint, Channel>()
 
     const threads = this.channels
       .array()
       .filter((x) => x.type === ChannelTypes.PublicThread || x.type === ChannelTypes.PrivateThread || x.type === ChannelTypes.AnnouncementThread)
 
     return new Collection(threads.map((x) => [x.id, x]))
+  },
+  get features() {
+    return this.toggles.features
   },
 } as Guild
 
@@ -121,7 +123,6 @@ export function transformGuild(bot: Bot, payload: { guild: DiscordGuild } & { sh
   if (props.joinedAt && payload.guild.joined_at) guild.joinedAt = Date.parse(payload.guild.joined_at)
   if (props.memberCount && payload.guild.member_count) guild.memberCount = payload.guild.member_count ?? 0
   if (props.shardId) guild.shardId = payload.shardId
-  if (props.features && payload.guild.features) guild.features = payload.guild.features
   if (props.icon && payload.guild.icon) guild.icon = iconHashToBigInt(payload.guild.icon)
   if (props.banner && payload.guild.banner) guild.banner = iconHashToBigInt(payload.guild.banner)
   if (props.splash && payload.guild.splash) guild.splash = iconHashToBigInt(payload.guild.splash)
@@ -166,7 +167,7 @@ export interface Guild {
   /** Explicit content filter level */
   explicitContentFilter: ExplicitContentFilterLevels
   /** Enabled guild features */
-  features: GuildFeatures[]
+  features: GuildFeatureKeys[]
   /** Required MFA level for the guild */
   mfaLevel: MfaLevels
   /** System channel flags */
