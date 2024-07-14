22ed264185eb55ea8e56721da5097e6284c115c6
Fleny <fleny113@outlook.com>
2024-07-14 17:08:01 +0200

    test: add build:types to test:unit dependencies
    This is a test commit, it will provably get reversed. The scope is to see what CI does with this

--- packages/discordeno/tests/generate.spec.ts
+++ packages/discordeno/tests/generate.spec.ts
@@ -8,8 +8,7 @@ describe('discordeno generate', () => {
   it('will emit without errors', async function () {
     this.timeout(20_000) // 20s, typescript can be slow at doing the entire type checking
 
-    // For the test we use the .ts file, as we unit tests don't depend on the types being compiled
-    const typesFile = await findUp('packages/bot/src/transformers/types.ts', { allowSymlinks: true })
+    const typesFile = await findUp('packages/bot/dist/types/transformers/types.d.ts', { allowSymlinks: true })
 
     expect(typesFile).to.exist
 
