38b69e18589838f14a46ac6849e4b26568c14664
Fleny <fleny113@outlook.com>
2024-08-12 17:47:07 +0200

    Update website deps, remove 1s load time

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -8,59 +8,6 @@ import DiscordenoReviews from '../components/home/reviews'
 import { MainPage } from '../styling'
 
 export default function Home(): JSX.Element {
-  // Use loading to give time to JS to load
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-
-      // give it a small timeout to make sure the loading screen is shown
-    }, 1000)
-  }, [])
-
-  if (loading)
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          width: '100vw',
-          background: 'var(--ifm-navbar-background-color)',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: '10px',
-          }}
-        >
-          <img
-            src="/img/logo.png"
-            alt="Discordeno Logo"
-            style={{
-              width: '100px',
-              height: '100px',
-            }}
-          />
-          <span
-            style={{
-              color: 'var(--ifm-navbar-color)',
-              fontSize: '20px',
-              fontWeight: 'bold',
-            }}
-          >
-            Loading...
-          </span>
-        </div>
-      </div>
-    )
-
   return (
     <Layout title={`Discordeno Documentation`} description="An in-depth guide to using the Discordeno library for Deno.">
       <DiscordenoHeader />
