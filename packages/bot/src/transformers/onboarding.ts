c1b0dc925d90fc67fb0582694d2914496f980a24
Fleny <Fleny113@outlook.com>
2024-08-18 09:04:18 +0200

    fix(bot)!: Remove direct calls to transformers (#3863)
    * Remove direct calls to transformers
    
    * Sort transformers by name
    
    * Fix error

--- packages/bot/src/transformers/onboarding.ts
+++ packages/bot/src/transformers/onboarding.ts
@@ -10,7 +10,7 @@ export function transformGuildOnboarding(bot: Bot, payload: DiscordGuildOnboardi
     guildOnboarding.defaultChannelIds = payload.default_channel_ids.map(bot.transformers.snowflake)
   if (props.enabled) guildOnboarding.enabled = payload.enabled
   if (props.mode) guildOnboarding.mode = payload.mode
-  if (payload.prompts) guildOnboarding.prompts = payload.prompts.map((prompt) => transformGuildOnboardingPrompt(bot, prompt))
+  if (payload.prompts) guildOnboarding.prompts = payload.prompts.map((prompt) => bot.transformers.guildOnboardingPrompt(bot, prompt))
 
   return bot.transformers.customizers.guildOnboarding(bot, payload, guildOnboarding)
 }
@@ -25,9 +25,9 @@ export function transformGuildOnboardingPrompt(bot: Bot, payload: DiscordGuildOn
   if (props.singleSelect && payload.single_select) prompt.singleSelect = payload.single_select
   if (props.title && payload.title) prompt.title = payload.title
   if (props.type) prompt.type = payload.type
-  if (payload.options) prompt.options = payload.options.map((option) => transformGuildOnboardingPromptOption(bot, option))
+  if (payload.options) prompt.options = payload.options.map((option) => bot.transformers.guildOnboardingPromptOption(bot, option))
 
-  return prompt
+  return bot.transformers.customizers.guildOnboardingPrompt(bot, payload, prompt)
 }
 
 export function transformGuildOnboardingPromptOption(bot: Bot, payload: DiscordGuildOnboardingPromptOption): GuildOnboardingPromptOption {
@@ -41,5 +41,5 @@ export function transformGuildOnboardingPromptOption(bot: Bot, payload: DiscordG
   if (props.title && payload.title) option.title = payload.title
   if (props.description && payload.description) option.description = payload.description
 
-  return option
+  return bot.transformers.customizers.guildOnboardingPromptOption(bot, payload, option)
 }
