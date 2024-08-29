15dd4d8a99a9fbf2d239d399d7bedfa6a688b07f
Fleny <fleny113@outlook.com>
2024-08-29 18:23:42 +0200

    Use atob instead of node:buffer for getBotIdFromToken

--- packages/rest/tests/constants.ts
+++ packages/rest/tests/constants.ts
@@ -1,3 +1,3 @@
 import { Buffer } from 'node:buffer'
 
-export const fakeToken = `${Buffer.from('1033452747380494366', 'base64').toString()}.zawsxedcrftvgybhu`
+export const fakeToken = `${Buffer.from('1033452747380494366').toString('base64')}.zawsxedcrftvgybhu`
