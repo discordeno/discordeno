876fc65a646e3515670d89b7d3be3239adc8a47a
Fleny <Fleny113@outlook.com>
2024-10-10 17:48:30 +0200

    Apply suggestions from code review
    Co-authored-by: Awesome Stickz <awesome@stickz.dev>

--- packages/bot/src/transformers/subscription.ts
+++ packages/bot/src/transformers/subscription.ts
@@ -7,8 +7,8 @@ export function transformSubscription(bot: Bot, payload: DiscordSubscription): S
 
   if (props.id && payload.id) subscription.id = bot.transformers.snowflake(payload.id)
   if (props.userId && payload.user_id) subscription.userId = bot.transformers.snowflake(payload.user_id)
-  if (props.skuIds && payload.sku_ids) subscription.skuIds = payload.sku_ids.map((x) => bot.transformers.snowflake(x))
-  if (props.entitlementIds && payload.entitlement_ids) subscription.entitlementIds = payload.entitlement_ids.map((x) => bot.transformers.snowflake(x))
+  if (props.skuIds && payload.sku_ids) subscription.skuIds = payload.sku_ids.map((skuId) => bot.transformers.snowflake(skuId))
+  if (props.entitlementIds && payload.entitlement_ids) subscription.entitlementIds = payload.entitlement_ids.map((entitlementId) => bot.transformers.snowflake(entitlementId))
   if (props.currentPeriodStart && payload.current_period_start) subscription.currentPeriodStart = Date.parse(payload.current_period_start)
   if (props.currentPeriodEnd && payload.current_period_end) subscription.currentPeriodEnd = Date.parse(payload.current_period_end)
   if (props.status && payload.status) subscription.status = payload.status
