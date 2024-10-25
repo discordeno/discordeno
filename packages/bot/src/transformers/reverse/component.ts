c1b133ff444a55cba98d5bc167cf16e02d96f8d0
Fleny <fleny113@outlook.com>
2024-10-25 20:54:37 +0200

    Remove commented code from reverse/component.ts

--- packages/bot/src/transformers/reverse/component.ts
+++ packages/bot/src/transformers/reverse/component.ts
@@ -24,47 +24,6 @@ export function transformComponentToDiscordComponent(bot: Bot, payload: Componen
     case MessageComponentTypes.SelectMenuUsersAndRoles:
       return transformSelectMenuComponent(bot, payload)
   }
-
-  // return {
-  //   type: payload.type,
-  //   custom_id: payload.customId,
-  //   disabled: payload.disabled,
-  //   required: payload.required,
-  //   style: payload.style ? payload.style : undefined,
-  //   label: payload.label,
-  //   emoji: payload.emoji
-  //     ? {
-  //         id: payload.emoji.id?.toString(),
-  //         name: payload.emoji.name,
-  //         animated: payload.emoji.animated,
-  //       }
-  //     : undefined,
-  //   url: payload.url,
-  //   channel_types: payload.channelTypes,
-  //   options: payload.options?.map((option) => ({
-  //     label: option.label,
-  //     value: option.value,
-  //     description: option.description,
-  //     emoji: option.emoji
-  //       ? {
-  //           id: option.emoji.id?.toString(),
-  //           name: option.emoji.name,
-  //           animated: option.emoji.animated,
-  //         }
-  //       : undefined,
-  //     default: option.default,
-  //   })),
-  //   placeholder: payload.placeholder,
-  //   min_values: payload.minValues,
-  //   max_values: payload.maxValues,
-  //   min_length: payload.minLength,
-  //   max_length: payload.maxLength,
-  //   value: payload.value,
-  //   // This casts to a button component, but this really is any component that can go in a actionRow
-  //   components: payload.components?.map((component) => bot.transformers.reverse.component(bot, component)) as DiscordButtonComponent[],
-  //   default_values: payload.defaultValues,
-  //   sku_id: payload.skuId ? bot.transformers.reverse.snowflake(payload.skuId) : undefined,
-  // }
 }
 
 function transformActionRow(bot: Bot, payload: Component): DiscordActionRow {
