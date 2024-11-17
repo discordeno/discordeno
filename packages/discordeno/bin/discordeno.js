2762b483ec9df35a2f115f774051929c13ee284e
Fleny <Fleny113@outlook.com>
2024-11-17 15:39:18 +0100

    chore(discordeno)!: Remove CLI (#3980)
    * Remove the CLI
    
    * Update lockfile
    
    * Fix CI

--- packages/discordeno/bin/discordeno.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-import('../dist/esm/bin/index.js')
--- packages/discordeno/src/index.ts
+++ packages/discordeno/src/index.ts
@@ -1,3 +1,1 @@
 export * from '@discordeno/bot'
-// This needs to be explicit or else typescript gets confused
-export { DesiredPropertiesBehavior, type DiscordenoConfig, defineConfig, findConfig, typescriptOptions } from './bin/config.js'
