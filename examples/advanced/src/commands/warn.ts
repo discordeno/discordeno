124a4b3fc10932701f84c811dc0c1c6b73dd6c19
Fleny <fleny113@outlook.com>
2024-11-28 20:21:17 +0100

    Update examples to v20

--- examples/advanced/src/commands/warn.ts
+++ examples/advanced/src/commands/warn.ts
@@ -30,9 +30,9 @@ createCommand({
     // Type based on the options declared above
     const { user, reason } = options as { user: UserResolved; reason?: string }
 
-    const guild = await bot.cache.guild.get(interaction.guildId)
+    const guild = await bot.cache.guilds.get(interaction.guildId)
 
-    if (!guild) {
+    if (!guild || !guild.roles) {
       await interaction.respond('An error has occurred')
       return
     }
