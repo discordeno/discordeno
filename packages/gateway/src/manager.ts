153408f2862ab33f786da689e4c09c894dc8c1a4
qreepex <dev@qreepex.xyz>
2025-01-30 20:20:41 +0100

    fix(gateway): wrong calculation in calculateWorkerId() (#4096)
    * Update manager.ts
    
    Fix wrong calculation of workerId
    
    * Update manager.ts
    
    * spread incrementally
    
    * fix usage of incorrect variable
    
    ---------
    
    Co-authored-by: Awesome Stickz <awesome@stickz.dev>

--- packages/gateway/src/manager.ts
+++ packages/gateway/src/manager.ts
@@ -271,7 +271,7 @@ export function createGatewayManager(options: CreateGatewayManagerOptions): Gate
       )
     },
     calculateWorkerId(shardId) {
-      const workerId = Math.min(shardId % gateway.shardsPerWorker, gateway.totalWorkers - 1)
+      const workerId = Math.min(Math.floor(shardId / gateway.shardsPerWorker), gateway.totalWorkers - 1);
       gateway.logger.debug(
         `[Gateway] Calculating workerId: Shard: ${shardId} -> Worker: ${workerId} -> Per Worker: ${gateway.shardsPerWorker} -> Total: ${gateway.totalWorkers}`,
       )
