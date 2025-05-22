7abd6fdb35ca837433a12c1ccb2c299fdecdd322
Max Zargov <zargovv@gmail.com>
2025-05-23 01:56:52 +0500

    fix: Unexpected TypeError in `GatewayManager.requestMembers` debug message
    closes #4196

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,3 +1,25 @@
+/** Convert `JSON.stringify`-unserializable record values for debugging purposes. */
+export function jsonSafe<T extends object>(value: T): object {
+  if (value == null) return value
+  const shallowCopy = Array.isArray(value) ? Array.from(value) : Object.assign(Object.create(value), value)
+  for (const key in shallowCopy) {
+    const prop = key as keyof typeof value
+    switch (typeof shallowCopy[prop]) {
+      case 'object':
+        // Recursive traverse.
+        if (shallowCopy[prop] !== null) shallowCopy[prop] = jsonSafe(shallowCopy[prop])
+        break
+      case 'bigint':
+        // Bigints are unserializable by `JSON.stringify`.
+        shallowCopy[prop] = String(shallowCopy[prop])
+        break
+      default: // Any other unhandled type that isn't supposed to require conversion.
+        break
+    }
+  }
+  return shallowCopy
+}
+
 /** Pause the execution for a given amount of milliseconds. */
 export async function delay(ms: number): Promise<void> {
   return new Promise(
