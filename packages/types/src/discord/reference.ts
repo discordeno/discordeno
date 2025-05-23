8ab08c69eb5b5eaa3fe2d2278c4227fb781d5ed0
Fleny <fleny113@outlook.com>
2025-05-23 18:24:51 +0200

    feat(utils): animated webp emojis

--- packages/types/src/discord/reference.ts
+++ packages/types/src/discord/reference.ts
@@ -6,7 +6,7 @@
  * @remarks
  * json is only for stickers
  */
-export type ImageFormat = 'jpg' | 'jpeg' | 'png' | 'webp' | 'gif' | 'json'
+export type ImageFormat = 'jpg' | 'jpeg' | 'png' | 'webp' | 'gif' | 'avif' | 'json'
 
 /** https://discord.com/developers/docs/reference#image-formatting */
 export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096
