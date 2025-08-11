75a728e15ceb0634c970ac10971ca2001d1759f8
Fleny <fleny113@outlook.com>
2025-08-11 19:31:33 +0200

    Remove DiscordApplicationWebhook and rename DiscordIncomingWebhook to DiscordWebhook

--- packages/types/src/discord/webhook.ts
+++ packages/types/src/discord/webhook.ts
@@ -5,43 +5,11 @@ import type { DiscordGuild } from './guild.js'
 import type { DiscordUser } from './user.js'
 
 /** https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-structure */
-export type DiscordWebhook = DiscordIncomingWebhook | DiscordApplicationWebhook
-
-/** https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-structure */
-export interface DiscordIncomingWebhook {
+export interface DiscordWebhook {
   /** The id of the webhook */
   id: string
   /** The type of the webhook */
   type: WebhookTypes
-  /** The guild id this webhook is for */
-  guild_id?: string | null
-  /** The channel id this webhook is for */
-  channel_id: string | null
-  /** The user this webhook was created by (not returned when getting a webhook with its token) */
-  user?: DiscordUser
-  /** The default name of the webhook */
-  name: string | null
-  /** The default user avatar hash of the webhook */
-  avatar: string | null
-  /** The secure token of the webhook (returned for Incoming Webhooks) */
-  token?: string
-  /** The bot/OAuth2 application that created this webhook */
-  application_id: string | null
-  /** The guild of the channel that this webhook is following (returned for Channel Follower Webhooks) */
-  source_guild?: Partial<DiscordGuild>
-  /** The channel that this webhook is following (returned for Channel Follower Webhooks) */
-  source_channel?: Partial<DiscordChannel>
-  /** The url used for executing the webhook (returned by the webhooks OAuth2 flow) */
-  url?: string
-}
-
-// TODO: We might want to remove this type / alias it as DiscordWebhook because discord does not actually document the type specificly for a webhook of type Application (3)
-/** https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-structure */
-export interface DiscordApplicationWebhook {
-  /** The id of the webhook */
-  id: string
-  /** The type of the webhook */
-  type: WebhookTypes.Application
   /** The guild id this webhook is for */
   guild_id?: string | null
   /** The channel id this webhook is for */
