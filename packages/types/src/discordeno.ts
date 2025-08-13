51158071ad4ab951acadd8544c083c10605c1a3a
Fleny <Fleny113@outlook.com>
2025-08-13 11:09:39 +0200

    api-docs!: remove `GetInvite.withExpiration`, remove optional from `DiscordInvite.expires_at` (#4321)

--- packages/types/src/discordeno.ts
+++ packages/types/src/discordeno.ts
@@ -580,8 +580,6 @@ export interface GetScheduledEventUsers {
 export interface GetInvite {
   /** Whether the invite should contain approximate member counts */
   withCounts?: boolean
-  /** Whether the invite should contain the expiration date */
-  withExpiration?: boolean
   /** the guild scheduled event to include with the invite */
   scheduledEventId?: BigString
 }
