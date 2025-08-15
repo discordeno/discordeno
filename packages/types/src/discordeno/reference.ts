c87d5b4dfc551e46019fab167d130a52fa6c1fb1
Link <link20050703@gmail.com>
2025-08-15 14:08:34 -0400

    Apply suggestions from code review

--- packages/types/src/discordeno/reference.ts
+++ packages/types/src/discordeno/reference.ts
@@ -1,12 +1,12 @@
 /** Types for: https://discord.com/developers/docs/reference */
 
 /**
- * This is not a mapping of any discord documented type, however the is some docs that refer to an object like this:
+ * This is not a mapping of any discord documented type, however there is some docs that refer to an object like this:
  * https://discord.com/developers/docs/reference#uploading-files, the second paragraph
  *
  * > All files[n] parameters must include a valid `Content-Disposition` subpart header with a filename and unique name parameter.
  *
- * The blob maps to the FormData data sent and the name to the FormData file name.
+ * The blob maps to the FormData data sent and the name maps to the FormData file name.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/FormData/set}
  */
