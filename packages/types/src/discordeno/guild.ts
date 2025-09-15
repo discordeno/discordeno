141f2f590c202e4a3147c0f7c2c5a4656cb76110
Fleny <Fleny113@outlook.com>
2025-09-15 18:33:25 +0200

    Update packages/types/src/discordeno/guild.ts
    Co-authored-by: Awesome Stickz <awesome@stickz.dev>

--- packages/types/src/discordeno/guild.ts
+++ packages/types/src/discordeno/guild.ts
@@ -169,13 +169,13 @@ export interface ModifyGuildMember {
 
 /** https://discord.com/developers/docs/resources/guild#modify-current-member-json-params */
 export interface EditBotMemberOptions {
-  /** value to set user's nickname to	 */
+  /** Value to set user's nickname to	 */
   nick?: string | null
-  /** data URI base64 encoded banner image */
+  /** Data URI base64 encoded banner image */
   banner?: string | null
-  /** data URI base64 encoded avatar image */
+  /** Data URI base64 encoded avatar image */
   avatar?: string | null
-  /** guild member bio */
+  /** Guild member bio */
   bio?: string | null
 }
 
