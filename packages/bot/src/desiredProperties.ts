87d16dd9baff5aec101e474eb40bb1a197437fc2
Fleny <Fleny113@outlook.com>
2025-09-17 21:20:59 +0200

    feat(types,bot): Add Label component and new modal stuff (#4304)
    * feat(types,bot): Add Label component and new modal stuff
    
    Since now there are some fields that are only in responses the types got a bit more complicated
    
    * Add char limits to label label & desc
    
    * update comments
    
    * fix format
    
    * Move Require to shared.ts
    
    * code review
    
    ---------
    
    Co-authored-by: Awesome Stickz <awesome@stickz.dev>

--- packages/bot/src/desiredProperties.ts
+++ packages/bot/src/desiredProperties.ts
@@ -338,6 +338,8 @@ export function createDesiredPropertiesObject<T extends RecursivePartial<Transfo
       accentColor: defaultValue,
       name: defaultValue,
       size: defaultValue,
+      component: defaultValue,
+      values: defaultValue,
       ...desiredProperties.component,
     },
     forumTag: {
