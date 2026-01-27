bd01ded6bfc9020e693a7dbbc0dcf8d0ce97e3cd
Fleny <Fleny113@outlook.com>
2026-01-27 19:08:08 +0100

    feat: Community Invites (#4685)
    * api-docs: Community Invites
    
    Add support for invites that gives roles to users.
    Add support for target users on invites.
    
    Sort-of unrleated change required: `restManager.makeRequest` `resolve` function had to changed or else the `getTargetUsers` would hang forever due to a JSON parsing issue.
    
    * fix type error & add bot helpers

--- packages/bot/src/desiredProperties.ts
+++ packages/bot/src/desiredProperties.ts
@@ -493,6 +493,7 @@ export function createDesiredPropertiesObject<T extends RecursivePartial<Transfo
       guildScheduledEvent: defaultValue,
       expiresAt: defaultValue,
       flags: defaultValue,
+      roles: defaultValue,
       ...desiredProperties.invite,
     },
     member: {
