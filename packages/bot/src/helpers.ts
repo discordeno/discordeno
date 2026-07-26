f3d0d952556db559fda5a28acac7ba5ffde6edcf
Fleny <fleny113@outlook.com>
2026-07-26 11:45:03 +0200

    feat: Add deleteCurrentUserApplicationRoleConnection endpoint

--- packages/bot/src/helpers.ts
+++ packages/bot/src/helpers.ts
@@ -743,6 +743,9 @@ export function createBotHelpers<TProps extends TransformersDesiredProperties, T
     editUserApplicationRoleConnection: async (bearerToken, applicationId, options) => {
       return await bot.rest.editUserApplicationRoleConnection(bearerToken, applicationId, options);
     },
+    deleteCurrentUserApplicationRoleConnection: async (bearerToken, applicationId) => {
+      return await bot.rest.deleteCurrentUserApplicationRoleConnection(bearerToken, applicationId);
+    },
     joinThread: async (channelId) => {
       return await bot.rest.joinThread(channelId);
     },
@@ -979,6 +982,7 @@ export type BotHelpers<TProps extends TransformersDesiredProperties, TBehavior e
     applicationId: BigString,
     options: Camelize<DiscordApplicationRoleConnection>,
   ) => Promise<Camelize<DiscordApplicationRoleConnection>>;
+  deleteCurrentUserApplicationRoleConnection: (bearerToken: string, applicationId: BigString) => Promise<void>;
   executeWebhook: (
     webhookId: BigString,
     token: string,
