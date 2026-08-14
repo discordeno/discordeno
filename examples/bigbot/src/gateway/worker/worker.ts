b089918769d7fe63008817b70ff43538af53bae9
Fleny <fleny113@outlook.com>
2026-08-14 15:39:47 +0200

    Update tsconfigs and fixup examples to compile

--- examples/bigbot/src/gateway/worker/worker.ts
+++ examples/bigbot/src/gateway/worker/worker.ts
@@ -186,11 +186,6 @@ function createShard(shardId: number): DiscordenoShard {
     return await promise;
   };
 
-  // We do not want to camelize the packet, so we need to override the function as the default behavior is to camelize
-  shard.forwardToBot = (packet) => {
-    shard.events.message?.(shard, packet);
-  };
-
   shard.events.message = handleShardMessageEvent;
 
   return shard;
