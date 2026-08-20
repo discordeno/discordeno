c792010203976f4c00e77e11dd58e7d705505f2e
Fleny <fleny113@outlook.com>
2026-08-20 13:54:54 +0200

    feat!: team partial transformer

--- packages/bot/src/transformers/team.ts
+++ packages/bot/src/transformers/team.ts
@@ -1,23 +1,33 @@
-import type { DiscordTeam } from '@discordeno/types';
+import type { DiscordTeam, DiscordTeamMember } from '@discordeno/types';
 import { iconHashToBigInt } from '@discordeno/utils';
 import type { Bot } from '../bot.js';
-import type { Team } from './types.js';
+import type { DesiredPropertiesBehavior, SetupDesiredProps, TransformersDesiredProperties } from '../desiredProperties.js';
+import { callCustomizer } from '../transformers.js';
+import type { Team, TeamMember } from './types.js';
 
-export function transformTeam(bot: Bot, payload: DiscordTeam) {
-  const id = bot.transformers.snowflake(payload.id);
+export function transformTeam(bot: Bot, payload: Partial<DiscordTeam>, extra?: { partial?: boolean }) {
+  const team = {} as SetupDesiredProps<Team, TransformersDesiredProperties, DesiredPropertiesBehavior>;
 
-  const team = {
-    name: payload.name,
-    id,
-    icon: payload.icon ? iconHashToBigInt(payload.icon) : undefined,
-    ownerUserId: bot.transformers.snowflake(payload.owner_user_id),
-    members: payload.members.map((member) => ({
-      membershipState: member.membership_state,
-      teamId: id,
-      user: bot.transformers.user(bot, member.user),
-      role: member.role,
-    })),
-  } as Team;
+  if (payload.id !== undefined) team.id = bot.transformers.snowflake(payload.id);
+  if (payload.name !== undefined) team.name = payload.name;
+  if (payload.icon) team.icon = iconHashToBigInt(payload.icon);
+  if (payload.owner_user_id !== undefined) team.ownerUserId = bot.transformers.snowflake(payload.owner_user_id);
+  if (payload.members !== undefined) team.members = payload.members.map((member) => bot.transformers.teamMember(bot, member));
 
-  return bot.transformers.customizers.team(bot, payload, team);
+  return callCustomizer('team', bot, payload, team, {
+    partial: extra?.partial ?? false,
+  });
+}
+
+export function transformTeamMember(bot: Bot, payload: Partial<DiscordTeamMember>, extra?: { partial?: boolean }) {
+  const teamMember = {} as SetupDesiredProps<TeamMember, TransformersDesiredProperties, DesiredPropertiesBehavior>;
+
+  if (payload.membership_state !== undefined) teamMember.membershipState = payload.membership_state;
+  if (payload.team_id !== undefined) teamMember.teamId = bot.transformers.snowflake(payload.team_id);
+  if (payload.user !== undefined) teamMember.user = bot.transformers.user(bot, payload.user);
+  if (payload.role !== undefined) teamMember.role = payload.role;
+
+  return callCustomizer('teamMember', bot, payload, teamMember, {
+    partial: extra?.partial ?? false,
+  });
 }
